e7e6183059304096b1add865d0d3f1ae9a87a27b
Antonio Navarro Perez <antnavper@gmail.com>
2016-11-18 15:08:59 +0100

    expression: add Alias (#59)

---
+++ sql/expression/alias.go
@@ -0,0 +1,31 @@
+package expression
+
+import "github.com/gitql/gitql/sql"
+
+type Alias struct {
+	UnaryExpression
+	name string
+}
+
+func NewAlias(child sql.Expression, name string) *Alias {
+	return &Alias{UnaryExpression{child}, name}
+}
+
+func (e *Alias) Type() sql.Type {
+	return e.Child.Type()
+}
+
+func (e *Alias) Eval(row sql.Row) interface{} {
+	return e.Child.Eval(row)
+}
+
+func (e *Alias) Name() string {
+	return e.name
+}
+
+func (e *Alias) TransformUp(f func(sql.Expression) sql.Expression) sql.Expression {
+	c := e.Child.TransformUp(f)
+	n := NewAlias(c, e.name)
+
+	return f(n)
+}
