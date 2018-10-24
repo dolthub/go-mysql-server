277877eda45ae4d78629797a075ff8e9508df893
Javi Fontan <jfontan@gmail.com>
2018-10-24 18:38:00 +0200

    auth: test auth modules
    Signed-off-by: Javi Fontan <jfontan@gmail.com>

---
+++ auth/none_test.go
@@ -0,0 +1,46 @@
+package auth_test
+
+import (
+	"testing"
+
+	"gopkg.in/src-d/go-mysql-server.v0/auth"
+)
+
+func TestNoneAuthentication(t *testing.T) {
+	a := new(auth.None)
+
+	tests := authenticationTests{
+		{"root", "", true},
+		{"root", "password", true},
+		{"root", "mysql_password", true},
+		{"user", "password", true},
+		{"user", "other_password", true},
+		{"user", "", true},
+		{"", "", true},
+		{"", "password", true},
+	}
+
+	testAuthentication(t, a, tests)
+}
+
+func TestNoneAuthorization(t *testing.T) {
+	a := new(auth.None)
+
+	tests := authorizationTests{
+		{"user", queries["select"], true},
+		{"root", queries["select"], true},
+		{"", queries["select"], true},
+
+		{"user", queries["create_index"], true},
+
+		{"root", queries["drop_index"], true},
+
+		{"", queries["insert"], true},
+
+		{"user", queries["lock"], true},
+
+		{"root", queries["unlock"], true},
+	}
+
+	testAuthorization(t, a, tests)
+}
