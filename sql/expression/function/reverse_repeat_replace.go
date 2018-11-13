087d76ca55ddf3d42b3ba822c6f2915eff18c448
Theo Despoudis <thdespou@hotmail.com>
2018-11-13 10:06:51 +0000

    Code review
    Signed-off-by: Theo Despoudis <thdespou@hotmail.com>

--- sql/expression/function/reverse_repeat_replace.go
+++ sql/expression/function/reverse_repeat_replace.go
@@ -44,7 +44,9 @@ func (r *Reverse) Eval(
 
 func reverseString(s string) string {
 	r := []rune(s)
-	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+	l := len(r)
+	l2 := l/2
+	for i, j := 0, l-1; i < l2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
 	return string(r)
