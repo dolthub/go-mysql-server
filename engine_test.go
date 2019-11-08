9be9deb8d1910d6cb2ebc445c4aceb528c7c85a7
Zach Musgrave <zach@liquidata.co>
2019-11-07 16:29:30 -0800

    Better type checking / conversion / error messages for insert. Introduced a Zero method for sql.Type which lets us compare types for compatibility by giving a default value for conversion.
    Signed-off-by: Zach Musgrave <zach@liquidata.co>

--- engine_test.go
+++ engine_test.go
@@ -2054,9 +2054,9 @@ func TestInsertInto(t *testing.T) {
 			[]sql.Row{{
 				int64(999), int8(math.MaxInt8), int16(math.MaxInt16), int32(math.MaxInt32), int64(math.MaxInt64),
 				uint8(math.MaxUint8), uint16(math.MaxUint16), uint32(math.MaxUint32), uint64(math.MaxUint64),
-				float64(math.MaxFloat32), float64(math.MaxFloat64),
+				float32(math.MaxFloat32), float64(math.MaxFloat64),
 				timeParse(sql.TimestampLayout, "2132-04-05 12:51:36"), timeParse(sql.DateLayout, "2231-11-07"),
-				"random text", true, `{"key":"value"}`, "blobdata",
+				"random text", true, ([]byte)(`{"key":"value"}`), ([]byte)("blobdata"),
 			}},
 		},
 		{
@@ -2072,9 +2072,9 @@ func TestInsertInto(t *testing.T) {
 			[]sql.Row{{
 				int64(999), int8(math.MaxInt8), int16(math.MaxInt16), int32(math.MaxInt32), int64(math.MaxInt64),
 				uint8(math.MaxUint8), uint16(math.MaxUint16), uint32(math.MaxUint32), uint64(math.MaxUint64),
-				float64(math.MaxFloat32), float64(math.MaxFloat64),
+				float32(math.MaxFloat32), float64(math.MaxFloat64),
 				timeParse(sql.TimestampLayout, "2132-04-05 12:51:36"), timeParse(sql.DateLayout, "2231-11-07"),
-				"random text", true, `{"key":"value"}`, "blobdata",
+				"random text", true, ([]byte)(`{"key":"value"}`), ([]byte)("blobdata"),
 			}},
 		},
 		{
@@ -2090,9 +2090,9 @@ func TestInsertInto(t *testing.T) {
 			[]sql.Row{{
 				int64(999), int8(-math.MaxInt8 - 1), int16(-math.MaxInt16 - 1), int32(-math.MaxInt32 - 1), int64(-math.MaxInt64 - 1),
 				uint8(0), uint16(0), uint32(0), uint64(0),
-				float64(math.SmallestNonzeroFloat32), float64(math.SmallestNonzeroFloat64),
+				float32(math.SmallestNonzeroFloat32), float64(math.SmallestNonzeroFloat64),
 				timeParse(sql.TimestampLayout, "0010-04-05 12:51:36"), timeParse(sql.DateLayout, "0101-11-07"),
-				"", false, ``, "",
+				"", false, ([]byte)(`""`), ([]byte)(""),
 			}},
 		},
 		{
@@ -2108,9 +2108,9 @@ func TestInsertInto(t *testing.T) {
 			[]sql.Row{{
 				int64(999), int8(-math.MaxInt8 - 1), int16(-math.MaxInt16 - 1), int32(-math.MaxInt32 - 1), int64(-math.MaxInt64 - 1),
 				uint8(0), uint16(0), uint32(0), uint64(0),
-				float64(math.SmallestNonzeroFloat32), float64(math.SmallestNonzeroFloat64),
+				float32(math.SmallestNonzeroFloat32), float64(math.SmallestNonzeroFloat64),
 				timeParse(sql.TimestampLayout, "0010-04-05 12:51:36"), timeParse(sql.DateLayout, "0101-11-07"),
-				"", false, ``, "",
+				"", false, ([]byte)(`""`), ([]byte)(""),
 			}},
 		},
 		{
@@ -3070,7 +3070,8 @@ func testQueryWithContext(ctx *sql.Context, t *testing.T, e *sqle.Engine, q stri
 		rows, err := sql.RowIterToRows(iter)
 		require.NoError(err)
 
-		if orderBy {
+		// .Equal gives better error messages than .ElementsMatch, so use it when possible
+		if orderBy || len(rows) == 1 {
 			require.Equal(expected, rows)
 		} else {
 			require.ElementsMatch(expected, rows)
