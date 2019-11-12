1e23033978c685e4dd036a90ad3d2fccd642744d
Zach Musgrave <zach@liquidata.co>
2019-11-12 15:02:52 -0800

    Fixed replace tests (broken by my changes to type handling code in Update)
    Signed-off-by: Zach Musgrave <zach@liquidata.co>

--- engine_test.go
+++ engine_test.go
@@ -2367,9 +2367,9 @@ func TestReplaceInto(t *testing.T) {
 			[]sql.Row{{
 				int64(999), int8(math.MaxInt8), int16(math.MaxInt16), int32(math.MaxInt32), int64(math.MaxInt64),
 				uint8(math.MaxUint8), uint16(math.MaxUint16), uint32(math.MaxUint32), uint64(math.MaxUint64),
-				float64(math.MaxFloat32), float64(math.MaxFloat64),
+				float32(math.MaxFloat32), float64(math.MaxFloat64),
 				timeParse(sql.TimestampLayout, "2132-04-05 12:51:36"), timeParse(sql.DateLayout, "2231-11-07"),
-				"random text", true, `{"key":"value"}`, "blobdata",
+				"random text", true, ([]byte)(`{"key":"value"}`), ([]byte)("blobdata"),
 			}},
 		},
 		{
@@ -2385,9 +2385,9 @@ func TestReplaceInto(t *testing.T) {
 			[]sql.Row{{
 				int64(999), int8(math.MaxInt8), int16(math.MaxInt16), int32(math.MaxInt32), int64(math.MaxInt64),
 				uint8(math.MaxUint8), uint16(math.MaxUint16), uint32(math.MaxUint32), uint64(math.MaxUint64),
-				float64(math.MaxFloat32), float64(math.MaxFloat64),
+				float32(math.MaxFloat32), float64(math.MaxFloat64),
 				timeParse(sql.TimestampLayout, "2132-04-05 12:51:36"), timeParse(sql.DateLayout, "2231-11-07"),
-				"random text", true, `{"key":"value"}`, "blobdata",
+				"random text", true, ([]byte)(`{"key":"value"}`), ([]byte)("blobdata"),
 			}},
 		},
 		{
@@ -2403,9 +2403,9 @@ func TestReplaceInto(t *testing.T) {
 			[]sql.Row{{
 				int64(999), int8(-math.MaxInt8 - 1), int16(-math.MaxInt16 - 1), int32(-math.MaxInt32 - 1), int64(-math.MaxInt64 - 1),
 				uint8(0), uint16(0), uint32(0), uint64(0),
-				float64(math.SmallestNonzeroFloat32), float64(math.SmallestNonzeroFloat64),
+				float32(math.SmallestNonzeroFloat32), float64(math.SmallestNonzeroFloat64),
 				timeParse(sql.TimestampLayout, "0010-04-05 12:51:36"), timeParse(sql.DateLayout, "0101-11-07"),
-				"", false, ``, "",
+				"", false, ([]byte)(`""`), ([]byte)(""),
 			}},
 		},
 		{
@@ -2421,9 +2421,9 @@ func TestReplaceInto(t *testing.T) {
 			[]sql.Row{{
 				int64(999), int8(-math.MaxInt8 - 1), int16(-math.MaxInt16 - 1), int32(-math.MaxInt32 - 1), int64(-math.MaxInt64 - 1),
 				uint8(0), uint16(0), uint32(0), uint64(0),
-				float64(math.SmallestNonzeroFloat32), float64(math.SmallestNonzeroFloat64),
+				float32(math.SmallestNonzeroFloat32), float64(math.SmallestNonzeroFloat64),
 				timeParse(sql.TimestampLayout, "0010-04-05 12:51:36"), timeParse(sql.DateLayout, "0101-11-07"),
-				"", false, ``, "",
+				"", false, ([]byte)(`""`), ([]byte)(""),
 			}},
 		},
 		{
