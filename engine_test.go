42cdb21168657ca6e9a3fe4151d9c9d2496b5cea
Zach Musgrave <zach@liquidata.co>
2019-12-31 15:02:15 -0800

    Added engine tests for describe and show
    Signed-off-by: Zach Musgrave <zach@liquidata.co>

--- engine_test.go
+++ engine_test.go
@@ -730,6 +730,54 @@ var queries = []queryTest{
 		},
 	},
 	{
+		`DESCRIBE mytable`,
+		[]sql.Row{
+			{"i", "BIGINT", "NO", "", "", ""},
+			{"s", "TEXT", "NO", "", "", ""},
+		},
+	},
+	{
+		`DESC mytable`,
+		[]sql.Row{
+			{"i", "BIGINT", "NO", "", "", ""},
+			{"s", "TEXT", "NO", "", "", ""},
+		},
+	},
+	{
+		`SHOW COLUMNS FROM one_pk`,
+		[]sql.Row{
+			{"pk", "TINYINT", "NO", "PRI", "", ""},
+			{"c1", "TINYINT", "NO", "", "", ""},
+			{"c2", "TINYINT", "NO", "", "", ""},
+			{"c3", "TINYINT", "NO", "", "", ""},
+			{"c4", "TINYINT", "NO", "", "", ""},
+			{"c5", "TINYINT", "NO", "", "", ""},
+		},
+	},
+	{
+		`DESCRIBE one_pk`,
+		[]sql.Row{
+			{"pk", "TINYINT", "NO", "PRI", "", ""},
+			{"c1", "TINYINT", "NO", "", "", ""},
+			{"c2", "TINYINT", "NO", "", "", ""},
+			{"c3", "TINYINT", "NO", "", "", ""},
+			{"c4", "TINYINT", "NO", "", "", ""},
+			{"c5", "TINYINT", "NO", "", "", ""},
+		},
+	},
+	{
+		`DESC one_pk`,
+		[]sql.Row{
+			{"pk", "TINYINT", "NO", "PRI", "", ""},
+			{"c1", "TINYINT", "NO", "", "", ""},
+			{"c2", "TINYINT", "NO", "", "", ""},
+			{"c3", "TINYINT", "NO", "", "", ""},
+			{"c4", "TINYINT", "NO", "", "", ""},
+			{"c5", "TINYINT", "NO", "", "", ""},
+		},
+	},
+
+	{
 		`SHOW COLUMNS FROM mytable WHERE Field = 'i'`,
 		[]sql.Row{
 			{"i", "BIGINT", "NO", "", "", ""},
@@ -746,6 +794,17 @@ var queries = []queryTest{
 		[]sql.Row{
 			{"i", "BIGINT", nil, "NO", "", "", "", "", ""},
 			{"s", "TEXT", "utf8_bin", "NO", "", "", "", "", ""},
+		},
+	},
+	{
+		`SHOW FULL COLUMNS FROM one_pk`,
+		[]sql.Row{
+			{"pk", "TINYINT", nil, "NO", "PRI", "", "", "", ""},
+			{"c1", "TINYINT", nil, "NO", "", "", "", "", ""},
+			{"c2", "TINYINT", nil, "NO", "", "", "", "", ""},
+			{"c3", "TINYINT", nil, "NO", "", "", "", "", ""},
+			{"c4", "TINYINT", nil, "NO", "", "", "", "", ""},
+			{"c5", "TINYINT", nil, "NO", "", "", "", "", "column 5"},
 		},
 	},
 	{
@@ -3641,7 +3700,7 @@ func allTestTables(t *testing.T, numPartitions int) map[string]*memory.Table {
 		{Name: "c2", Type: sql.Int8, Source: "one_pk"},
 		{Name: "c3", Type: sql.Int8, Source: "one_pk"},
 		{Name: "c4", Type: sql.Int8, Source: "one_pk"},
-		{Name: "c5", Type: sql.Int8, Source: "one_pk"},
+		{Name: "c5", Type: sql.Int8, Source: "one_pk", Comment: "column 5"},
 	}, numPartitions)
 
 	insertRows(t,
