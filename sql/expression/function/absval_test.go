089a100f28317e95148f52a7020dd399d11b8c77
Brian Hendriks <brian@liquidata.co>
2020-01-09 14:31:16 -0800

    absolute value function for SQL (#51)
    * Add ABS function
    Signed-off-by: Brian Hendriks <brian@liquidata.co>

---
+++ sql/expression/function/absval_test.go
@@ -0,0 +1,107 @@
+package function
+
+import (
+	"github.com/shopspring/decimal"
+	"github.com/src-d/go-mysql-server/sql"
+	"github.com/src-d/go-mysql-server/sql/expression"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestAbsValue(t *testing.T) {
+	type toTypeFunc func(float64) interface{}
+
+	decimal1616 := sql.MustCreateDecimalType(16,16)
+
+	toInt64 := func(x float64) interface{} { return int64(x) }
+	toInt32 := func(x float64) interface{} { return int32(x) }
+	toInt := func(x float64) interface{} { return int(x) }
+	toInt16 := func(x float64) interface{} { return int16(x) }
+	toInt8 := func(x float64) interface{} { return int8(x) }
+	toUint64 := func(x float64) interface{} { return uint64(x) }
+	toUint32 := func(x float64) interface{} { return uint32(x) }
+	toUint := func(x float64) interface{} { return uint(x) }
+	toUint16 := func(x float64) interface{} { return uint16(x) }
+	toUint8 := func(x float64) interface{} { return uint8(x) }
+	toFloat64 := func(x float64) interface{} {return x}
+	toFloat32 := func(x float64) interface{} {return float32(x)}
+	toDecimal1616 := func(x float64) interface{} {return decimal.NewFromFloat(x)}
+
+	signedTypes := map[sql.Type]toTypeFunc {
+		sql.Int64: toInt64,
+		sql.Int32: toInt32,
+		sql.Int24: toInt,
+		sql.Int16: toInt16,
+		sql.Int8: toInt8}
+	unsignedTypes := map[sql.Type]toTypeFunc {
+		sql.Uint64: toUint64,
+		sql.Uint32: toUint32,
+		sql.Uint24: toUint,
+		sql.Uint16: toUint16,
+		sql.Uint8: toUint8}
+	floatTypes := map[sql.Type]toTypeFunc{
+		sql.Float64: toFloat64,
+		sql.Float32: toFloat32,
+		decimal1616: toDecimal1616,
+	}
+
+	testCases := []struct {
+		name     string
+		typeToConv    map[sql.Type]toTypeFunc
+		val      float64
+		expected float64
+		err      error
+	}{
+		{
+			"signed types positive int",
+			signedTypes,
+			5.0,
+			5.0,
+			nil,
+		},{
+			"signed types negative int",
+			signedTypes,
+			-5.0,
+			5.0,
+			nil,
+		},
+		{
+			"unsigned types positive int",
+			unsignedTypes,
+			5.0,
+			5.0,
+			nil,
+		},
+		{
+			"float positive int",
+			floatTypes,
+			5.0,
+			5.0,
+			nil,
+		},{
+			"float negative int",
+			floatTypes,
+			-5.0,
+			5.0,
+			nil,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			for sqlType, conv := range test.typeToConv {
+				f := NewAbsVal(expression.NewGetField(0, sqlType, "blob", true))
+
+				row := sql.NewRow(conv(test.val))
+				res, err := f.Eval(sql.NewEmptyContext(), row)
+
+				if test.err == nil {
+					require.NoError(t, err)
+					require.Equal(t, conv(test.expected), res)
+				} else {
+					require.Error(t, err)
+				}
+			}
+		})
+	}
+}
