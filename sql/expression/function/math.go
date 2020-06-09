2bcba03296a8b806d608e3edcf4eab18e622eba4
Brian Hendriks <brian@liquidata.co>
2020-06-08 17:03:29 -0700

    trig functions
    Signed-off-by: Brian Hendriks <brian@liquidata.co>

--- sql/expression/function/math.go
+++ sql/expression/function/math.go
@@ -17,7 +17,13 @@ package function
 import (
 	"fmt"
 	"github.com/liquidata-inc/go-mysql-server/sql"
+	"github.com/liquidata-inc/go-mysql-server/sql/expression"
+	"github.com/shopspring/decimal"
+	"math"
 	"math/rand"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 // Rand returns a random float 0 <= x < 1. If it has an argument, that argument will be used to seed the random number
@@ -105,4 +111,236 @@ func (r *Rand) Eval(ctx *sql.Context, row sql.Row) (interface{}, error) {
 	}
 
 	return rand.New(rand.NewSource(seed)).Float64(), nil
+}
+
+var UintRegex, _ = regexp.Compile("^[1-9][0-9]*$*")
+var IntRegex, _ = regexp.Compile("^-?[1-9][0-9]*$")
+
+type UnaryMathFloatFuncLogic interface {
+	EvalFloat(float64) (interface{}, error)
+}
+
+type UnaryMathFuncLogic interface {
+	UnaryMathFloatFuncLogic
+	EvalUint(uint64) (interface{}, error)
+	EvalInt(int64) (interface{}, error)
+	EvalDecimal(decimal.Decimal) (interface{}, error)
+}
+
+type UnaryMathFloatFuncWrapper struct {
+	FlLogic UnaryMathFloatFuncLogic
+}
+
+func WrapUnaryMathFloatFuncLogic(logic UnaryMathFloatFuncLogic) UnaryMathFuncLogic {
+	return UnaryMathFloatFuncWrapper{logic}
+}
+
+func (wr UnaryMathFloatFuncWrapper) EvalFloat(n float64) (interface{}, error) {
+	return wr.FlLogic.EvalFloat(n)
+}
+
+func (wr UnaryMathFloatFuncWrapper) EvalUint(n uint64) (interface{}, error) {
+	return wr.FlLogic.EvalFloat(float64(n))
+}
+func (wr UnaryMathFloatFuncWrapper) EvalInt(n int64) (interface{}, error) {
+	return wr.FlLogic.EvalFloat(float64(n))
+}
+
+func (wr UnaryMathFloatFuncWrapper) EvalDecimal(dec decimal.Decimal) (interface{}, error) {
+	n, _ := dec.Float64()
+	return wr.FlLogic.EvalFloat(n)
+}
+
+type UnaryMathFunc struct {
+	expression.UnaryExpression
+	Name  string
+	Logic UnaryMathFuncLogic
+}
+
+func NewUnaryMathFunc(name string, logic UnaryMathFuncLogic) func(e sql.Expression) sql.Expression {
+	return func(e sql.Expression) sql.Expression {
+		return &UnaryMathFunc{expression.UnaryExpression{Child: e}, name, logic}
+	}
+}
+
+// Eval implements the Expression interface.
+func (mf *UnaryMathFunc) Eval(ctx *sql.Context, row sql.Row) (interface{}, error) {
+	val, err := mf.Child.Eval(ctx, row)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if val == nil {
+		return nil, nil
+	}
+
+	// Fucking Golang
+	switch x := val.(type) {
+	case uint8:
+		return mf.Logic.EvalUint(uint64(x))
+	case uint16:
+		return mf.Logic.EvalUint(uint64(x))
+	case uint32:
+		return mf.Logic.EvalUint(uint64(x))
+	case uint64:
+		return mf.Logic.EvalUint(x)
+	case uint:
+		return mf.Logic.EvalUint(uint64(x))
+	case int8:
+		return mf.Logic.EvalInt(int64(x))
+	case int16:
+		return mf.Logic.EvalInt(int64(x))
+	case int32:
+		return mf.Logic.EvalInt(int64(x))
+	case int64:
+		return mf.Logic.EvalInt(x)
+	case int:
+		return mf.Logic.EvalInt(int64(x))
+	case float64:
+		return mf.Logic.EvalFloat(x)
+	case float32:
+		return mf.Logic.EvalFloat(float64(x))
+	case decimal.Decimal:
+		return mf.Logic.EvalDecimal(x)
+	case string:
+		if UintRegex.MatchString(x) {
+			n, err := strconv.ParseUint(x, 10, 64)
+
+			if err != nil {
+				return nil, err
+			}
+
+			return mf.Logic.EvalUint(n)
+		} else if IntRegex.MatchString(x) {
+			n, err := strconv.ParseInt(x, 10, 64)
+
+			if err != nil {
+				return nil, err
+			}
+
+			return mf.Logic.EvalInt(n)
+		} else {
+			n, err := strconv.ParseFloat(x, 64)
+
+			if err != nil {
+				return nil, err
+			}
+
+			return mf.Logic.EvalFloat(n)
+		}
+	}
+
+	return nil, nil
+}
+
+// String implements the Stringer interface.
+func (mf *UnaryMathFunc) String() string {
+	return fmt.Sprintf("%s(%s)", strings.ToUpper(mf.Name), mf .Child.String())
+}
+
+// IsNullable implements the Expression interface.
+func (mf *UnaryMathFunc) IsNullable() bool {
+	return mf.Child.IsNullable()
+}
+
+// WithChildren implements the Expression interface.
+func (mf *UnaryMathFunc) WithChildren(children ...sql.Expression) (sql.Expression, error) {
+	if len(children) != 1 {
+		return nil, sql.ErrInvalidChildrenNumber.New(mf, len(children), 1)
+	}
+
+	return &UnaryMathFunc{expression.UnaryExpression{Child:children[0]}, mf.Name, mf.Logic}, nil
+}
+
+// Type implements the Expression interface.
+func (mf *UnaryMathFunc) Type() sql.Type {
+	return mf.Child.Type()
+}
+
+
+type AbsFuncLogic struct{}
+
+func (fl AbsFuncLogic) EvalUint(n uint64) (interface{}, error) {
+	return n, nil
+}
+func (fl AbsFuncLogic) EvalInt(n int64) (interface{}, error) {
+	if n < 0 {
+		return -n, nil
+	}
+
+	return n, nil
+}
+
+func (fl AbsFuncLogic) EvalFloat(n float64) (interface{}, error) {
+	if n < 0 {
+		return -n, nil
+	}
+
+	return n, nil
+}
+
+func (fl AbsFuncLogic) EvalDecimal(dec decimal.Decimal) (interface{}, error) {
+	return dec.Abs(), nil
+}
+
+
+type SinFuncLogic struct{}
+
+func (fl SinFuncLogic) EvalFloat(n float64) (interface{}, error) {
+	return math.Sin(n), nil
+}
+
+type CosFuncLogic struct{}
+
+func (fl CosFuncLogic) EvalFloat(n float64) (interface{}, error) {
+	return math.Cos(n), nil
+}
+
+
+type TanFuncLogic struct{}
+
+func (fl TanFuncLogic) EvalFloat(n float64) (interface{}, error) {
+	return math.Tan(n), nil
+}
+
+
+type ASinFuncLogic struct{}
+
+func (fl ASinFuncLogic) EvalFloat(n float64) (interface{}, error) {
+	return math.Asin(n), nil
+}
+
+
+type ACosFuncLogic struct{}
+
+func (fl ACosFuncLogic) EvalFloat(n float64) (interface{}, error) {
+	return math.Acos(n), nil
+}
+
+
+type ATanFuncLogic struct{}
+
+func (fl ATanFuncLogic) EvalFloat(n float64) (interface{}, error) {
+	return math.Atan(n), nil
+}
+
+
+type CotFuncLogic struct{}
+
+func (fl CotFuncLogic) EvalFloat(n float64) (interface{}, error) {
+	return 1.0 / math.Tan(n), nil
+}
+
+
+type DegreesFuncLogic struct {}
+
+func (fl DegreesFuncLogic) EvalFloat(n float64) (interface{}, error) {
+	return (n * 180.0) / math.Pi, nil
+}
+
+type RadiansFuncLogic struct {}
+
+func (fl RadiansFuncLogic) EvalFloat(n float64) (interface{}, error) {
+	return (n * math.Pi) / 180.0, nil
 }
--- sql/expression/function/math_test.go
+++ sql/expression/function/math_test.go
@@ -17,8 +17,10 @@ package function
 import (
 	"github.com/liquidata-inc/go-mysql-server/sql"
 	"github.com/liquidata-inc/go-mysql-server/sql/expression"
+	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"math"
 	"testing"
 )
 
@@ -78,4 +80,207 @@ func TestRandWithSeed(t *testing.T) {
 	f642 = f.(float64)
 
 	assert.Equal(t, f64, f642)
+}
+
+func TestAbsValue(t *testing.T) {
+	type toTypeFunc func(float64) interface{}
+
+	decimal1616 := sql.MustCreateDecimalType(16,16)
+
+	toInt64 := func(x float64) interface{} { return int64(x) }
+	toInt32 := func(x float64) interface{} { return int32(x) }
+	toInt := func(x float64) interface{} { return int(x) }
+	toInt16 := func(x float64) interface{} { return int16(x) }
+	toInt8 := func(x float64) interface{} { return int8(x) }
+	toUint64 := func(x float64) interface{} { return uint64(x) }
+	toUint32 := func(x float64) interface{} { return uint32(x) }
+	toUint := func(x float64) interface{} { return uint(x) }
+	toUint16 := func(x float64) interface{} { return uint16(x) }
+	toUint8 := func(x float64) interface{} { return uint8(x) }
+	toFloat64 := func(x float64) interface{} {return x}
+	toFloat32 := func(x float64) interface{} {return float32(x)}
+	toDecimal1616 := func(x float64) interface{} {return decimal.NewFromFloat(x)}
+
+	signedTypes := map[sql.Type]toTypeFunc {
+		sql.Int64: toInt64,
+		sql.Int32: toInt32,
+		sql.Int24: toInt,
+		sql.Int16: toInt16,
+		sql.Int8: toInt8}
+	unsignedTypes := map[sql.Type]toTypeFunc {
+		sql.Uint64: toUint64,
+		sql.Uint32: toUint32,
+		sql.Uint24: toUint,
+		sql.Uint16: toUint16,
+		sql.Uint8: toUint8}
+	floatTypes := map[sql.Type]toTypeFunc{
+		sql.Float64: toFloat64,
+		sql.Float32: toFloat32,
+		decimal1616: toDecimal1616,
+	}
+	expectedConv := map[sql.Type]toTypeFunc{
+		sql.Int64: toInt64,
+		sql.Int32: toInt64,
+		sql.Int24: toInt64,
+		sql.Int16: toInt64,
+		sql.Int8: toInt64,
+		sql.Uint64: toUint64,
+		sql.Uint32: toUint64,
+		sql.Uint24: toUint64,
+		sql.Uint16: toUint64,
+		sql.Uint8: toUint64,
+		sql.Float64: toFloat64,
+		sql.Float32: toFloat64,
+		decimal1616: toDecimal1616,
+	}
+
+	testCases := []struct {
+		name     string
+		typeToConv    map[sql.Type]toTypeFunc
+		val      float64
+		expected float64
+		err      error
+	}{
+		{
+			"signed types positive int",
+			signedTypes,
+			5.0,
+			5.0,
+			nil,
+		},{
+			"signed types negative int",
+			signedTypes,
+			-5.0,
+			5.0,
+			nil,
+		},
+		{
+			"unsigned types positive int",
+			unsignedTypes,
+			5.0,
+			5.0,
+			nil,
+		},
+		{
+			"float positive int",
+			floatTypes,
+			5.0,
+			5.0,
+			nil,
+		},{
+			"float negative int",
+			floatTypes,
+			-5.0,
+			5.0,
+			nil,
+		},
+	}
+
+	newAbsVal := NewUnaryMathFunc("abs", AbsFuncLogic{})
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			for sqlType, conv := range test.typeToConv {
+				f := newAbsVal(expression.NewGetField(0, sqlType, "blob", true))
+
+				row := sql.NewRow(conv(test.val))
+				res, err := f.Eval(sql.NewEmptyContext(), row)
+
+				if test.err == nil {
+					require.NoError(t, err)
+					require.Equal(t, expectedConv[sqlType](test.expected), res)
+				} else {
+					require.Error(t, err)
+				}
+			}
+		})
+	}
+}
+
+func TestRadians(t *testing.T) {
+	tests := []struct{
+		name string
+		input interface{}
+		expected float64
+	}{
+		{"int val of 180", int16(180), math.Pi},
+		{"uint val of 90", uint(90), math.Pi / 2.0},
+		{"float value of 360", 360.0, 2*math.Pi},
+	}
+
+	logic := WrapUnaryMathFloatFuncLogic(RadiansFuncLogic{})
+	f := NewUnaryMathFunc("radians", logic)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			radians := f(expression.NewLiteral(test.input, nil))
+			res, err := radians.Eval(nil, nil)
+			assert.NoError(t, err)
+			assert.Equal(t, test.expected, res)
+		})
+	}
+}
+
+func TestDegrees(t *testing.T) {
+	tests := []struct{
+		name string
+		input interface{}
+		expected float64
+	}{
+		{"float32 pi", float32(math.Pi), 180.0},
+		{"decimal 2pi", decimal.NewFromFloat(2*math.Pi), 360.0},
+		{"float6h4 pi/2", math.Pi/2.0, 90.0},
+	}
+
+	logic := WrapUnaryMathFloatFuncLogic(DegreesFuncLogic{})
+	f := NewUnaryMathFunc("degrees", logic)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			degrees := f(expression.NewLiteral(test.input, nil))
+			res, err := degrees.Eval(nil, nil)
+			assert.NoError(t, err)
+			assert.Equal(t, test.expected, res)
+		})
+	}
+}
+
+func TestTrigFunctions(t *testing.T) {
+	sin := NewUnaryMathFunc("sin", WrapUnaryMathFloatFuncLogic(SinFuncLogic{}))
+	cos := NewUnaryMathFunc("cos", WrapUnaryMathFloatFuncLogic(CosFuncLogic{}))
+	tan := NewUnaryMathFunc("tan", WrapUnaryMathFloatFuncLogic(TanFuncLogic{}))
+	asin := NewUnaryMathFunc("asin", WrapUnaryMathFloatFuncLogic(ASinFuncLogic{}))
+	acos := NewUnaryMathFunc("acos", WrapUnaryMathFloatFuncLogic(ACosFuncLogic{}))
+	atan := NewUnaryMathFunc("atan", WrapUnaryMathFloatFuncLogic(ATanFuncLogic{}))
+
+	const NUM_CHECKS = 24
+	delta := (2*math.Pi) / float64(NUM_CHECKS)
+	for i := 0; i <= NUM_CHECKS; i++ {
+		theta := delta * float64(i)
+		thetaLiteral := expression.NewLiteral(theta, nil)
+		sinVal, err := sin(thetaLiteral).Eval(nil, nil)
+		assert.NoError(t, err)
+		cosVal, err := cos(thetaLiteral).Eval(nil, nil)
+		assert.NoError(t, err)
+		tanVal, err := tan(thetaLiteral).Eval(nil, nil)
+		assert.NoError(t, err)
+
+		assert.Equal(t, math.Sin(theta), sinVal)
+		assert.Equal(t, math.Cos(theta), cosVal)
+		assert.Equal(t, math.Tan(theta), tanVal)
+
+		sinF, _ := sinVal.(float64)
+		cosF, _ := cosVal.(float64)
+		tanF, _ := tanVal.(float64)
+
+		asinVal, err := asin(expression.NewLiteral(sinF, nil)).Eval(nil, nil)
+		assert.NoError(t, err)
+		acosVal, err := acos(expression.NewLiteral(cosF, nil)).Eval(nil, nil)
+		assert.NoError(t, err)
+		atanVal, err := atan(expression.NewLiteral(tanF, nil)).Eval(nil, nil)
+		assert.NoError(t, err)
+
+		assert.Equal(t, math.Asin(sinF), asinVal)
+		assert.Equal(t, math.Acos(cosF), acosVal)
+		assert.Equal(t, math.Atan(tanF), atanVal)
+	}
 }
