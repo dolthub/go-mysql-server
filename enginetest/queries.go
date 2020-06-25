3cb7e0efa1a685a4819757c4260cc6a22d14cc5f
Zach Musgrave <zach@liquidata.co>
2020-06-25 16:55:04 -0700

    typo
    Signed-off-by: Zach Musgrave <zach@liquidata.co>

--- enginetest/queries.go
+++ enginetest/queries.go
@@ -2642,8 +2642,8 @@ var InfoSchemaQueries = []QueryTest{
 					"  `i` BIGINT NOT NULL,\n" +
 					"  `s` TEXT NOT NULL COMMENT 'column s',\n" +
 					"  PRIMARY KEY (`i`),\n" +
-					"  KEY `mytable_i_s` (`i`,`s`)\n" +
-					"  UNIQUE KEY `mytable_s` (`s`),\n" +
+					"  KEY `mytable_i_s` (`i`,`s`),\n" +
+					"  UNIQUE KEY `mytable_s` (`s`)\n" +
 					") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4"},
 		},
 	},
