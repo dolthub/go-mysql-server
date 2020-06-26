3842ea21943186aca315c6960c410deb9b278b18
Daylon Wilkins <daylon@liquidata.co>
2020-06-26 14:27:10 -0700

    Added format script
    Signed-off-by: Daylon Wilkins <daylon@liquidata.co>

---
+++ format_repo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eo pipefail
+
+paths=`find . -maxdepth 1 -mindepth 1 \( -name gen -prune -o -type d -print -o -type f -name '*.go' -print \)`
+
+goimports -w -local github.com/liquidata-inc/go-mysql-server $paths
+
+bad_files=$(find $paths -name '*.go' | while read f; do
+    if [[ $(awk '/import \(/{flag=1;next}/\)/{flag=0}flag' < $f | egrep -c '$^') -gt 2 ]]; then
+        echo $f
+    fi
+done)
+
+if [ "$bad_files" != "" ]; then
+    for f in $bad_files; do
+        awk '/import \(/{flag=1}/\)/{flag=0}flag&&!/^$/||!flag' < "$f" > "$f.bak"
+        mv "$f.bak" "$f"
+    done
+    goimports -w -local github.com/liquidata-inc/go-mysql-server .
+fi
