3165c8af092e94d5f823c4be0a8c47f39d7e6579
Zach Musgrave <zach@dolthub.com>
2020-10-14 14:43:44 -0700

    Updated query plan tests with new corrected query plans
    Signed-off-by: Zach Musgrave <zach@dolthub.com>

--- enginetest/query_plans.go
+++ enginetest/query_plans.go
@@ -90,11 +90,9 @@ var PlanTests = []QueryPlanTest{
 	{
 		Query: "SELECT * FROM mytable mt INNER JOIN othertable ot ON mt.i = ot.i2 AND mt.i > 2",
 		ExpectedPlan: "IndexedJoin(mt.i = ot.i2)\n" +
-			// TODO: this filter should be removed, handled by the index
-			" ├─ Filter(mt.i > 2)\n" +
-			" │   └─ TableAlias(mt)\n" +
-			" │       └─ Indexed table access on index [mytable.i]\n" +
-			" │           └─ Table(mytable)\n" +
+			" ├─ TableAlias(mt)\n" +
+			" │   └─ Indexed table access on index [mytable.i]\n" +
+			" │       └─ Table(mytable)\n" +
 			" └─ TableAlias(ot)\n" +
 			"     └─ Table(othertable)\n" +
 			"",
@@ -533,12 +531,12 @@ var PlanTests = []QueryPlanTest{
 		ExpectedPlan: "Sort(t1.pk ASC, t2.pk2 ASC)\n" +
 			" └─ Project(t1.pk, t2.pk2)\n" +
 			"     └─ CrossJoin\n" +
-			"         ├─ Filter(t1.pk = 1)\n" +
-			"         │   └─ TableAlias(t1)\n" +
-			"         │       └─ Indexed table access on index [one_pk.pk]\n" +
-			"         │           └─ Table(one_pk)\n" +
+			"         ├─ TableAlias(t1)\n" +
+			"         │   └─ Indexed table access on index [one_pk.pk]\n" +
+			"         │       └─ Table(one_pk)\n" +
 			"         └─ Filter(t2.pk2 = 1)\n" +
 			"             └─ TableAlias(t2)\n" +
-			"                 └─ Table(two_pk)\n",
+			"                 └─ Table(two_pk)\n" +
+			"",
 	},
 }
