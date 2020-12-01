948c8cd65d776df6b922fab751e7252c3ef4a1b3
Zach Musgrave <zach@dolthub.com>
2020-12-01 10:23:02 -0800

    Removed offending parts of UnaryFunction that don't allow function expressions to be comparable with reflect.DeepEquals, required by the analyzer
    Signed-off-by: Zach Musgrave <zach@dolthub.com>

--- sql/expression/function/function.go
+++ sql/expression/function/function.go
@@ -8,28 +8,12 @@ import (
 	"github.com/dolthub/go-mysql-server/sql/expression"
 )
 
-type UnaryFuncLogic func(*sql.Context, interface{}) (interface{}, error)
-
 type UnaryFunc struct {
 	expression.UnaryExpression
 	// Name is the name of the function
 	Name string
 	// The type returned by the function
 	RetType sql.Type
-	// Logic contains the logic being executed when the function is called
-	Logic UnaryFuncLogic
-}
-
-var _ sql.FunctionExpression = (*UnaryFunc)(nil)
-
-// NewUnaryFunc returns a function which is called to create a sql.Expression representing the function and its
-// argemunts
-func NewUnaryFunc(name string, retType sql.Type, logic UnaryFuncLogic) sql.Function1 {
-	fn := func(e sql.Expression) sql.Expression {
-		return &UnaryFunc{expression.UnaryExpression{Child: e}, name, retType, logic}
-	}
-
-	return sql.Function1{Name: name, Fn: fn}
 }
 
 // FunctionName implements sql.FunctionExpression
@@ -38,7 +22,7 @@ func (uf *UnaryFunc) FunctionName() string {
 }
 
 // Eval implements the Expression interface.
-func (uf *UnaryFunc) Eval(ctx *sql.Context, row sql.Row) (interface{}, error) {
+func (uf *UnaryFunc) EvalChild(ctx *sql.Context, row sql.Row) (interface{}, error) {
 	if uf.Child == nil {
 		return nil, nil
 	}
@@ -49,30 +33,12 @@ func (uf *UnaryFunc) Eval(ctx *sql.Context, row sql.Row) (interface{}, error) {
 		return nil, err
 	}
 
-	if val == nil {
-		return nil, nil
-	}
-
-	return uf.Logic(ctx, val)
+	return val, nil
 }
 
 // String implements the fmt.Stringer interface.
 func (uf *UnaryFunc) String() string {
 	return fmt.Sprintf("%s(%s)", strings.ToUpper(uf.Name), uf.Child.String())
-}
-
-// IsNullable implements the Expression interface.
-func (uf *UnaryFunc) IsNullable() bool {
-	return uf.Child.IsNullable()
-}
-
-// WithChildren implements the Expression interface.
-func (uf *UnaryFunc) WithChildren(children ...sql.Expression) (sql.Expression, error) {
-	if len(children) != 1 {
-		return nil, sql.ErrInvalidChildrenNumber.New(uf, len(children), 1)
-	}
-
-	return &UnaryFunc{expression.UnaryExpression{Child: children[0]}, uf.Name, uf.RetType, uf.Logic}, nil
 }
 
 // Type implements the Expression interface.
