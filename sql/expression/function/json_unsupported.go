08f2bbff9e1d2073cd378e6fe6d530ec3faa8a79
Andy Arthur <andy@dolthub.com>
2021-03-04 17:16:03 -0800

    added stubs for JSON table and utility functions

--- sql/expression/function/json_unsupported.go
+++ sql/expression/function/json_unsupported.go
@@ -619,3 +619,179 @@ func NewJSONValid(args ...sql.Expression) (sql.Expression, error) {
 func (j JSONValid) FunctionName() string {
 	return "json_valid"
 }
+
+
+// JSON_TABLE(expr, path COLUMNS (column_list) [AS] alias)
+//
+// JSONTable Extracts data from a JSON document and returns it as a relational table having the specified columns.
+// TODO(andy): this doc was heavily truncated
+//
+// https://dev.mysql.com/doc/refman/8.0/en/json-table-functions.html#function_json-table
+type JSONTable struct {
+	sql.Expression
+}
+
+var _ sql.FunctionExpression = (JSONTable)(nil)
+
+// NewJSONTable creates a new JSONTable function.
+func NewJSONTable(args ...sql.Expression) (sql.Expression, error) {
+	return nil, ErrUnsupportedJSONFunction.New(JSONTable{}.FunctionName())
+}
+
+// FunctionName implements sql.FunctionExpression
+func (j JSONTable) FunctionName() string {
+	return "json_table"
+}
+
+
+// JSON_SCHEMA_VALID(schema,document)
+//
+// JSONSchemaValid Validates a JSON document against a JSON schema. Both schema and document are required. The schema
+// must be a valid JSON object; the document must be a valid JSON document. Provided that these conditions are met: If
+// the document validates against the schema, the function returns true (1); otherwise, it returns false (0).
+// https://dev.mysql.com/doc/refman/8.0/en/json-validation-functions.html#function_json-schema-valid
+type JSONSchemaValid struct {
+	sql.Expression
+}
+
+var _ sql.FunctionExpression = (JSONSchemaValid)(nil)
+
+// NewJSONSchemaValid creates a new JSONSchemaValid function.
+func NewJSONSchemaValid(args ...sql.Expression) (sql.Expression, error) {
+	return nil, ErrUnsupportedJSONFunction.New(JSONSchemaValid{}.FunctionName())
+}
+
+// FunctionName implements sql.FunctionExpression
+func (j JSONSchemaValid) FunctionName() string {
+	return "json_schema_valid"
+}
+
+
+
+// JSON_SCHEMA_VALIDATION_REPORT(schema,document)
+//
+// JSONSchemaValidationReport Validates a JSON document against a JSON schema. Both schema and document are required.
+// As with JSONSchemaValid, the schema must be a valid JSON object, and the document must be a valid JSON document.
+// Provided that these conditions are met, the function returns a report, as a JSON document, on the outcome of the
+// validation. If the JSON document is considered valid according to the JSON Schema, the function returns a JSON object
+// with one property valid having the value "true". If the JSON document fails validation, the function returns a JSON
+// object which includes the properties listed here:
+//   - valid: Always "false" for a failed schema validation
+//   - reason: A human-readable string containing the reason for the failure
+//   - schema-location: A JSON pointer URI fragment identifier indicating where in the JSON schema the validation failed
+//     (see Note following this list)
+//   - document-location: A JSON pointer URI fragment identifier indicating where in the JSON document the validation
+//     failed (see Note following this list)
+//   - schema-failed-keyword: A string containing the name of the keyword or property in the JSON schema that was
+//     violated
+//
+// https://dev.mysql.com/doc/refman/8.0/en/json-validation-functions.html#function_json-schema-validation-report
+type JSONSchemaValidationReport struct {
+	sql.Expression
+}
+
+var _ sql.FunctionExpression = (JSONSchemaValidationReport)(nil)
+
+// NewJSONSchemaValidationReport creates a new JSONSchemaValidationReport function.
+func NewJSONSchemaValidationReport(args ...sql.Expression) (sql.Expression, error) {
+	return nil, ErrUnsupportedJSONFunction.New(JSONSchemaValidationReport{}.FunctionName())
+}
+
+// FunctionName implements sql.FunctionExpression
+func (j JSONSchemaValidationReport) FunctionName() string {
+	return "json_schema_validation_report"
+}
+
+
+
+// JSON_PRETTY(json_val)
+//
+// JSONPretty Provides pretty-printing of JSON values similar to that implemented in PHP and by other languages and
+// database systems. The value supplied must be a JSON value or a valid string representation of a JSON value.
+// Extraneous whitespaces and newlines present in this value have no effect on the output. For a NULL value, the
+// function returns NULL. If the value is not a JSON document, or if it cannot be parsed as one, the function fails
+// with an error. Formatting of the output from this function adheres to the following rules:
+//   - Each array element or object member appears on a separate line, indented by one additional level as compared to
+//     its parent.
+//   - Each level of indentation adds two leading spaces.
+//   - A comma separating individual array elements or object members is printed before the newline that separates the
+//     two elements or members.
+//   - The key and the value of an object member are separated by a colon followed by a space (': ').
+//   - An empty object or array is printed on a single line. No space is printed between the opening and closing brace.
+//   - Special characters in string scalars and key names are escaped employing the same rules used by JSONQuote.
+//
+// https://dev.mysql.com/doc/refman/8.0/en/json-utility-functions.html#function_json-pretty
+type JSONPretty struct {
+	sql.Expression
+}
+
+var _ sql.FunctionExpression = (JSONPretty)(nil)
+
+// NewJSONPretty creates a new JSONPretty function.
+func NewJSONPretty(args ...sql.Expression) (sql.Expression, error) {
+	return nil, ErrUnsupportedJSONFunction.New(JSONPretty{}.FunctionName())
+}
+
+// FunctionName implements sql.FunctionExpression
+func (j JSONPretty) FunctionName() string {
+	return "json_pretty"
+}
+
+
+
+// JSON_STORAGE_FREE(json_val)
+//
+// JSONStorageFree For a JSON column value, this function shows how much storage space was freed in its binary
+// representation after it was updated in place using JSON_SET(), JSON_REPLACE(), or JSON_REMOVE(). The argument can
+// also be a valid JSON document or a string which can be parsed as one—either as a literal value or as the value of a
+// user variable—in which case the function returns 0. It returns a positive, nonzero value if the argument is a JSON
+// column value which has been updated as described previously, such that its binary representation takes up less space
+// than it did prior to the update. For a JSON column which has been updated such that its binary representation is the
+// same as or larger than before, or if the update was not able to take advantage of a partial update, it returns 0; it
+// returns NULL if the argument is NULL. If json_val is not NULL, and neither is a valid JSON document nor can be
+// successfully parsed as one, an error results.
+//
+// https://dev.mysql.com/doc/refman/8.0/en/json-utility-functions.html#function_json-storage-size
+type JSONStorageFree struct {
+	sql.Expression
+}
+
+var _ sql.FunctionExpression = (JSONStorageFree)(nil)
+
+// NewJSONStorageFree creates a new JSONStorageFree function.
+func NewJSONStorageFree(args ...sql.Expression) (sql.Expression, error) {
+	return nil, ErrUnsupportedJSONFunction.New(JSONStorageFree{}.FunctionName())
+}
+
+// FunctionName implements sql.FunctionExpression
+func (j JSONStorageFree) FunctionName() string {
+	return "json_storage_free"
+}
+
+
+// JSON_STORAGE_SIZE(json_val)
+//
+// JSONStorageSize This function returns the number of bytes used to store the binary representation of a JSON document.
+// When the argument is a JSON column, this is the space used to store the JSON document as it was inserted into the
+// column, prior to any partial updates that may have been performed on it afterwards. json_val must be a valid JSON
+// document or a string which can be parsed as one. In the case where it is string, the function returns the amount of
+// storage space in the JSON binary representation that is created by parsing the string as JSON and converting it to
+// binary. It returns NULL if the argument is NULL. An error results when json_val is not NULL, and is not—or cannot be
+// successfully parsed as—a JSON document.
+//
+// https://dev.mysql.com/doc/refman/8.0/en/json-utility-functions.html#function_json-storage-size
+type JSONStorageSize struct {
+	sql.Expression
+}
+
+var _ sql.FunctionExpression = (JSONStorageSize)(nil)
+
+// NewJSONStorageSize creates a new JSONStorageSize function.
+func NewJSONStorageSize(args ...sql.Expression) (sql.Expression, error) {
+	return nil, ErrUnsupportedJSONFunction.New(JSONStorageSize{}.FunctionName())
+}
+
+// FunctionName implements sql.FunctionExpression
+func (j JSONStorageSize) FunctionName() string {
+	return "json_storage_size"
+}
