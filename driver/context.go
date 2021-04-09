7db2fc590783fa61bb4ddf1a0fbe55240174cad7
Ethan Reesor <ethan.reesor@gmail.com>
2021-04-08 22:25:45 -0500

    [driver] Lint

--- driver/context.go
+++ driver/context.go
@@ -6,12 +6,15 @@ import (
 	"github.com/dolthub/go-mysql-server/sql"
 )
 
+// A ContextBuilder creates SQL contexts.
 type ContextBuilder interface {
 	NewContext(context.Context, *Conn, ...sql.ContextOption) (*sql.Context, error)
 }
 
+// DefaultContextBuilder creates basic SQL contexts.
 type DefaultContextBuilder struct{}
 
+// NewContext calls sql.NewContext.
 func (DefaultContextBuilder) NewContext(ctx context.Context, conn *Conn, opts ...sql.ContextOption) (*sql.Context, error) {
 	return sql.NewContext(ctx, opts...), nil
 }
