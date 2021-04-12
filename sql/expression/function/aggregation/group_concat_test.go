c9acdb5998c101488e6cb54f9eaaa31390367b40
Vinai Rachakonda <rachakondavinai@gmail.com>
2021-04-12 16:10:12 -0400

    small nit

--- sql/expression/function/aggregation/group_concat_test.go
+++ sql/expression/function/aggregation/group_concat_test.go
@@ -84,8 +84,8 @@ func TestGroupConcat_ReturnType(t *testing.T) {
 		returnType sql.Type
 		row        sql.Row
 	}{
-		{[]sql.Expression{expression.NewGetField(0, sql.Int64, "int", true)}, 200, sql.MustCreateString(query.Type_VARCHAR, 512, sql.Collation_Default), sql.Row{int64(1)}},
-		{[]sql.Expression{expression.NewGetField(0, sql.Int64, "int", true)}, 1020, sql.Text, sql.Row{int64(1)}},
+		{[]sql.Expression{expression.NewGetField(0, sql.LongText, "test", true)}, 200, sql.MustCreateString(query.Type_VARCHAR, 512, sql.Collation_Default), sql.Row{int64(1)}},
+		{[]sql.Expression{expression.NewGetField(0, sql.Text, "text", true)}, 1020, sql.Text, sql.Row{int64(1)}},
 		{[]sql.Expression{expression.NewGetField(0, sql.Blob, "myblob", true)}, 200, sql.MustCreateString(query.Type_VARBINARY, 512, sql.Collation_binary), sql.Row{"hi"}},
 		{[]sql.Expression{expression.NewGetField(0, sql.Blob, "myblob", true)}, 1020, sql.Blob, sql.Row{"hi"}},
 	}
