98c469d86a7bd099f878bddfccaa82e768c9b641
Daylon Wilkins <daylon@liquidata.co>
2021-08-26 17:01:18 -0700

    Added function to parse column type strings to SQL types

--- sql/parse/parse_test.go
+++ sql/parse/parse_test.go
@@ -3274,3 +3274,171 @@ func TestPrintTree(t *testing.T) {
                  └─ UnresolvedTable(bar)
 `, node.String())
 }
+
+func TestParseColumnTypeString(t *testing.T) {
+	tests := []struct {
+		columnType      string
+		expectedSqlType sql.Type
+	}{
+		{
+			"tinyint",
+			sql.Int8,
+		},
+		{
+			"SMALLINT",
+			sql.Int16,
+		},
+		{
+			"MeDiUmInT",
+			sql.Int24,
+		},
+		{
+			"INT",
+			sql.Int32,
+		},
+		{
+			"BIGINT",
+			sql.Int64,
+		},
+		{
+			"TINYINT UNSIGNED",
+			sql.Uint8,
+		},
+		{
+			"SMALLINT UNSIGNED",
+			sql.Uint16,
+		},
+		{
+			"MEDIUMINT UNSIGNED",
+			sql.Uint24,
+		},
+		{
+			"INT UNSIGNED",
+			sql.Uint32,
+		},
+		{
+			"BIGINT UNSIGNED",
+			sql.Uint64,
+		},
+		{
+			"BOOLEAN",
+			sql.Int8,
+		},
+		{
+			"FLOAT",
+			sql.Float32,
+		},
+		{
+			"DOUBLE",
+			sql.Float64,
+		},
+		{
+			"REAL",
+			sql.Float64,
+		},
+		{
+			"DECIMAL",
+			sql.MustCreateDecimalType(10, 0),
+		},
+		{
+			"DECIMAL(22)",
+			sql.MustCreateDecimalType(22, 0),
+		},
+		{
+			"DECIMAL(55, 13)",
+			sql.MustCreateDecimalType(55, 13),
+		},
+		{
+			"DEC(34, 2)",
+			sql.MustCreateDecimalType(34, 2),
+		},
+		{
+			"FIXED(4, 4)",
+			sql.MustCreateDecimalType(4, 4),
+		},
+		{
+			"BIT(31)",
+			sql.MustCreateBitType(31),
+		},
+		{
+			"TINYBLOB",
+			sql.TinyBlob,
+		},
+		{
+			"BLOB",
+			sql.Blob,
+		},
+		{
+			"MEDIUMBLOB",
+			sql.MediumBlob,
+		},
+		{
+			"LONGBLOB",
+			sql.LongBlob,
+		},
+		{
+			"TINYTEXT",
+			sql.TinyText,
+		},
+		{
+			"TEXT",
+			sql.Text,
+		},
+		{
+			"MEDIUMTEXT",
+			sql.MediumText,
+		},
+		{
+			"LONGTEXT",
+			sql.LongText,
+		},
+		{
+			"CHAR(5)",
+			sql.MustCreateStringWithDefaults(sqltypes.Char, 5),
+		},
+		{
+			"VARCHAR(255)",
+			sql.MustCreateStringWithDefaults(sqltypes.VarChar, 255),
+		},
+		{
+			"VARCHAR(300) COLLATE cp1257_lithuanian_ci",
+			sql.MustCreateString(sqltypes.VarChar, 300, sql.Collation_cp1257_lithuanian_ci),
+		},
+		{
+			"BINARY(6)",
+			sql.MustCreateBinary(sqltypes.Binary, 6),
+		},
+		{
+			"VARBINARY(256)",
+			sql.MustCreateBinary(sqltypes.VarBinary, 256),
+		},
+		{
+			"YEAR",
+			sql.Year,
+		},
+		{
+			"DATE",
+			sql.Date,
+		},
+		{
+			"TIME",
+			sql.Time,
+		},
+		{
+			"TIMESTAMP",
+			sql.Timestamp,
+		},
+		{
+			"DATETIME",
+			sql.Datetime,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.columnType, func(t *testing.T) {
+			res, err := ParseColumnTypeString(sql.NewEmptyContext(), test.columnType)
+			require.NoError(t, err)
+			require.Equal(t, test.expectedSqlType, res)
+		})
+	}
+}
