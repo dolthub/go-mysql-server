b81cb907c2f5acbe9b36d376f9e7637a22261f11
reltuk <reltuk@users.noreply.github.com>
2021-09-17 21:17:40 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/plan/indexed_table_access.go
+++ sql/plan/indexed_table_access.go
@@ -118,7 +118,7 @@ func formatIndexDecoratorString(idx sql.Index) string {
 
 func (i *IndexedTableAccess) DebugString() string {
 	if i.lookup != nil {
-		return fmt.Sprintf("IndexedTableAccess(%s on %s, using fields %s)", i.Name(), formatIndexDecoratorString(i.index), "STATIC LOOKUP(" + sql.DebugString(i.lookup) + ")")
+		return fmt.Sprintf("IndexedTableAccess(%s on %s, using fields %s)", i.Name(), formatIndexDecoratorString(i.index), "STATIC LOOKUP("+sql.DebugString(i.lookup)+")")
 	}
 	keyExprs := make([]string, len(i.keyExprs))
 	for j := range i.keyExprs {
