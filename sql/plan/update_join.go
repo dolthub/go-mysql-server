c9361557d25e2722fc06994d89ce268e4bb45232
Vinai Rachakonda <rachakondavinai@gmail.com>
2021-10-08 14:10:05 -0700

    comment the update join class

--- sql/plan/update_join.go
+++ sql/plan/update_join.go
@@ -1,3 +1,17 @@
+// Copyright 2020-2021 Dolthub, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package plan
 
 import (
@@ -5,24 +19,26 @@ import (
 )
 
 type UpdateJoin struct {
-	editors map[string]sql.RowUpdater
+	updaters map[string]sql.RowUpdater
 	UnaryNode
 }
 
-// NewUpdateJoin R
+// NewUpdateJoin returns an *UpdateJoin node.
 func NewUpdateJoin(editorMap map[string]sql.RowUpdater, child sql.Node) *UpdateJoin {
 	return &UpdateJoin{
-		editors:   editorMap,
+		updaters:  editorMap,
 		UnaryNode: UnaryNode{Child: child},
 	}
 }
 
 var _ sql.Node = (*UpdateJoin)(nil)
 
+// Resolved implements the sql.Node interface.
 func (u *UpdateJoin) Resolved() bool {
 	return true
 }
 
+// String implements the sql.Node interface.
 func (u *UpdateJoin) String() string {
 	pr := sql.NewTreePrinter()
 	_ = pr.WriteNode("Update Join")
@@ -31,94 +47,111 @@ func (u *UpdateJoin) String() string {
 
 }
 
+// Schema implements the sql.Node interface.
 func (u *UpdateJoin) Schema() sql.Schema {
 	return u.Child.Schema()
 }
 
+// Children implements the sql.Node interface.
 func (u *UpdateJoin) Children() []sql.Node {
 	return []sql.Node{u.Child}
 }
 
+// RowIter implements the sql.Node interface.
 func (u *UpdateJoin) RowIter(ctx *sql.Context, row sql.Row) (sql.RowIter, error) {
 	return u.Child.RowIter(ctx, row)
 }
 
+// GetUpdatable returns an updateJoinTable which implements sql.UpdatableTable.
 func (u *UpdateJoin) GetUpdatable() sql.UpdatableTable {
 	return &updatableJoinTable{
-		editors:  u.editors,
+		updaters: u.updaters,
 		joinNode: u.Child.(*UpdateSource).Child,
 	}
 }
 
+// WithChildren implements the sql.Node interface.
 func (u *UpdateJoin) WithChildren(children ...sql.Node) (sql.Node, error) {
 	if len(children) != 1 {
 		return nil, sql.ErrInvalidChildrenNumber.New(u, len(children), 1)
 	}
 
-	return NewUpdateJoin(u.editors, children[0]), nil
+	return NewUpdateJoin(u.updaters, children[0]), nil
 }
 
-// Manges the editing of a table
+// updatableJoinTable manages the update of multiple tables.
 type updatableJoinTable struct {
-	editors  map[string]sql.RowUpdater
+	updaters map[string]sql.RowUpdater
 	joinNode sql.Node
 }
 
 var _ sql.UpdatableTable = (*updatableJoinTable)(nil)
 
+// Partitions implements the sql.UpdatableTable interface.
 func (u updatableJoinTable) Partitions(context *sql.Context) (sql.PartitionIter, error) {
 	panic("this method should not be called")
 }
 
+// PartitionsRows implements the sql.UpdatableTable interface.
 func (u updatableJoinTable) PartitionRows(context *sql.Context, partition sql.Partition) (sql.RowIter, error) {
 	panic("this method should not be called")
 }
 
+// Name implements the sql.UpdatableTable interface.
 func (u updatableJoinTable) Name() string {
 	panic("this method should not be called")
 }
 
+// String implements the sql.UpdatableTable interface.
 func (u updatableJoinTable) String() string {
 	panic("this method should not be called")
 }
 
+// Schema implements the sql.UpdatableTable interface.
 func (u updatableJoinTable) Schema() sql.Schema {
-	return u.joinNode.Schema() // just return the schema of the join row for now. This will cause for additional updates but could be better ig?
+	return u.joinNode.Schema()
 }
 
+// Updater implements the sql.UpdatableTable interface.
 func (u updatableJoinTable) Updater(ctx *sql.Context) sql.RowUpdater {
 	return &updatableJoinUpdater{
-		initialEditorMap: u.editors,
-		updatedEditorMap: u.editors,
-		joinSchema:       u.joinNode.Schema(),
+		initialUpdaterMap: u.updaters,
+		updatedUpdaterMap: u.updaters,
+		joinSchema:        u.joinNode.Schema(),
 	}
 }
 
+// updatableJoinUpdater manages the process of taking a join row and allocating the respective updates to each updatable
+// table.
 type updatableJoinUpdater struct {
-	initialEditorMap map[string]sql.RowUpdater
-	updatedEditorMap map[string]sql.RowUpdater
-	joinSchema       sql.Schema
+	initialUpdaterMap map[string]sql.RowUpdater
+	updatedUpdaterMap map[string]sql.RowUpdater
+	joinSchema        sql.Schema
 }
 
 var _ sql.RowUpdater = (*updatableJoinUpdater)(nil)
 
+// StatementBegin implements the sql.TableEditor interface.
 func (u updatableJoinUpdater) StatementBegin(ctx *sql.Context) {}
 
+// DiscardChanges implements the sql.TableEditor interface.
 func (u updatableJoinUpdater) DiscardChanges(ctx *sql.Context, errorEncountered error) error {
-	u.updatedEditorMap = u.initialEditorMap
+	u.updatedUpdaterMap = u.initialUpdaterMap
 	return nil
 }
 
+// StatementComplete implements the sql.TableEditor interface.
 func (u updatableJoinUpdater) StatementComplete(ctx *sql.Context) error {
-	u.initialEditorMap = u.updatedEditorMap
+	u.initialUpdaterMap = u.updatedUpdaterMap
 	return nil
 }
 
+// Update implements the sql.RowUpdater interface.
 func (u updatableJoinUpdater) Update(ctx *sql.Context, old sql.Row, new sql.Row) error {
 	tableToOldRowMap := splitRowIntoTableRowMap(old, u.joinSchema)
 	tableToNewRowMap := splitRowIntoTableRowMap(new, u.joinSchema)
 
-	for tableName, updater := range u.updatedEditorMap {
+	for tableName, updater := range u.updatedUpdaterMap {
 		oldRow := tableToOldRowMap[tableName]
 		newRow := tableToNewRowMap[tableName]
 
@@ -131,8 +164,9 @@ func (u updatableJoinUpdater) Update(ctx *sql.Context, old sql.Row, new sql.Row)
 	return nil
 }
 
+// Close implements the sql.RowUpdater interface.
 func (u updatableJoinUpdater) Close(ctx *sql.Context) error {
-	for _, updater := range u.updatedEditorMap {
+	for _, updater := range u.updatedUpdaterMap {
 		err := updater.Close(ctx)
 		if err != nil {
 			return err
