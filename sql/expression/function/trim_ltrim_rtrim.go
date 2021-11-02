f22194173843b50c922aadc25bf672b0d7286598
James Cor <james@dolthub.com>
2021-11-02 15:16:46 -0700

    updated tests for whitespace case to match mysql

--- sql/expression/function/trim_ltrim_rtrim.go
+++ sql/expression/function/trim_ltrim_rtrim.go
@@ -18,7 +18,6 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
-	"unicode"
 
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/expression"
@@ -105,7 +104,7 @@ func (t *Trim) Eval(ctx *sql.Context, row sql.Row) (interface{}, error) {
 	end := len(str_text)
 	n := len(pat_text)
 
-	if n > end {
+	if n == 0 || n > end {
 		return str_text, nil
 	}
 
@@ -194,7 +193,9 @@ func (t *LeftTrim) Eval(ctx *sql.Context, row sql.Row) (interface{}, error) {
 		return nil, sql.ErrInvalidType.New(reflect.TypeOf(str))
 	}
 
-	return strings.TrimLeftFunc(str.(string), unicode.IsSpace), nil
+	return strings.TrimLeftFunc(str.(string), func(r rune) bool {
+		return r == ' '
+	}), nil
 }
 
 type RightTrim struct {
@@ -243,122 +244,7 @@ func (t *RightTrim) Eval(ctx *sql.Context, row sql.Row) (interface{}, error) {
 		return nil, sql.ErrInvalidType.New(reflect.TypeOf(str))
 	}
 
-	return strings.TrimRightFunc(str.(string), unicode.IsSpace), nil
+	return strings.TrimRightFunc(str.(string), func(r rune) bool {
+		return r == ' '
+	}), nil
 }
-
-/*
-package function
-
-import (
-	"fmt"
-	"reflect"
-	"strings"
-	"unicode"
-
-	"github.com/dolthub/go-mysql-server/sql"
-	"github.com/dolthub/go-mysql-server/sql/expression"
-)
-
-type trimType rune
-
-const (
-	lTrimType trimType = 'l'
-	rTrimType trimType = 'r'
-	bTrimType trimType = 'b'
-)
-
-func NewLeftTrim(e sql.Expression) sql.Expression {
-	return newTrim(lTrimType, e)
-}
-
-func NewRightTrim(e sql.Expression) sql.Expression {
-	return newTrim(rTrimType, e)
-}
-
-func NewTrim(e sql.Expression) sql.Expression {
-	return newTrim(bTrimType, e)
-}
-
-// newTrim creates a new Trim expression.
-func newTrim(tType trimType, str sql.Expression) sql.Expression {
-	return &Trim{expression.UnaryExpression{Child: str}, tType}
-}
-
-// Trim is a function that returns the string with prefix or suffix spaces removed based on the trimType
-type Trim struct {
-	expression.UnaryExpression
-	trimType
-}
-
-var _ sql.FunctionExpression = (*Trim)(nil)
-
-// FunctionName implements sql.FunctionExpression
-func (t *Trim) FunctionName() string {
-	switch t.trimType {
-	case lTrimType:
-		return "ltrim"
-	case rTrimType:
-		return "rtrim"
-	case bTrimType:
-		return "trim"
-	default:
-		panic("unknown name for trim type")
-	}
-}
-
-// Type implements the Expression interface.
-func (t *Trim) Type() sql.Type { return sql.LongText }
-
-func (t *Trim) String() string {
-	switch t.trimType {
-	case lTrimType:
-		return fmt.Sprintf("ltrim(%s)", t.Child)
-	case rTrimType:
-		return fmt.Sprintf("rtrim(%s)", t.Child)
-	default:
-		return fmt.Sprintf("trim(%s)", t.Child)
-	}
-}
-
-// IsNullable implements the Expression interface.
-func (t *Trim) IsNullable() bool {
-	return t.Child.IsNullable()
-}
-
-// WithChildren implements the Expression interface.
-func (t *Trim) WithChildren(children ...sql.Expression) (sql.Expression, error) {
-	if len(children) != 1 {
-		return nil, sql.ErrInvalidChildrenNumber.New(t, len(children), 1)
-	}
-	return newTrim(t.trimType, children[0]), nil
-}
-
-// Eval implements the Expression interface.
-func (t *Trim) Eval(
-	ctx *sql.Context,
-	row sql.Row,
-) (interface{}, error) {
-	str, err := t.Child.Eval(ctx, row)
-	if err != nil {
-		return nil, err
-	}
-
-	if str == nil {
-		return nil, nil
-	}
-
-	str, err = sql.LongText.Convert(str)
-	if err != nil {
-		return nil, sql.ErrInvalidType.New(reflect.TypeOf(str))
-	}
-
-	switch t.trimType {
-	case lTrimType:
-		return strings.TrimLeftFunc(str.(string), unicode.IsSpace), nil
-	case rTrimType:
-		return strings.TrimRightFunc(str.(string), unicode.IsSpace), nil
-	default:
-		return strings.TrimFunc(str.(string), unicode.IsSpace), nil
-	}
-}
-*/
