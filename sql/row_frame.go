fd62f4968c40298d5dd6a138a37e91d8e4d0db3f
Andy Arthur <andy@dolthub.com>
2021-12-10 11:30:40 -0800

    added more detail

--- sql/row_frame.go
+++ sql/row_frame.go
@@ -15,31 +15,86 @@
 package sql
 
 import (
+	"sync"
+
 	querypb "github.com/dolthub/vitess/go/vt/proto/query"
 )
 
 const (
-	valueBackingArraySize = 64
-	fieldBackingArraySize = 2048
+	valueArrSize = 64
+	fieldArrSize = 2048
 )
 
 // Row2 is a tuple of values.
 type Row2 []Value
 
-type Value struct { // 28 bytes
+type Value struct {
 	Typ querypb.Type
 	Val []byte
 }
 
-type RowFrame struct { // 3866 bytes
+type RowFrame struct {
 
 	// Values are the values this row.
-	// If possible |vba| is used as the backing array
-	// for Values. Additionaly, |fba| is used as the
-	// backing array for each Value, if possible.
 	Values []Value
 
-	vba [valueBackingArraySize]Value
-	fba [fieldBackingArraySize]byte
+	// varr is used as the backing array for the |Values|
+	// slice when len(Values) <= valueArrSize
+	varr [valueArrSize]Value
+
+	// farr is used as the backing array for |Value.Val|
+	// slices when there is capacity
+	farr [fieldArrSize]byte
+
+	// off tracks the next available position in |farr|
 	off uint16
+}
+
+func NewRowFrame(vals ...Value) (f *RowFrame) {
+	f = framePool.Get().(*RowFrame)
+	f.Append(vals...)
+	return
+}
+
+var framePool = sync.Pool{New: makeRowFrame}
+
+func makeRowFrame() interface{} {
+	return &RowFrame{}
+}
+
+func (f *RowFrame) Row2() Row2 {
+	return f.Values
+}
+
+func (f *RowFrame) Append(vals ...Value) {
+	for _, v := range vals {
+		f.append(v)
+	}
+}
+
+func (f *RowFrame) append(v Value) {
+	buf := f.getBuffer(v)
+	copy(buf, v.Val)
+	v.Val = buf
+
+	// if |f.Values| grows past |len(f.varr)|
+	// we'll allocate a new backing array here
+	f.Values = append(f.Values, v)
+}
+
+func (f *RowFrame) getBuffer(v Value) (buf []byte) {
+	if f.checkCapacity(v) {
+		start := f.off
+		f.off += uint16(len(v.Val))
+		stop := f.off
+		buf = f.farr[start:stop]
+	} else {
+		buf = make([]byte, len(v.Val))
+	}
+
+	return
+}
+
+func (f *RowFrame) checkCapacity(v Value) bool {
+	return len(v.Val) <= (len(f.farr) - int(f.off))
 }
