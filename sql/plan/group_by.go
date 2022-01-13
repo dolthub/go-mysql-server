8b8c5e108c3148085418fa4691b461267e0ccaf5
Max Hoffman <max@dolthub.com>
2022-01-13 11:40:52 -0800

    only group by no agg path

--- sql/plan/group_by.go
+++ sql/plan/group_by.go
@@ -16,8 +16,10 @@ package plan
 
 import (
 	"fmt"
+	"io"
 	"strings"
 
+	"github.com/cespare/xxhash"
 	opentracing "github.com/opentracing/opentracing-go"
 	errors "gopkg.in/src-d/go-errors.v1"
 
@@ -95,24 +97,29 @@ func (g *GroupBy) RowIter(ctx *sql.Context, row sql.Row) (sql.RowIter, error) {
 		return nil, err
 	}
 
-	aggs := make([]*aggregation.Aggregation, len(g.SelectedExprs))
-	for i, e := range g.SelectedExprs {
-		switch a := e.(type) {
-		case sql.WindowAdaptableExpression:
-			fn, err := a.NewWindowFunction()
-			if err != nil {
-				return nil, err
+	var iter sql.RowIter
+	if len(g.GroupByExprs) == 0 {
+		iter = newGroupByIter(g.SelectedExprs, i)
+	} else {
+		aggs := make([]*aggregation.Aggregation, len(g.SelectedExprs))
+		for i, e := range g.SelectedExprs {
+			switch a := e.(type) {
+			case sql.WindowAdaptableExpression:
+				fn, err := a.NewWindowFunction()
+				if err != nil {
+					return nil, err
+				}
+				aggs[i] = aggregation.NewAggregation(fn, aggregation.NewGroupByFramer())
+			default:
+				fn, err := aggregation.NewLast(a).NewWindowFunction()
+				if err != nil {
+					return nil, err
+				}
+				aggs[i] = aggregation.NewAggregation(fn, aggregation.NewGroupByFramer())
 			}
-			aggs[i] = aggregation.NewAggregation(fn, aggregation.NewGroupByFramer())
-		default:
-			fn, err := aggregation.NewLast(a).NewWindowFunction()
-			if err != nil {
-				return nil, err
-			}
-			aggs[i] = aggregation.NewAggregation(fn, aggregation.NewGroupByFramer())
 		}
+		iter = aggregation.NewWindowBlockIter(g.GroupByExprs, nil, aggs, i)
 	}
-	iter := aggregation.NewWindowBlockIter(g.GroupByExprs, nil, aggs, i)
 
 	return sql.NewSpanIter(span, iter), nil
 }
@@ -192,4 +199,125 @@ func (g *GroupBy) Expressions() []sql.Expression {
 	exprs = append(exprs, g.SelectedExprs...)
 	exprs = append(exprs, g.GroupByExprs...)
 	return exprs
+}
+
+type groupByIter struct {
+	selectedExprs []sql.Expression
+	child         sql.RowIter
+	ctx           *sql.Context
+	buf           []sql.AggregationBuffer
+	done          bool
+}
+
+func newGroupByIter(selectedExprs []sql.Expression, child sql.RowIter) *groupByIter {
+	return &groupByIter{
+		selectedExprs: selectedExprs,
+		child:         child,
+		buf:           make([]sql.AggregationBuffer, len(selectedExprs)),
+	}
+}
+
+func (i *groupByIter) Next(ctx *sql.Context) (sql.Row, error) {
+	if i.done {
+		return nil, io.EOF
+	}
+
+	i.done = true
+
+	var err error
+	for j, a := range i.selectedExprs {
+		i.buf[j], err = newAggregationBuffer(a)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for {
+		row, err := i.child.Next(ctx)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+
+		if err := updateBuffers(ctx, i.buf, row); err != nil {
+			return nil, err
+		}
+	}
+
+	return evalBuffers(ctx, i.buf)
+}
+
+func (i *groupByIter) Close(ctx *sql.Context) error {
+	i.Dispose()
+	i.buf = nil
+	return i.child.Close(ctx)
+}
+
+func (i *groupByIter) Dispose() {
+	for _, b := range i.buf {
+		b.Dispose()
+	}
+}
+
+func groupingKey(
+	ctx *sql.Context,
+	exprs []sql.Expression,
+	row sql.Row,
+) (uint64, error) {
+	hash := xxhash.New()
+	for _, expr := range exprs {
+		v, err := expr.Eval(ctx, row)
+		if err != nil {
+			return 0, err
+		}
+		_, err = hash.Write(([]byte)(fmt.Sprintf("%#v,", v)))
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return hash.Sum64(), nil
+}
+
+func newAggregationBuffer(expr sql.Expression) (sql.AggregationBuffer, error) {
+	switch n := expr.(type) {
+	case sql.Aggregation:
+		return n.NewBuffer()
+	default:
+		// The semantics for a non-aggregation in a group by node is Last.
+		return aggregation.NewLast(expr).NewBuffer()
+	}
+}
+
+func updateBuffers(
+	ctx *sql.Context,
+	buffers []sql.AggregationBuffer,
+	row sql.Row,
+) error {
+	for _, b := range buffers {
+		if err := b.Update(ctx, row); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func evalBuffers(
+	ctx *sql.Context,
+	buffers []sql.AggregationBuffer,
+) (sql.Row, error) {
+	var row = make(sql.Row, len(buffers))
+
+	var err error
+	for i, b := range buffers {
+		row[i], err = b.Eval(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return row, nil
 }
