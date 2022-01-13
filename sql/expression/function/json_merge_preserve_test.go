c958634170827b65fd5788b6b255385578e72d62
jennifersp <jspurevsuren@gmail.com>
2022-01-13 12:13:32 -0800

    WIP

--- sql/expression/function/json_merge_preserve_test.go
+++ sql/expression/function/json_merge_preserve_test.go
@@ -18,7 +18,6 @@ import (
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/expression"
 	"github.com/stretchr/testify/require"
-	"strings"
 	"testing"
 )
 
@@ -52,6 +51,28 @@ func TestJSONMergePreserve(t *testing.T) {
 	jsonObj4 := map[string]interface{}{"a": 3, "c": 4}
 	jsonObj5 := map[string]interface{}{"a": 5, "d": 6}
 	json3ObjResult := map[string]interface{}{"a": []interface{}{1, 3, 5}, "b": 2, "c": 4, "d": 6}
+	sData1 := map[string]interface{}{
+		"Suspect": map[string]interface{}{
+			"Name": "Bart",
+			"Hobbies": []interface{}{"Skateboarding", "Mischief"},
+		},
+	}
+	sData2 := map[string]interface{}{
+		"Suspect": map[string]interface{}{
+		"Age": 10,
+		"Parents": []interface{}{"Marge","Homer"},
+		"Hobbies": []interface{}{"Trouble"},
+		},
+	}
+	resultData := map[string]interface{}{
+		"Suspect": map[string]interface{}{
+			"Age": 10,
+			"Name": "Bart",
+			"Hobbies": []interface{}{"Skateboarding", "Mischief", "Trouble"},
+			"Parents": []interface{}{"Marge","Homer"},
+		},
+	}
+
 
 	testCases := []struct {
 		f        sql.Expression
@@ -59,27 +80,22 @@ func TestJSONMergePreserve(t *testing.T) {
 		expected interface{}
 		err      error
 	}{
-		{f2, sql.Row{jsonArray1}, sql.JSONDocument{Val: nil}, sql.ErrInvalidArgumentNumber.New("JSON_MERGE_PRESERVE", 2, 1)},
 		{f2, sql.Row{jsonArray1, jsonArray2}, sql.JSONDocument{Val: []interface{}{1, 2, true, false}}, nil},
 		{f2, sql.Row{jsonObj1, jsonObj2}, sql.JSONDocument{Val: map[string]interface{}{"name": "x" , "id": 47}}, nil},
 		{f2, sql.Row{1, true}, sql.JSONDocument{Val: []interface{}{1, true}}, nil},
 		{f2, sql.Row{jsonArray1, jsonObj2}, sql.JSONDocument{Val: []interface{}{1, 2, map[string]interface{}{"id": 47}}}, nil},
 		{f3, sql.Row{jsonObj3, jsonObj4, jsonObj5}, sql.JSONDocument{Val: json3ObjResult}, nil},
+		{f2, sql.Row{sData1, sData2}, sql.JSONDocument{Val: resultData}, nil},
 	}
 
 	for _, tt := range testCases {
-		var paths []string
-		for _, path := range tt.row[1:] {
-			paths = append(paths, path.(string))
-		}
-
-		t.Run(tt.f.String()+"."+strings.Join(paths, ","), func(t *testing.T) {
+		t.Run(tt.f.String(), func(t *testing.T) {
 			require := require.New(t)
 			result, err := tt.f.Eval(sql.NewEmptyContext(), tt.row)
 			if tt.err == nil {
 				require.NoError(err)
 			} else {
-				require.Error(tt.err, err)
+				require.Equal(err.Error(), tt.err.Error())
 			}
 
 			require.Equal(tt.expected, result)
