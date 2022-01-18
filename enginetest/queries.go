3cc8f183f22fc13681bc6a46034f19ce9a4ee59f
jennifersp <44716627+jennifersp@users.noreply.github.com>
2022-01-18 12:08:10 -0800

    add JSON_MERGE_PRESERVE() function (#733)

--- enginetest/queries.go
+++ enginetest/queries.go
@@ -593,6 +593,29 @@ var QueryTests = []QueryTest{
 		},
 	},
 	{
+		Query: `SELECT JSON_MERGE_PRESERVE('{ "a": 1, "b": 2 }','{ "a": 3, "c": 4 }','{ "a": 5, "d": 6 }')`,
+		Expected: []sql.Row{
+			{sql.MustJSON(`{"a": [1, 3, 5], "b": 2, "c": 4, "d": 6}`)},
+		},
+	},
+	{
+		Query: `SELECT JSON_MERGE_PRESERVE(val1, val2) 
+                    FROM (values
+						 row('{ "a": 1, "b": 2 }','null'), 
+                         row('{ "a": 1, "b": 2 }','"row one"'), 
+                         row('{ "a": 3, "c": 4 }','4'), 
+                         row('{ "a": 5, "d": 6 }','[true, true]'),
+                         row('{ "a": 5, "d": 6 }','{ "a": 3, "e": 2 }'))
+                    test (val1, val2)`,
+		Expected: []sql.Row{
+			{sql.MustJSON(`[{ "a": 1, "b": 2 }, null]`)},
+			{sql.MustJSON(`[{ "a": 1, "b": 2 }, "row one"]`)},
+			{sql.MustJSON(`[{ "a": 3, "c": 4 }, 4]`)},
+			{sql.MustJSON(`[{ "a": 5, "d": 6 }, true, true]`)},
+			{sql.MustJSON(`{ "a": [5, 3], "d": 6, "e": 2}`)},
+		},
+	},
+	{
 		Query: `SELECT column_0, sum(column_1) FROM 
 			(values row(1,1), row(1,3), row(2,2), row(2,5), row(3,9)) a 
 			group by 1 order by 1`,
