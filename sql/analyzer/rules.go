b188cbbc6cfc2d83d25e27271191eadee59e8f7e
Zach Musgrave <zach@dolthub.com>
2022-02-07 17:00:59 -0800

    Reverted change to move transaction logic into analyzer: we need to begin a transaction before analysis

--- sql/analyzer/rules.go
+++ sql/analyzer/rules.go
@@ -116,7 +116,6 @@ var OnceAfterDefault = []Rule{
 // OnceAfterAll contains the rules to be applied just once after all other
 // rules have been applied.
 var OnceAfterAll = []Rule{
-	{"wrap_in_transaction", wrapInTransaction},
 	{"track_process", trackProcess},
 	{"parallelize", parallelize},
 	{"apply_row_frame_toggle", applyRowFrameToggle},
