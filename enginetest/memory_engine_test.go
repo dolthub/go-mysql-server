9e2d699985dd52f638258fa2aa2c7ff070136c06
Andy Arthur <andy@dolthub.com>
2022-03-03 09:47:13 -0800

    pr feedback

--- enginetest/memory_engine_test.go
+++ enginetest/memory_engine_test.go
@@ -258,6 +258,7 @@ func TestIndexQueryPlans(t *testing.T) {
 // query plan results. Handy when you've made a large change to the analyzer or node formatting, and you want to examine
 // how query plans have changed without a lot of manual copying and pasting.
 func TestWriteQueryPlans(t *testing.T) {
+	t.Skip()
 
 	harness := enginetest.NewDefaultMemoryHarness()
 	engine := enginetest.NewEngine(t, harness)
@@ -313,6 +314,7 @@ func TestWriteQueryPlans(t *testing.T) {
 }
 
 func TestWriteIndexQueryPlans(t *testing.T) {
+	t.Skip()
 
 	harness := enginetest.NewDefaultMemoryHarness()
 	engine := enginetest.NewEngine(t, harness)
