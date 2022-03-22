b6b34ae3ed70c1f6f40bbe7d5ce7ce838b1801d8
jennifersp <jspurevsuren@gmail.com>
2022-03-22 09:55:24 -0700

    add test

--- enginetest/enginetests.go
+++ enginetest/enginetests.go
@@ -226,6 +226,21 @@ func TestIndexQueryPlans(t *testing.T, harness Harness) {
 			TestQueryPlan(t, NewContextWithEngine(harness, engine), engine, harness, tt.Query, tt.ExpectedPlan)
 		})
 	}
+
+	t.Run("no database selected", func(t *testing.T) {
+		ctx := NewContext(harness)
+		ctx.SetCurrentDatabase("")
+
+		RunQuery(t, engine, harness, "CREATE DATABASE otherdb")
+		RunQuery(t, engine, harness, `CREATE TABLE otherdb.a (x int, y int)`)
+		RunQuery(t, engine, harness, `CREATE INDEX idx1 ON otherdb.a (y);`)
+		//otherdb, err := engine.Analyzer.Catalog.Database(ctx, "otherdb")
+
+		TestQueryWithContext(t, ctx, engine, "SHOW INDEXES FROM otherdb.a", []sql.Row{
+			{"a", 1, "idx1", 1, "y", nil, 0, nil, nil, "YES", "BTREE", "", "", "YES", nil},
+		}, nil, nil)
+
+	})
 }
 
 // Tests a variety of queries against databases and tables provided by the given harness.
