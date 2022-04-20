d12eab3a47c9c0842ab207d00a476a57aafff05a
Max Hoffman <max@dolthub.com>
2022-04-20 13:16:54 -0700

    Arithmetic .Type() Perf Improvement
    SQL Logictests were timing out on huge CASE statements with ~40
    nodes. This fixes the computational blowup, which I think was
    somehow exponential before.

--- enginetest/queries.go
+++ enginetest/queries.go
@@ -5975,6 +5975,10 @@ var QueryTests = []QueryTest{
 		Expected: []sql.Row{{int64(1)}, {int64(2)}, {int64(3)}},
 	},
 	{
+		Query:    `SELECT ALL - - 20 * - CASE + AVG ( ALL + + 89 ) WHEN - 66 THEN NULL WHEN - 15 THEN 38 * COUNT( * ) * MIN( DISTINCT - + 88 ) - MIN( ALL + 0 ) - - COUNT( * ) + - 0 + - 14 * + ( 98 ) * + 70 * 14 * + 57 * 48 - 53 + + 7 END * + 78 + - 11 * + 29 + + + 46 + + 10 + + ( - 83 ) * - - 74 / - 8 + 18`,
+		Expected: []sql.Row{},
+	},
+	{
 		Query:    "SELECT 1/0 FROM dual",
 		Expected: []sql.Row{{sql.Null}},
 	},
