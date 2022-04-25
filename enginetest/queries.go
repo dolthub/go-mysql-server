1ab603f16a9de23e7bb20ee97a2ff6f180d80272
jennifersp <jspurevsuren@gmail.com>
2022-04-25 14:41:49 -0700

    add error test

--- enginetest/queries.go
+++ enginetest/queries.go
@@ -8613,6 +8613,11 @@ var errorQueries = []QueryErrorTest{
 						LEFT JOIN mytable as t15 ON t14.i = t15.i`,
 		ExpectedErr: sql.ErrUnsupportedJoinFactorCount,
 	},
+	// this query should be allowed, but should return error when this procedure is called
+	{
+		Query:       `CREATE PROCEDURE proc1 (OUT out_count INT) READS SQL DATA SELECT COUNT(*) FROM mytable WHERE i = 1 AND s = 'first row' AND func1(i);`,
+		ExpectedErr: sql.ErrFunctionNotFound,
+	},
 }
 
 // WriteQueryTest is a query test for INSERT, UPDATE, etc. statements. It has a query to run and a select query to
