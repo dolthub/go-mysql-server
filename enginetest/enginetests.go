45b7375e660208d43baeb3198ec9581fb14a6944
JCOR11599 <JCOR11599@users.noreply.github.com>
2022-05-18 20:51:38 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- enginetest/enginetests.go
+++ enginetest/enginetests.go
@@ -17,7 +17,6 @@ package enginetest
 import (
 	"context"
 	"fmt"
-	"github.com/dolthub/go-mysql-server/sql/mysql_db/serial"
 	"net"
 	"strings"
 	"sync/atomic"
@@ -40,6 +39,7 @@ import (
 	"github.com/dolthub/go-mysql-server/sql/expression/function/aggregation"
 	"github.com/dolthub/go-mysql-server/sql/information_schema"
 	"github.com/dolthub/go-mysql-server/sql/mysql_db"
+	"github.com/dolthub/go-mysql-server/sql/mysql_db/serial"
 	"github.com/dolthub/go-mysql-server/sql/parse"
 	"github.com/dolthub/go-mysql-server/sql/plan"
 	"github.com/dolthub/go-mysql-server/sql/transform"
