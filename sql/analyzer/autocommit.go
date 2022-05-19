cca445fef1e514c0984f7846a9d8cb31a9fdb8e8
fulghum <fulghum@users.noreply.github.com>
2022-05-19 22:38:47 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/analyzer/autocommit.go
+++ sql/analyzer/autocommit.go
@@ -15,10 +15,11 @@
 package analyzer
 
 import (
+	"strings"
+
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/plan"
 	"github.com/dolthub/go-mysql-server/sql/transform"
-	"strings"
 )
 
 // addAutocommitNode wraps each query with a TransactionCommittingNode.
