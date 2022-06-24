195e6e878d7978c4d82e202e50e3f3f1e6b89d46
James Cor <james@dolthub.com>
2022-06-24 14:28:29 -0700

    oops

--- memory/table.go
+++ memory/table.go
@@ -279,7 +279,7 @@ func (t *Table) AnalyzeTable(ctx *sql.Context) error {
 		createdAt: time.Now(),
 	}
 
-	histMap, err := sql.NewHistogramMapBuilder(t).Build(ctx)
+	histMap, err := sql.NewHistogramMapFromTable(ctx, t)
 	if err != nil {
 		return err
 	}
