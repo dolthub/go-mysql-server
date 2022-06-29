7c0b943f9265fe93eb58c0ee79de842cf71caa9a
Vinai Rachakonda <rachakondavinai@gmail.com>
2022-06-29 12:56:54 -0700

    nit for show procedure status

--- sql/parse/parse.go
+++ sql/parse/parse.go
@@ -541,7 +541,7 @@ func convertShow(ctx *sql.Context, s *sqlparser.Show, query string) (sql.Node, e
 
 		node, err := Parse(ctx, "select routine_schema as `Db`, routine_name as `Name`, routine_type as `Type`,"+
 			"definer as `Definer`, last_altered as `Modified`, created as `Created`, security_type as `Security_type`,"+
-			"routine_comment as `Comment`, character_set_client, collation_connection,"+
+			"routine_comment as `Comment`, CHARACTER_SET_CLIENT as `character_set_client`, COLLATION_CONNECTION as `collation_connection`,"+
 			"database_collation as `Database Collation` from information_schema.routines where routine_type = 'PROCEDURE'")
 		if err != nil {
 			return nil, err
