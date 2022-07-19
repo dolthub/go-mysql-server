a3b51b413fca1d67b6141acc119b774f4d6e2757
James Cor <james@dolthub.com>
2022-07-19 12:43:03 -0700

    ability to recover from panics

--- sql/mysql_db/mysql_db.go
+++ sql/mysql_db/mysql_db.go
@@ -129,7 +129,7 @@ func (db *MySQLDb) LoadPrivilegeData(ctx *sql.Context, users []*User, roleConnec
 
 // LoadData adds the given data to the MySQL Tables. It does not remove any current data, but will overwrite any
 // pre-existing data.
-func (db *MySQLDb) LoadData(ctx *sql.Context, buf []byte) error {
+func (db *MySQLDb) LoadData(ctx *sql.Context, buf []byte) (err error) {
 	// Do nothing if data file doesn't exist or is empty
 	if buf == nil || len(buf) == 0 {
 		return nil
@@ -140,7 +140,7 @@ func (db *MySQLDb) LoadData(ctx *sql.Context, buf []byte) error {
 		Roles []*RoleEdge
 	}
 
-	// if it's a json file, convert it to flatbuffer
+	// if it's a json file, read it; will be rewritten as flatbuffer later
 	data := &privDataJson{}
 	if err := json.Unmarshal(buf, data); err == nil {
 		return db.LoadPrivilegeData(ctx, data.Users, data.Roles)
@@ -148,6 +148,13 @@ func (db *MySQLDb) LoadData(ctx *sql.Context, buf []byte) error {
 
 	// Indicate that mysql db exists
 	db.Enabled = true
+
+	// Recover from panics
+	defer func() {
+		if recover() != nil {
+			err = fmt.Errorf("ill formatted privileges file")
+		}
+	}()
 
 	// Deserialize the flatbuffer
 	serialMySQLDb := serial.GetRootAsMySQLDb(buf, 0)
@@ -179,7 +186,7 @@ func (db *MySQLDb) LoadData(ctx *sql.Context, buf []byte) error {
 	db.clearCache()
 
 	// TODO: fill in other tables when they exist
-	return nil
+	return
 }
 
 // SetPersister sets the custom persister to be used when the MySQL Db tables have been updated and need to be persisted.
