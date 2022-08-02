090cc22217933f8678239c0e463254fc0c4299f6
Max Hoffman <max@dolthub.com>
2022-08-02 15:24:00 -0700

    delete stale error

--- sql/analyzer/indexes.go
+++ sql/analyzer/indexes.go
@@ -15,14 +15,10 @@
 package analyzer
 
 import (
-	errors "gopkg.in/src-d/go-errors.v1"
-
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/expression"
 	"github.com/dolthub/go-mysql-server/sql/plan"
 )
-
-var errInvalidInRightEvaluation = errors.NewKind("expecting evaluation of IN expression right hand side to be a tuple, but it is %T")
 
 // indexLookup contains an sql.IndexLookup and all sql.Index that are involved
 // in it.
