5da84b47a5ac53bef28ba49b9420729074934bdf
Jason Fulghum <jason@dolthub.com>
2022-08-10 09:29:05 -0700

    Pulling out an ExternalStoredProcedureDirectory utility class to remove the duplicated logic in ExternalStoredProcedureProvider implementations.

--- memory/external_sp_db.go
+++ memory/external_sp_db.go
@@ -101,78 +101,31 @@ var (
 
 // ExternalStoredProcedureProvider is an implementation of sql.ExternalStoredProcedureProvider for the memory db.
 type ExternalStoredProcedureProvider struct {
-	procedures map[string]map[int]sql.ExternalStoredProcedureDetails
+	procedureDirectory ExternalStoredProcedureDirectory
 }
 
 var _ sql.ExternalStoredProcedureProvider = (*ExternalStoredProcedureProvider)(nil)
 
 // NewExternalStoredProcedureProvider returns a new ExternalStoredProcedureProvider.
 func NewExternalStoredProcedureProvider() ExternalStoredProcedureProvider {
-	procedures := make(map[string]map[int]sql.ExternalStoredProcedureDetails)
+	procedureDirectory := NewExternalStoredProcedureDirectory()
 	for _, esp := range externalStoredProcedures {
-		numOfParams := countNumberOfParams(esp)
-
-		if _, ok := procedures[esp.Name]; !ok {
-			procedures[esp.Name] = make(map[int]sql.ExternalStoredProcedureDetails)
-		}
-		procedures[esp.Name][numOfParams] = esp
+		procedureDirectory.Register(esp)
 	}
 
 	return ExternalStoredProcedureProvider{
-		procedures: procedures,
+		procedureDirectory: procedureDirectory,
 	}
 }
 
-func countNumberOfParams(externalProcedure sql.ExternalStoredProcedureDetails) int {
-	funcVal := reflect.ValueOf(externalProcedure.Function)
-	funcType := funcVal.Type()
-
-	// Return MaxInt for variadic types, since they can accommodate any number of params
-	if funcVal.Type().IsVariadic() {
-		return math.MaxInt
-	}
-
-	// We subtract one because ctx is required to always be the first parameter to a function, but
-	// customers won't actually pass that in to the stored procedure.
-	// TODO: Should we keep the ctx check in here?
-	return funcType.NumIn() - 1
-}
-
+// ExternalStoredProcedure implements the sql.ExternalStoredProcedureProvider interface
 func (e ExternalStoredProcedureProvider) ExternalStoredProcedure(_ *sql.Context, name string, numOfParams int) (*sql.ExternalStoredProcedureDetails, error) {
-	procedureVariants, ok := e.procedures[strings.ToLower(name)]
-	if !ok {
-		return nil, sql.ErrStoredProcedureDoesNotExist.New(name)
-	}
-
-	// If we find an exact match, go ahead and return that stored procedure
-	procedure, ok := procedureVariants[numOfParams]
-	if ok {
-		return &procedure, nil
-	}
-
-	// Otherwise, find the largest param length and return that stored procedure
-	var largestParamLen int
-	var largestParamProc *sql.ExternalStoredProcedureDetails
-	for paramLen, procedure := range procedureVariants {
-		if largestParamProc == nil || largestParamLen < paramLen {
-			largestParamProc = &procedure
-			largestParamLen = paramLen
-		}
-	}
-	return largestParamProc, nil
+	return e.procedureDirectory.LookupByNameAndParamCount(name, numOfParams)
 }
 
-func (e ExternalStoredProcedureProvider) ExternalStoredProcedures(ctx *sql.Context, name string) ([]sql.ExternalStoredProcedureDetails, error) {
-	procedureVariants, ok := e.procedures[strings.ToLower(name)]
-	if !ok {
-		return nil, sql.ErrStoredProcedureDoesNotExist.New(name)
-	}
-
-	procedures := make([]sql.ExternalStoredProcedureDetails, 0, len(procedureVariants))
-	for _, procedure := range procedureVariants {
-		procedures = append(procedures, procedure)
-	}
-	return procedures, nil
+// ExternalStoredProcedures implements the sql.ExternalStoredProcedureProvider interface
+func (e ExternalStoredProcedureProvider) ExternalStoredProcedures(_ *sql.Context, name string) ([]sql.ExternalStoredProcedureDetails, error) {
+	return e.procedureDirectory.LookupByName(name)
 }
 
 func inout_add(_ *sql.Context, a *int64, b int64) (sql.RowIter, error) {
@@ -191,7 +144,7 @@ func overloaded_mult3(_ *sql.Context, a int8, b int32, c int64) (sql.RowIter, er
 }
 
 func overloaded_type_test1(
-	ctx *sql.Context,
+	_ *sql.Context,
 	aa int8, ab int16, ac int, ad int32, ae int64, af float32, ag float64,
 	ba *int8, bb *int16, bc *int, bd *int32, be *int64, bf *float32, bg *float64,
 ) (sql.RowIter, error) {
@@ -249,4 +202,88 @@ func variadic_overload1(_ *sql.Context, a string, b string) (sql.RowIter, error)
 
 func variadic_overload2(_ *sql.Context, a string, b string, vals ...uint8) (sql.RowIter, error) {
 	return sql.RowsToRowIter(sql.Row{fmt.Sprintf("%s,%s,%v", a, b, vals)}), nil
+}
+
+// ExternalStoredProcedureDirectory manages a collection of ExternalStoredProcedures and encapsulates
+// the logic for looking up external stored procedures based on name and number of arguments.
+type ExternalStoredProcedureDirectory struct {
+	procedures map[string]map[int]sql.ExternalStoredProcedureDetails
+}
+
+// NewExternalStoredProcedureDirectory creates a new, empty instance of ExternalStoredProcedureDirectory.
+func NewExternalStoredProcedureDirectory() ExternalStoredProcedureDirectory {
+	return ExternalStoredProcedureDirectory{
+		procedures: make(map[string]map[int]sql.ExternalStoredProcedureDetails),
+	}
+}
+
+// Register adds an external stored procedure to this directory.
+func (epd *ExternalStoredProcedureDirectory) Register(procedureDetails sql.ExternalStoredProcedureDetails) {
+	numOfParams := epd.countNumberOfParams(procedureDetails)
+
+	if _, ok := epd.procedures[procedureDetails.Name]; !ok {
+		epd.procedures[procedureDetails.Name] = make(map[int]sql.ExternalStoredProcedureDetails)
+	}
+	epd.procedures[procedureDetails.Name][numOfParams] = procedureDetails
+}
+
+// LookupByName returns all stored procedure variants registered with the specified name, no matter
+// how many parameters they require. If no external stored procedures are registered with the specified
+// name, an ErrStoredProcedureDoesNotExist error is returned.
+func (epd *ExternalStoredProcedureDirectory) LookupByName(name string) ([]sql.ExternalStoredProcedureDetails, error) {
+	procedureVariants, ok := epd.procedures[strings.ToLower(name)]
+	if !ok {
+		return nil, sql.ErrStoredProcedureDoesNotExist.New(name)
+	}
+
+	procedures := make([]sql.ExternalStoredProcedureDetails, 0, len(procedureVariants))
+	for _, procedure := range procedureVariants {
+		procedures = append(procedures, procedure)
+	}
+	return procedures, nil
+}
+
+// LookupByNameAndParamCount returns the external stored procedure registered with the specified name
+// and able to accept the specified number of parameters. If no external stored procedures are
+// registered with the specified name and able to accept the specified number of parameters, an
+// ErrStoredProcedureDoesNotExist error is returned.
+func (epd *ExternalStoredProcedureDirectory) LookupByNameAndParamCount(name string, numOfParams int) (*sql.ExternalStoredProcedureDetails, error) {
+	procedureVariants, ok := epd.procedures[strings.ToLower(name)]
+	if !ok {
+		return nil, sql.ErrStoredProcedureDoesNotExist.New(name)
+	}
+
+	// If we find an exact match on param count, return that stored procedure
+	procedure, ok := procedureVariants[numOfParams]
+	if ok {
+		return &procedure, nil
+	}
+
+	// Otherwise, find the largest param length and return that stored procedure
+	var largestParamLen int
+	var largestParamProc *sql.ExternalStoredProcedureDetails
+	for paramLen, procedure := range procedureVariants {
+		if largestParamProc == nil || largestParamLen < paramLen {
+			largestParamProc = &procedure
+			largestParamLen = paramLen
+		}
+	}
+	return largestParamProc, nil
+}
+
+// countNumberOfParams returns the number of parameters accepted by the specified external stored
+// procedure, including allowing variadic return types to expand to accept at most MaxInt parameters.
+func (epd *ExternalStoredProcedureDirectory) countNumberOfParams(externalProcedure sql.ExternalStoredProcedureDetails) int {
+	funcVal := reflect.ValueOf(externalProcedure.Function)
+	funcType := funcVal.Type()
+
+	// Return MaxInt for variadic types, since they can accommodate any number of params
+	if funcVal.Type().IsVariadic() {
+		return math.MaxInt
+	}
+
+	// We subtract one because ctx is required to always be the first parameter to a function, but
+	// customers won't actually pass that in to the stored procedure.
+	// TODO: Should we keep the ctx check in here?
+	return funcType.NumIn() - 1
 }
