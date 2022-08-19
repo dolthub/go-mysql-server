c34de83aa8de8397739492594ebbb200121af303
Zach Musgrave <zach@dolthub.com>
2022-08-19 12:49:25 -0700

    PR feedback:

--- enginetest/enginetests.go
+++ enginetest/enginetests.go
@@ -214,8 +214,12 @@ func TestInfoSchema(t *testing.T, h Harness) {
 		TestQueryWithContext(t, ctx, e, h, "SELECT * FROM information_schema.processlist", []sql.Row{{1, "root", "localhost", "NULL", "Query", 0, "processlist(processlist (0/? partitions))", "SELECT foo"}}, nil, nil)
 	})
 
-	t.Skip()
-	RunQueryTests(t, h, queries.SkippedInfoSchemaQueries)
+	for _, tt := range queries.SkippedInfoSchemaQueries {
+		t.Run(tt.Query, func(t *testing.T) {
+			t.Skip()
+			TestQuery(t, h, tt.Query, tt.Expected, tt.ExpectedColumns, nil)
+		})
+	}
 }
 
 func CreateIndexes(t *testing.T, harness Harness, engine *sqle.Engine) {
