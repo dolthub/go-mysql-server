b08ccc19314908bb42becce22c016449b9f81751
xujihui1985 <xujihui1985@gmail.com>
2022-09-09 20:11:41 +0800

    feat:(sql function) support last_insert_id with expr
    mysql support parameter for LAST_INSERT_ID(expr), it is useful in
    upsert query when we want to get the updated recored id
    ref: https://dev.mysql.com/doc/refman/8.0/en/information-functions.html#function_last-insert-id

--- sql/expression/function/queryinfo.go
+++ sql/expression/function/queryinfo.go
@@ -1,6 +1,10 @@
 package function
 
-import "github.com/dolthub/go-mysql-server/sql"
+import (
+	"fmt"
+	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/expression"
+)
 
 // RowCount implements the ROW_COUNT() function
 type RowCount struct{}
@@ -61,14 +65,23 @@ func (r RowCount) FunctionName() string {
 }
 
 // LastInsertId implements the LAST_INSERT_ID() function
-type LastInsertId struct{}
+type LastInsertId struct {
+	expression.UnaryExpression
+}
 
 func (r LastInsertId) IsNonDeterministic() bool {
 	return true
 }
 
-func NewLastInsertId() sql.Expression {
-	return LastInsertId{}
+func NewLastInsertId(children ...sql.Expression) (sql.Expression, error) {
+	switch len(children) {
+	case 0:
+		return LastInsertId{}, nil
+	case 1:
+		return LastInsertId{UnaryExpression: expression.UnaryExpression{Child: children[0]}}, nil
+	default:
+		return nil, sql.ErrInvalidArgumentNumber.New("LastInsertId", len(children), 1)
+	}
 }
 
 var _ sql.FunctionExpression = LastInsertId{}
@@ -85,7 +98,7 @@ func (r LastInsertId) Resolved() bool {
 
 // String implements sql.Expression
 func (r LastInsertId) String() string {
-	return "LAST_INSERT_ID()"
+	return fmt.Sprintf("LAST_INSERT_ID(%s)", r.Child)
 }
 
 // Type implements sql.Expression
@@ -100,17 +113,31 @@ func (r LastInsertId) IsNullable() bool {
 
 // Eval implements sql.Expression
 func (r LastInsertId) Eval(ctx *sql.Context, row sql.Row) (interface{}, error) {
-	return ctx.GetLastQueryInfo(sql.LastInsertId), nil
+	// if no expr like LAST_INSERT_ID(), we should return lastInsertId from ctx
+	if len(r.Children()) == 0 {
+		return ctx.GetLastQueryInfo(sql.LastInsertId), nil
+	}
+	// if expr is provided like LAST_INSERT_ID(id + 1), then the return value is the
+	// value of expr and we also need to save the lastinsertid into ctx for next query
+	res, err := r.Child.Eval(ctx, row)
+	if err != nil {
+		return nil, err
+	}
+	ctx.SetLastQueryInfo(sql.LastInsertId, toInt64(res))
+	return res, nil
 }
 
 // Children implements sql.Expression
 func (r LastInsertId) Children() []sql.Expression {
-	return nil
+	if r.Child == nil {
+		return nil
+	}
+	return []sql.Expression{r.Child}
 }
 
 // WithChildren implements sql.Expression
 func (r LastInsertId) WithChildren(children ...sql.Expression) (sql.Expression, error) {
-	return sql.NillaryWithChildren(r, children...)
+	return NewLastInsertId(children...)
 }
 
 // FunctionName implements sql.FunctionExpression
@@ -174,4 +201,35 @@ func (r FoundRows) Children() []sql.Expression {
 // WithChildren implements sql.Expression
 func (r FoundRows) WithChildren(children ...sql.Expression) (sql.Expression, error) {
 	return sql.NillaryWithChildren(r, children...)
+}
+
+func toInt64(x interface{}) int64 {
+	switch x := x.(type) {
+	case int:
+		return int64(x)
+	case uint:
+		return int64(x)
+	case int8:
+		return int64(x)
+	case uint8:
+		return int64(x)
+	case int16:
+		return int64(x)
+	case uint16:
+		return int64(x)
+	case int32:
+		return int64(x)
+	case uint32:
+		return int64(x)
+	case int64:
+		return x
+	case uint64:
+		return int64(x)
+	case float32:
+		return int64(x)
+	case float64:
+		return int64(x)
+	default:
+		panic(fmt.Sprintf("Expected a numeric auto increment value, but got %T", x))
+	}
 }
