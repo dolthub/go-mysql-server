1858ab158b3ce6003cb81d9071ed87136f58f62a
Jason Fulghum <jason@dolthub.com>
2022-09-12 15:49:21 -0700

    Removing an old debugging test

--- enginetest/memory_engine_test.go
+++ enginetest/memory_engine_test.go
@@ -239,23 +239,6 @@ func TestSingleScript(t *testing.T) {
 						`   HAVING s = 'secon'`,
 					Expected: []sql.Row{{"secon"}},
 				},
-				{
-					Query: `select CONCAT(tbl.table_schema, '.', tbl.table_name) as the_table,
-							col.column_name, GROUP_CONCAT(kcu.column_name SEPARATOR ',') as pk
-							from information_schema.tables as tbl
-							join information_schema.columns as col
-							on tbl.table_name = col.table_name
-							join information_schema.key_column_usage as kcu
-							on tbl.table_name = kcu.table_name
-							join information_schema.table_constraints as tc
-							on kcu.constraint_name = tc.constraint_name
-							where tbl.table_schema = 'mydb' and
-							tbl.table_name = kcu.table_name and
-							tc.constraint_type = 'PRIMARY KEY' and
-							col.column_name like 'pk%'
-							group by the_table, col.column_name`,
-					Expected: []sql.Row{{"mydb.t", "pk", "pk,pk,pk,pk,pk,pk,pk"}},
-				},
 			},
 		},
 	}
