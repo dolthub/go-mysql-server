88455b06ea5c93fa83184675ba257e0eb6568b6f
jennifersp <jennifersp@users.noreply.github.com>
2022-09-12 18:43:09 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/expression/function/aggregation/unary_aggs.og.go
+++ sql/expression/function/aggregation/unary_aggs.og.go
@@ -4,6 +4,7 @@ package aggregation
 
 import (
 	"fmt"
+
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/expression"
 	"github.com/dolthub/go-mysql-server/sql/transform"
