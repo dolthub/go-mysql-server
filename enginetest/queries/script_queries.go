c92e508d7b7920e3be0e66800974cc65c1da2d5d
druvv <druvv@users.noreply.github.com>
2022-11-11 23:44:16 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- enginetest/queries/script_queries.go
+++ enginetest/queries/script_queries.go
@@ -19,9 +19,8 @@ import (
 
 	"gopkg.in/src-d/go-errors.v1"
 
-	"github.com/dolthub/go-mysql-server/sql/analyzer"
-
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/analyzer"
 	"github.com/dolthub/go-mysql-server/sql/plan"
 )
 
