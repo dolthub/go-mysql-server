9f0eecb2ba9341e2cc381c2abd3c1a067fddb830
jennifersp <jennifer@dolthub.com>
2022-11-11 11:36:46 -0800

    systemBoolType is Int8 so it's singedInt

--- sql/type.go
+++ sql/type.go
@@ -626,6 +626,10 @@ func IsNumber(t Type) bool {
 
 // IsSigned checks if t is a signed type.
 func IsSigned(t Type) bool {
+	// systemBoolType is Int8
+	if _, ok := t.(systemBoolType); ok {
+		return true
+	}
 	return t == Int8 || t == Int16 || t == Int24 || t == Int32 || t == Int64
 }
 
