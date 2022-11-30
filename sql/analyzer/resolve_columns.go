e2577ab65eaccd3b239fc045e633a3dbfb0886c7
Max Hoffman <max@dolthub.com>
2022-11-30 10:18:11 -0800

    Fixes issue 4857: insert qualify CTE source bug
    Edge case where we failed to resolve columns in the insert body
    if the source had a certain nested CTE/table alias/column alias structure.

--- sql/analyzer/resolve_columns.go
+++ sql/analyzer/resolve_columns.go
@@ -417,8 +417,16 @@ func getAvailableNamesByScope(n sql.Node, scope *Scope) availableNames {
 			return true
 		})
 		transform.Inspect(in.Source, func(n sql.Node) bool {
-			if resTbl, ok := n.(*plan.ResolvedTable); ok && !aliasedTables[resTbl] {
-				children = append(children, resTbl)
+			switch n := n.(type) {
+			case *plan.ResolvedTable:
+				if !aliasedTables[n] {
+					children = append(children, n)
+				}
+			case *plan.IndexedTableAccess:
+				if !aliasedTables[n] {
+					children = append(children, n.ResolvedTable)
+				}
+			default:
 			}
 			return true
 		})
@@ -920,9 +928,18 @@ func indexColumns(_ *sql.Context, _ *Analyzer, n sql.Node, scope *Scope) (map[ta
 			return true
 		})
 		transform.Inspect(node.Source, func(n sql.Node) bool {
-			if resTbl, ok := n.(*plan.ResolvedTable); ok && !aliasedTables[resTbl] {
-				idx = 0
-				indexSchema(resTbl.Schema())
+			switch n := n.(type) {
+			case *plan.ResolvedTable:
+				if !aliasedTables[n] {
+					idx = 0
+					indexSchema(n.Schema())
+				}
+			case *plan.IndexedTableAccess:
+				if !aliasedTables[n] {
+					idx = 0
+					indexSchema(n.ResolvedTable.Schema())
+				}
+			default:
 			}
 			return true
 		})
