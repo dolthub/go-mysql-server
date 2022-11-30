688af4d02b2af049b176600bd3133bb957c7269c
Zach Musgrave <zach@dolthub.com>
2022-11-30 12:58:22 -0800

    Bug fix for cached results in rCTE

--- sql/plan/join_iters.go
+++ sql/plan/join_iters.go
@@ -133,12 +133,6 @@ func (i *joinIter) Next(ctx *sql.Context) (sql.Row, error) {
 					return i.removeParentRow(row), nil
 				}
 				continue
-			} else if errors.Is(err, ErrEmptyCachedResult) {
-				if !i.foundMatch && i.joinType.IsLeftOuter() {
-					row := i.buildRow(primary, nil)
-					return i.removeParentRow(row), nil
-				}
-				return nil, io.EOF
 			}
 			return nil, err
 		}
