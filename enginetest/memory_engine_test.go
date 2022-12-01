d1d33e6d5da9684b6d5ead045bf51ac6e531c629
James Cor <james@dolthub.com>
2022-12-01 10:29:03 -0800

    remove unused testing code

--- enginetest/memory_engine_test.go
+++ enginetest/memory_engine_test.go
@@ -179,25 +179,26 @@ func TestSingleQueryPrepared(t *testing.T) {
 // Convenience test for debugging a single query. Unskip and set to the desired query.
 func TestSingleScript(t *testing.T) {
 	t.Skip()
+
 	var scripts = []queries.ScriptTest{
 		{
-			Name: "DELETE ME",
+			Name: "create table as select distinct",
 			SetUpScript: []string{
-				"SET GLOBAL sql_mode=(select concat(@@sql_mode, ',ONLY_FULL_GROUP_BY'))",
-				"SET GLOBAL sql_mode=(select replace(@@sql_mode,'ONLY_FULL_GROUP_BY',''));",
+				"CREATE TABLE t1 (a int, b varchar(10));",
+				"insert into t1 values (1, 'a'), (2, 'b'), (2, 'b'), (3, 'c');",
 			},
 			Assertions: []queries.ScriptTestAssertion{
 				{
-					Query:    "SET GLOBAL sql_mode=(select concat(@@sql_mode, ',ONLY_FULL_GROUP_BY'))",
-					Expected: []sql.Row{{}},
+					Query:    "create table t2 as select distinct b, a from t1;",
+					Expected: []sql.Row{{sql.OkResult{RowsAffected: 3}}},
 				},
 				{
-					Query:    "SET GLOBAL sql_mode=(select replace(@@sql_mode,'ONLY_FULL_GROUP_BY',''));",
-					Expected: []sql.Row{{}},
-				},
-				{
-					Query:    "SET GLOBAL sql_mode=(select concat(@@sql_mode, ',ONLY_FULL_GROUP_BY'))",
-					Expected: []sql.Row{{}},
+					Query: "select * from t2 order by a;",
+					Expected: []sql.Row{
+						{"a", 1},
+						{"b", 2},
+						{"c", 3},
+					},
 				},
 			},
 		},
@@ -205,51 +206,15 @@ func TestSingleScript(t *testing.T) {
 
 	for _, test := range scripts {
 		harness := enginetest.NewMemoryHarness("", 1, testNumPartitions, true, nil)
-		//engine, err := harness.NewEngine(t)
-		//if err != nil {
-		//	panic(err)
-		//}
+		engine, err := harness.NewEngine(t)
+		if err != nil {
+			panic(err)
+		}
+		engine.Analyzer.Debug = true
+		engine.Analyzer.Verbose = true
 
-		enginetest.TestScriptPrepared(t, harness, test)
-		//enginetest.TestScriptWithEngine(t, engine, harness, test)
+		enginetest.TestScriptWithEngine(t, engine, harness, test)
 	}
-	//t.Skip()
-	//
-	//var scripts = []queries.ScriptTest{
-	//	{
-	//		Name: "create table as select distinct",
-	//		SetUpScript: []string{
-	//			"CREATE TABLE t1 (a int, b varchar(10));",
-	//			"insert into t1 values (1, 'a'), (2, 'b'), (2, 'b'), (3, 'c');",
-	//		},
-	//		Assertions: []queries.ScriptTestAssertion{
-	//			{
-	//				Query:    "create table t2 as select distinct b, a from t1;",
-	//				Expected: []sql.Row{{sql.OkResult{RowsAffected: 3}}},
-	//			},
-	//			{
-	//				Query: "select * from t2 order by a;",
-	//				Expected: []sql.Row{
-	//					{"a", 1},
-	//					{"b", 2},
-	//					{"c", 3},
-	//				},
-	//			},
-	//		},
-	//	},
-	//}
-	//
-	//for _, test := range scripts {
-	//	harness := enginetest.NewMemoryHarness("", 1, testNumPartitions, true, nil)
-	//	engine, err := harness.NewEngine(t)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	engine.Analyzer.Debug = true
-	//	engine.Analyzer.Verbose = true
-	//
-	//	enginetest.TestScriptWithEngine(t, engine, harness, test)
-	//}
 }
 
 func TestUnbuildableIndex(t *testing.T) {
