b756ee20f7cafb4e9ed30aa84770a117332849dc
James Cor <james@dolthub.com>
2022-12-02 15:46:11 -0800

    figured out better way

--- sql/plan/group_by.go
+++ sql/plan/group_by.go
@@ -241,10 +241,10 @@ func (i *groupByIter) Next(ctx *sql.Context) (sql.Row, error) {
 		if err != nil {
 			return nil, err
 		}
-		// TODO: bad, figure out better way to determine if there are any agg selections that aren't any_value
-		// maybe should be handled in analyzer?
-		if agg, ok := a.(sql.Aggregation); ok && !strings.Contains(agg.String(), "ANYVALUE") {
-			onlyAnyValue = false
+		if agg, ok := a.(sql.Aggregation); ok {
+			if _, ok = agg.(*aggregation.AnyValue); !ok {
+				onlyAnyValue = false
+			}
 		}
 	}
 
@@ -440,7 +440,7 @@ func newAggregationBuffer(expr sql.Expression) (sql.AggregationBuffer, error) {
 		return n.NewBuffer()
 	default:
 		// The semantics for a non-aggregation expression in a group by node is First.
-		// When ONLY_FULL_GROUP_BY is enabled, this is an error, but it's allowed otherwise. 
+		// When ONLY_FULL_GROUP_BY is enabled, this is an error, but it's allowed otherwise.
 		return aggregation.NewFirst(expr).NewBuffer()
 	}
 }
