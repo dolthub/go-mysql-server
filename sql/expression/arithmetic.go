367dcb2f2aefcfe3d95d6e9200d36fc5f601fcde
jennifersp <jennifersp@users.noreply.github.com>
2022-12-06 00:25:31 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/expression/arithmetic.go
+++ sql/expression/arithmetic.go
@@ -64,7 +64,7 @@ var _ ArithmeticOp = (*Arithmetic)(nil)
 // Arithmetic expressions include plus, minus and multiplication (+, -, *) operations.
 type Arithmetic struct {
 	BinaryExpression
-	Op string
+	Op  string
 	ops int32
 }
 
