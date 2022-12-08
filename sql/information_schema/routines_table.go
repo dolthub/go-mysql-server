484d79ffb1aa15fd76be95ef1ff5074d55d1299e
jennifersp <jennifer@dolthub.com>
2022-12-08 10:29:02 -0800

    clean up

--- sql/information_schema/routines_table.go
+++ sql/information_schema/routines_table.go
@@ -104,7 +104,6 @@ func routinesRowIter(ctx *Context, c Catalog, p map[string][]*plan.Procedure) (R
 	var (
 		securityType    string
 		isDeterministic string
-		sqlMode         string
 	)
 
 	characterSetClient, err := ctx.GetSessionVariable(ctx, "character_set_client")
@@ -119,6 +118,9 @@ func routinesRowIter(ctx *Context, c Catalog, p map[string][]*plan.Procedure) (R
 	if err != nil {
 		return nil, err
 	}
+
+	// TODO: get sqlMode
+	//sqlMode, err := ctx.Session.GetSessionVariable(ctx, "sql_mode")
 
 	showExternalProcedures, err := ctx.GetSessionVariable(ctx, "show_external_procedures")
 	if err != nil {
@@ -136,42 +138,41 @@ func routinesRowIter(ctx *Context, c Catalog, p map[string][]*plan.Procedure) (R
 			}
 			securityType = "DEFINER"
 			isDeterministic = "" // YES or NO
-			sqlMode = "SQL"      // SQL, NO SQL, READS SQL DATA, or MODIFIES SQL DATA.
 			if procedure.SecurityContext == plan.ProcedureSecurityContext_Invoker {
 				securityType = "INVOKER"
 			}
 			rows = append(rows, Row{
-				procedure.Name,             // specific_name NOT NULL
-				"def",                      // routine_catalog
-				db,                         // routine_schema // TODO: get correct db associated to this procedure
-				procedure.Name,             // routine_name NOT NULL
-				"PROCEDURE",                // routine_type NOT NULL
-				"",                         // data_type
-				nil,                        // character_maximum_length
-				nil,                        // character_octet_length
-				nil,                        // numeric_precision
-				nil,                        // numeric_scale
-				nil,                        // datetime_precision
-				nil,                        // character_set_name
-				nil,                        // collation_name
-				"",                         // dtd_identifier
-				"SQL",                      // routine_body NOT NULL
-				procedure.Body.String(),    // routine_definition
-				nil,                        // external_name
-				"SQL",                      // external_language NOT NULL
-				"SQL",                      // parameter_style NOT NULL
-				isDeterministic,            // is_deterministic NOT NULL
-				"",                         // sql_data_access NOT NULL
-				nil,                        // sql_path
-				securityType,               // security_type NOT NULL
-				procedure.CreatedAt.UTC(),  // created NOT NULL
-				procedure.ModifiedAt.UTC(), // last_altered NOT NULL
-				sqlMode,                    // sql_mode NOT NULL
-				procedure.Comment,          // routine_comment NOT NULL
-				procedure.Definer,          // definer NOT NULL
-				characterSetClient,         // character_set_client NOT NULL
-				collationConnection,        // collation_connection NOT NULL
-				collationServer,            // database_collation NOT NULL
+				procedure.Name,                  // specific_name NOT NULL
+				"def",                           // routine_catalog
+				db,                              // routine_schema // TODO: get correct db associated to this procedure
+				procedure.Name,                  // routine_name NOT NULL
+				"PROCEDURE",                     // routine_type NOT NULL
+				"",                              // data_type
+				nil,                             // character_maximum_length
+				nil,                             // character_octet_length
+				nil,                             // numeric_precision
+				nil,                             // numeric_scale
+				nil,                             // datetime_precision
+				nil,                             // character_set_name
+				nil,                             // collation_name
+				"",                              // dtd_identifier
+				"SQL",                           // routine_body NOT NULL
+				procedure.CreateProcedureString, // routine_definition  // body.String() gives query plan
+				nil,                             // external_name
+				"SQL",                           // external_language NOT NULL
+				"SQL",                           // parameter_style NOT NULL
+				isDeterministic,                 // is_deterministic NOT NULL
+				nil,                             // sql_data_access NOT NULL
+				nil,                             // sql_path
+				securityType,                    // security_type NOT NULL
+				procedure.CreatedAt.UTC(),       // created NOT NULL
+				procedure.ModifiedAt.UTC(),      // last_altered NOT NULL
+				nil,                             // sql_mode NOT NULL
+				procedure.Comment,               // routine_comment NOT NULL
+				procedure.Definer,               // definer NOT NULL
+				characterSetClient,              // character_set_client NOT NULL
+				collationConnection,             // collation_connection NOT NULL
+				collationServer,                 // database_collation NOT NULL
 			})
 		}
 	}
