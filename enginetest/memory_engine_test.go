ab0dc22d6cff5853e3f936a8eff41d49a94acc9f
James Cor <james@dolthub.com>
2022-12-09 10:23:05 -0800

    skip

--- enginetest/memory_engine_test.go
+++ enginetest/memory_engine_test.go
@@ -258,7 +258,7 @@ func TestSingleScript(t *testing.T) {
 }
 
 func TestSingleScriptPrepared(t *testing.T) {
-	//t.Skip()
+	t.Skip()
 	var script = queries.ScriptTest{
 		Name: "ALTER TABLE ... ALTER COLUMN SET / DROP DEFAULT",
 		SetUpScript: []string{
