ca2af19aafa492d137d29c151f600794e640fbc8
max-hoffman <max-hoffman@users.noreply.github.com>
2022-12-14 23:21:34 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/analyzer/exec_builder.go
+++ sql/analyzer/exec_builder.go
@@ -2,6 +2,7 @@ package analyzer
 
 import (
 	"fmt"
+
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/expression"
 	"github.com/dolthub/go-mysql-server/sql/plan"
