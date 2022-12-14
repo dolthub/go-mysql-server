215254e2c6585a8f015d2bfc9575eb8d2d2feb56
James Cor <james@dolthub.com>
2022-12-14 12:50:27 -0800

    adding a simple complex join query

--- enginetest/queries/script_queries.go
+++ enginetest/queries/script_queries.go
@@ -3525,6 +3525,30 @@ var PreparedScriptTests = []ScriptTest{
 			},
 		},
 	},
+	{
+		Name: "Complex join query with foreign key constraints",
+		SetUpScript: []string{
+			"CREATE TABLE `users` (`id` int NOT NULL AUTO_INCREMENT, `username` varchar(255) NOT NULL, PRIMARY KEY (`id`));",
+			"CREATE TABLE `tweet` ( `id` int NOT NULL AUTO_INCREMENT, `user_id` int NOT NULL, `content` text NOT NULL, `timestamp` bigint NOT NULL, PRIMARY KEY (`id`), KEY `tweet_user_id` (`user_id`), CONSTRAINT `0qpfesgd` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`));",
+			"INSERT INTO `users` (`id`,`username`) VALUES (1,'huey'), (2,'zaizee'), (3,'mickey');",
+			"INSERT INTO `tweet` (`id`,`user_id`,`content`,`timestamp`) VALUES (1,1,'meow',1647463727), (2,1,'purr',1647463727), (3,2,'hiss',1647463727), (4,3,'woof',1647463727);",
+			"set @u2 = 'u2';",
+			"set @u3 = 'u3';",
+			"set @u4 = 'u4';",
+		},
+		Assertions: []ScriptTestAssertion{
+			{
+				Query: "prepare s from 'SELECT `t1`.`username`, COUNT(`t1`.`id`) AS `ct` FROM ((SELECT `t2`.`id`, `t2`.`content`, `t3`.`username` FROM `tweet` AS `t2` INNER JOIN `users` AS `t3` ON (`t2`.`user_id` = `t3`.`id`) WHERE (`t3`.`username` = ?)) UNION (SELECT `t4`.`id`, `t4`.`content`, `t5`.`username` FROM `tweet` AS `t4` INNER JOIN `users` AS `t5` ON (`t4`.`user_id` = `t5`.`id`) WHERE (`t5`.`username` IN (?, ?)))) AS `t1` GROUP BY `t1`.`username` ORDER BY COUNT(`t1`.`id`) DESC'",
+				Expected: []sql.Row{
+					{sql.OkResult{Info: plan.PrepareInfo{}}},
+				},
+			},
+			{
+				Query:    "execute s using @u3, @u2, @u4",
+				Expected: []sql.Row{},
+			},
+		},
+	},
 }
 
 var BrokenScriptTests = []ScriptTest{
