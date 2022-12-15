8b9674581ccbb7bd5872fc96a8057a8f65da73b3
Max Hoffman <max@dolthub.com>
2022-12-15 10:48:43 -0800

    delete writer for now

--- sql/information_schema/information_schema.go
+++ sql/information_schema/information_schema.go
@@ -153,7 +153,6 @@ type informationSchemaTable struct {
 	schema  Schema
 	catalog Catalog
 	reader  func(*Context, Catalog) (RowIter, error)
-	editor  TableEditor
 }
 
 type informationSchemaPartition struct {
