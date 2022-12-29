eccb5306d3fd132f41538f2611fc2849fa4286af
Zach Musgrave <zach@dolthub.com>
2022-12-29 13:24:51 -0800

    Organized databases.go

--- sql/databases.go
+++ sql/databases.go
@@ -19,6 +19,55 @@ import (
 	"time"
 )
 
+// DatabaseProvider is the fundamental interface to integrate with the engine. It provides access to all databases in 
+// a given backend. A DatabaseProvider is provided to the Catalog when the engine is initialized.
+type DatabaseProvider interface {
+	// Database gets a Database from the provider.
+	Database(ctx *Context, name string) (Database, error)
+	// HasDatabase checks if the Database exists in the provider.
+	HasDatabase(ctx *Context, name string) bool
+	// AllDatabases returns a slice of all Databases in the provider.
+	AllDatabases(ctx *Context) []Database
+}
+
+// MutableDatabaseProvider is a DatabaseProvider that can create and drop databases.
+type MutableDatabaseProvider interface {
+	DatabaseProvider
+
+	// CreateDatabase creates a database and adds it to the provider's collection.
+	CreateDatabase(ctx *Context, name string) error
+
+	// DropDatabase removes a database from the provider's collection.
+	DropDatabase(ctx *Context, name string) error
+}
+
+// CollatedDatabaseProvider is a DatabaseProvider that can create a Database with a specific collation.
+type CollatedDatabaseProvider interface {
+	MutableDatabaseProvider
+
+	// CreateCollatedDatabase creates a collated database and adds it to the provider's collection.
+	CreateCollatedDatabase(ctx *Context, name string, collation CollationID) error
+}
+
+// TableFunctionProvider is an interface that allows custom table functions to be provided. It's usually (but not
+// always) implemented by a DatabaseProvider.
+type TableFunctionProvider interface {
+	// TableFunction returns the table function with the name provided, case-insensitive
+	TableFunction(ctx *Context, name string) (TableFunction, error)
+}
+
+// Database represents the database. Its primary job is to provide access to all tables.
+type Database interface {
+	Nameable
+	// GetTableInsensitive retrieves a table by its case-insensitive name. To be SQL compliant, databases should not 
+	// allow two tables with the same case-insensitive name. Behavior is undefined when two tables have the same 
+	// case-insensitive name.
+	GetTableInsensitive(ctx *Context, tblName string) (Table, bool, error)
+	// GetTableNames returns the table names of every table in the database. It does not return the names of temporary
+	// tables
+	GetTableNames(ctx *Context) ([]string, error)
+}
+
 // Databaser is a node that contains a reference to a database.
 type Databaser interface {
 	// Database the current database.
@@ -43,75 +92,104 @@ type MultiDatabaser interface {
 	WithDatabaseProvider(DatabaseProvider) (Node, error)
 }
 
-// Database represents the database.
-type Database interface {
-	Nameable
-
-	// GetTableInsensitive retrieves a table by its case insensitive name.  Implementations should look for exact
-	// (case-sensitive matches) first.  If no exact matches are found then any table matching the name case insensitively
-	// should be returned.  If there is more than one table that matches a case insensitive comparison the resolution
-	// strategy is not defined.
-	GetTableInsensitive(ctx *Context, tblName string) (Table, bool, error)
-
-	// GetTableNames returns the table names of every table in the database. It does not return the names of temporary
-	// tables
-	GetTableNames(ctx *Context) ([]string, error)
-}
-
+// ReadOnlyDatabase is an extension of Database that may declare itself read-only, which will disallow any DDL or DML
+// statements from executing.
 type ReadOnlyDatabase interface {
 	Database
-
 	// IsReadOnly returns whether this database is read-only.
 	IsReadOnly() bool
+}
+
+// TableCreator is a Database that can create new tables.
+type TableCreator interface {
+	Database
+	// CreateTable creates the table with the given name and schema.
+	CreateTable(ctx *Context, name string, schema PrimaryKeySchema, collation CollationID) error
+}
+
+// IndexedTableCreator is a Database that can create new tables which have a Primary Key with columns that have 
+// prefix lengths.
+type IndexedTableCreator interface {
+	Database
+	// CreateIndexedTable creates the table with the given name and schema using the index definition provided for its 
+	// primary key index.
+	CreateIndexedTable(ctx *Context, name string, schema PrimaryKeySchema, idxDef IndexDef, collation CollationID) error
+}
+
+// TemporaryTableCreator is a database that can create temporary tables that persist only as long as the session.
+// Note that temporary tables with the same name as persisted tables take precedence in most SQL operations.
+type TemporaryTableCreator interface {
+	Database
+	// CreateTemporaryTable creates the table with the given name and schema. If a temporary table with that name already exists, must
+	// return sql.ErrTableAlreadyExists
+	CreateTemporaryTable(ctx *Context, name string, schema PrimaryKeySchema, collation CollationID) error
+}
+
+// TableDropper is a Datagbase that can drop tables.
+type TableDropper interface {
+	Database
+	DropTable(ctx *Context, name string) error
+}
+
+// TableRenamer is a database that can rename tables.
+type TableRenamer interface {
+	Database
+	// RenameTable renames a table from oldName to newName as given.
+	RenameTable(ctx *Context, oldName, newName string) error
 }
 
 // VersionedDatabase is a Database that can return tables as they existed at different points in time. The engine
 // supports queries on historical table data via the AS OF construct introduced in SQL 2011.
 type VersionedDatabase interface {
 	Database
-
 	// GetTableInsensitiveAsOf retrieves a table by its case-insensitive name with the same semantics as
 	// Database.GetTableInsensitive, but at a particular revision of the database. Implementors must choose which types
 	// of expressions to accept as revision names.
 	GetTableInsensitiveAsOf(ctx *Context, tblName string, asOf interface{}) (Table, bool, error)
-
 	// GetTableNamesAsOf returns the table names of every table in the database as of the revision given. Implementors
 	// must choose which types of expressions to accept as revision names.
 	GetTableNamesAsOf(ctx *Context, asOf interface{}) ([]string, error)
 }
 
-// CollatedDatabase is a Database that may store and update its collation.
+// CollatedDatabase is a Database that can store and update its collation.
 type CollatedDatabase interface {
 	Database
-
 	// GetCollation returns this database's collation.
 	GetCollation(ctx *Context) CollationID
-
 	// SetCollation updates this database's collation.
 	SetCollation(ctx *Context, collation CollationID) error
 }
 
-// TriggerDatabase is a Database that supports the creation and execution of triggers. The engine handles all parsing
-// and execution logic for triggers. Integrators are not expected to parse or understand the trigger definitions, but
-// must store and return them when asked.
+// TriggerDatabase is a Database that supports creating and storing triggers. The engine handles all parsing and 
+// execution logic for triggers. Integrators are not expected to parse or understand the trigger definitions, but must 
+// store and return them when asked.
 type TriggerDatabase interface {
 	Database
-
 	// GetTriggers returns all trigger definitions for the database
 	GetTriggers(ctx *Context) ([]TriggerDefinition, error)
-
-	// CreateTrigger is called when an integrator is asked to create a trigger. The create trigger statement string is
+	// CreateTrigger is called when an integrator is asked to create a trigger. The CREATE TRIGGER statement string is
 	// provided to store, along with the name of the trigger.
 	CreateTrigger(ctx *Context, definition TriggerDefinition) error
-
 	// DropTrigger is called when a trigger should no longer be stored. The name has already been validated.
 	// Returns ErrTriggerDoesNotExist if the trigger was not found.
 	DropTrigger(ctx *Context, name string) error
 }
 
+// TriggerDefinition defines a trigger. Integrators are not expected to parse or understand the trigger definitions,
+// but must store and return them when asked.
+type TriggerDefinition struct {
+	// The name of this trigger. Trigger names in a database are unique.
+	Name string
+	// The text of the statement to create this trigger.
+	CreateStatement string
+	// The time that the trigger was created.
+	CreatedAt time.Time
+}
+
 // TemporaryTableDatabase is a database that can query the session (which manages the temporary table state) to
 // retrieve the name of all temporary tables.
 type TemporaryTableDatabase interface {
+	// GetAllTemporaryTables returns the names of all temporary tables in the session.
 	GetAllTemporaryTables(ctx *Context) ([]Table, error)
 }
 
@@ -120,6 +198,44 @@ type TemporaryTableDatabase interface {
 type TableCopierDatabase interface {
 	// CopyTableData copies the sourceTable data to the destinationTable and returns the number of rows copied.
 	CopyTableData(ctx *Context, sourceTable string, destinationTable string) (uint64, error)
+}
+
+// StoredProcedureDatabase is a database that supports the creation and execution of stored procedures. The engine will
+// handle all parsing and execution logic for stored procedures. Integrators only need to store and retrieve
+// StoredProcedureDetails, while verifying that all stored procedures have a unique name without regard to
+// case-sensitivity.
+type StoredProcedureDatabase interface {
+	Database
+	// GetStoredProcedures returns all StoredProcedureDetails for the database.
+	GetStoredProcedures(ctx *Context) ([]StoredProcedureDetails, error)
+	// SaveStoredProcedure stores the given StoredProcedureDetails to the database. The integrator should verify that
+	// the name of the new stored procedure is unique amongst existing stored procedures.
+	SaveStoredProcedure(ctx *Context, spd StoredProcedureDetails) error
+	// DropStoredProcedure removes the StoredProcedureDetails with the matching name from the database.
+	DropStoredProcedure(ctx *Context, name string) error
+}
+
+// ViewDatabase is implemented by databases that persist view definitions
+type ViewDatabase interface {
+	// CreateView persists the definition a view with the name and select statement given. If a view with that name
+	// already exists, should return ErrExistingView
+	CreateView(ctx *Context, name string, selectStatement string) error
+
+	// DropView deletes the view named from persistent storage. If the view doesn't exist, should return
+	// ErrViewDoesNotExist
+	DropView(ctx *Context, name string) error
+
+	// GetView returns the textual definition of the view with the name given, or false if it doesn't exist.
+	GetView(ctx *Context, viewName string) (string, bool, error)
+
+	// AllViews returns the definitions of all views in the database
+	AllViews(ctx *Context) ([]ViewDefinition, error)
+}
+
+// ViewDefinition is the named textual definition of a view
+type ViewDefinition struct {
+	Name           string
+	TextDefinition string
 }
 
 // GetTableInsensitive implements a case-insensitive map lookup for tables keyed off of the table name.
@@ -142,9 +258,9 @@ func GetTableInsensitive(tblName string, tables map[string]Table) (Table, bool) 
 	return nil, false
 }
 
-// GetTableNameInsensitive implements a case insensitive search of a slice of table names. It looks for exact matches
+// GetTableNameInsensitive implements a case-insensitive search of a slice of table names. It looks for exact matches
 // first.  If no exact matches are found then any table matching the name case insensitively should be returned.  If
-// there is more than one table that matches a case insensitive comparison the resolution strategy is not defined.
+// there is more than one table that matches a case-insensitive comparison the resolution strategy is not defined.
 func GetTableNameInsensitive(tblName string, tableNames []string) (string, bool) {
 	for _, name := range tableNames {
 		if tblName == name {
@@ -193,124 +309,4 @@ func DBTableIter(ctx *Context, db Database, cb func(Table) (cont bool, err error
 	}
 
 	return nil
-}
-
-// TableCreator should be implemented by databases that can create new tables.
-type TableCreator interface {
-	// CreateTable creates the table with the given name and schema. If a table with that name already exists, must return
-	// sql.ErrTableAlreadyExists.
-	CreateTable(ctx *Context, name string, schema PrimaryKeySchema, collation CollationID) error
-}
-
-// IndexedTableCreator should be implemented by databases that create new tables where they have a Primary Key that has columns that have prefix lengths.
-type IndexedTableCreator interface {
-	CreateIndexedTable(ctx *Context, name string, schema PrimaryKeySchema, idxDef IndexDef, collation CollationID) error
-}
-
-// TemporaryTableCreator is a database that can create temporary tables that persist only as long as the session.
-// Note that temporary tables with the same name as persisted tables take precedence in most SQL operations.
-type TemporaryTableCreator interface {
-	Database
-	// CreateTemporaryTable creates the table with the given name and schema. If a temporary table with that name already exists, must
-	// return sql.ErrTableAlreadyExists
-	CreateTemporaryTable(ctx *Context, name string, schema PrimaryKeySchema, collation CollationID) error
-}
-
-// TableDropper should be implemented by databases that can drop tables.
-type TableDropper interface {
-	DropTable(ctx *Context, name string) error
-}
-
-// TableRenamer should be implemented by databases that can rename tables.
-type TableRenamer interface {
-	// RenameTable renames a table from oldName to newName as given. If a table with newName already exists, must return
-	// sql.ErrTableAlreadyExists.
-	RenameTable(ctx *Context, oldName, newName string) error
-}
-
-// StoredProcedureDatabase is a database that supports the creation and execution of stored procedures. The engine will
-// handle all parsing and execution logic for stored procedures. Integrators only need to store and retrieve
-// StoredProcedureDetails, while verifying that all stored procedures have a unique name without regard to
-// case-sensitivity.
-type StoredProcedureDatabase interface {
-	Database
-
-	// GetStoredProcedures returns all StoredProcedureDetails for the database.
-	GetStoredProcedures(ctx *Context) ([]StoredProcedureDetails, error)
-
-	// SaveStoredProcedure stores the given StoredProcedureDetails to the database. The integrator should verify that
-	// the name of the new stored procedure is unique amongst existing stored procedures.
-	SaveStoredProcedure(ctx *Context, spd StoredProcedureDetails) error
-
-	// DropStoredProcedure removes the StoredProcedureDetails with the matching name from the database.
-	DropStoredProcedure(ctx *Context, name string) error
-}
-
-// DatabaseProvider is a collection of Database.
-type DatabaseProvider interface {
-	// Database gets a Database from the provider.
-	Database(ctx *Context, name string) (Database, error)
-
-	// HasDatabase checks if the Database exists in the provider.
-	HasDatabase(ctx *Context, name string) bool
-
-	// AllDatabases returns a slice of all Databases in the provider.
-	AllDatabases(ctx *Context) []Database
-}
-
-// MutableDatabaseProvider is a DatabaseProvider that can create and drop databases.
-type MutableDatabaseProvider interface {
-	DatabaseProvider
-
-	// CreateDatabase creates a database and adds it to the provider's collection.
-	CreateDatabase(ctx *Context, name string) error
-
-	// DropDatabase removes a database from the provider's collection.
-	DropDatabase(ctx *Context, name string) error
-}
-
-// CollatedDatabaseProvider is a DatabaseProvider that can create a Database with a specific collation.
-type CollatedDatabaseProvider interface {
-	MutableDatabaseProvider
-
-	// CreateCollatedDatabase creates a collated database and adds it to the provider's collection.
-	CreateCollatedDatabase(ctx *Context, name string, collation CollationID) error
-}
-
-// ViewDefinition is the named textual definition of a view
-type ViewDefinition struct {
-	Name           string
-	TextDefinition string
-}
-
-// ViewDatabase is implemented by databases that persist view definitions
-type ViewDatabase interface {
-	// CreateView persists the definition a view with the name and select statement given. If a view with that name
-	// already exists, should return ErrExistingView
-	CreateView(ctx *Context, name string, selectStatement string) error
-
-	// DropView deletes the view named from persistent storage. If the view doesn't exist, should return
-	// ErrViewDoesNotExist
-	DropView(ctx *Context, name string) error
-
-	// GetView returns the textual definition of the view with the name given, or false if it doesn't exist.
-	GetView(ctx *Context, viewName string) (string, bool, error)
-
-	// AllViews returns the definitions of all views in the database
-	AllViews(ctx *Context) ([]ViewDefinition, error)
-}
-
-// TriggerDefinition defines a trigger. Integrators are not expected to parse or understand the trigger definitions,
-// but must store and return them when asked.
-type TriggerDefinition struct {
-	Name            string    // The name of this trigger. Trigger names in a database are unique.
-	CreateStatement string    // The text of the statement to create this trigger.
-	CreatedAt       time.Time // The time that the trigger was created.
-}
-
-// TableFunctionProvider is an interface that allows custom table functions to be provided. It's usually (but not
-// always) implemented by a DatabaseProvider.
-type TableFunctionProvider interface {
-	// TableFunction returns the table function with the name provided, case-insensitive
-	TableFunction(ctx *Context, name string) (TableFunction, error)
 }
