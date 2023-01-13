82566c81b7dfe008d7010b6be73dd03d46e65bed
James Cor <james@dolthub.com>
2023-01-12 21:59:23 -0800

    adding errors

--- sql/errors.go
+++ sql/errors.go
@@ -546,6 +546,15 @@ var (
 	// ErrUnsupportedSRID is thrown for spatial functions over unsupported SRIDs
 	ErrUnsupportedSRID = errors.NewKind("SRID %v has not been implemented for geographic spatial reference systems.")
 
+	// ErrTooManyKeyParts is thrown when creating an index with too many columns
+	ErrTooManyKeyParts = errors.NewKind("too many key parts specified; max %d parts allowed")
+
+	// ErrSRIDOnNonGeomCol is thrown when attempting to define SRID over a non-geometry column
+	ErrSRIDOnNonGeomCol = errors.NewKind("incorrect usage of SRID and non-geometry column")
+
+	// ErrBadSpatialIdxCol is thrown when attempting to define a SPATIAL index over a non-geometry column
+	ErrBadSpatialIdxCol = errors.NewKind("a SPATIAL index may only contain a geometrical type column")
+
 	// ErrUnsupportedGISTypeForSpatialFunc is a temporary error because geometry is hard
 	// TODO: remove this error when all types are full supported by spatial type functions
 	ErrUnsupportedGISTypeForSpatialFunc = errors.NewKind("unsupported spatial type: %s for function %s")
