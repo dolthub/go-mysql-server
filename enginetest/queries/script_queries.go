144c799aa8f5c3afa7dc04f640c3e96880d3379d
James Cor <james@dolthub.com>
2023-01-20 14:53:52 -0800

    tests for create and alter spatial index

--- enginetest/queries/script_queries.go
+++ enginetest/queries/script_queries.go
@@ -3200,6 +3200,30 @@ var SpatialScriptTests = []ScriptTest{
 				Query:       "create table geom(g geometry not null srid 4326, spatial index (g))",
 				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
 			},
+			{
+				Query:       "create table geom(g point not null srid 4326, spatial index (g))",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
+			},
+			{
+				Query:       "create table geom(g linestring not null srid 4326, spatial index (g))",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
+			},
+			{
+				Query:       "create table geom(g polygon not null srid 4326, spatial index (g))",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
+			},
+			{
+				Query:       "create table geom(g multipoint not null srid 4326, spatial index (g))",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
+			},
+			{
+				Query:       "create table geom(g multilinestring not null srid 4326, spatial index (g))",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
+			},
+			{
+				Query:       "create table geom(g multipolygon not null srid 4326, spatial index (g))",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
+			},
 		},
 	},
 	{
@@ -3223,6 +3247,48 @@ var SpatialScriptTests = []ScriptTest{
 			{
 				Query:       "alter table geom add spatial index (g)",
 				ExpectedErr: sql.ErrNullableSpatialIdx,
+			},
+		},
+	},
+	{
+		Name: "valid add spatial index alter",
+		SetUpScript: []string{
+			"create table geom1(g point SRID 4326 NOT NULL)",
+			"create table geom2(g linestring SRID 4326 NOT NULL)",
+			"create table geom3(g polygon SRID 4326 NOT NULL)",
+			"create table geom4(g multipoint SRID 4326 NOT NULL)",
+			"create table geom5(g multilinestring SRID 4326 NOT NULL)",
+			"create table geom6(g multipolygon SRID 4326 NOT NULL)",
+			"create table geom7(g geometry SRID 4326 NOT NULL)",
+		},
+		Assertions: []ScriptTestAssertion{
+			{
+				Query:       "alter table geom1 add spatial index (g)",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
+			},
+			{
+				Query:       "alter table geom2 add spatial index (g)",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
+			},
+			{
+				Query:       "alter table geom3 add spatial index (g)",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
+			},
+			{
+				Query:       "alter table geom4 add spatial index (g)",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
+			},
+			{
+				Query:       "alter table geom5 add spatial index (g)",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
+			},
+			{
+				Query:       "alter table geom6 add spatial index (g)",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
+			},
+			{
+				Query:       "alter table geom7 add spatial index (g)",
+				ExpectedErr: sql.ErrUnsupportedSpatialIdx,
 			},
 		},
 	},
