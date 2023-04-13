21d6f4fc79c5563f04a527eb5368621740af6624
max-hoffman <max-hoffman@users.noreply.github.com>
2023-04-13 15:58:06 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/plan/join.go
+++ sql/plan/join.go
@@ -16,11 +16,10 @@ package plan
 
 import (
 	"fmt"
-	"github.com/dolthub/go-mysql-server/sql/transform"
-
-	"github.com/dolthub/go-mysql-server/sql/expression"
 
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/expression"
+	"github.com/dolthub/go-mysql-server/sql/transform"
 )
 
 //go:generate stringer -type=JoinType -linecomment
