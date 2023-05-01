949ce107784f093610bce9fe0ffc360be1974cd0
James Cor <james@dolthub.com>
2023-05-01 15:58:46 -0700

    working with debug string

--- sql/plan/indexed_table_access.go
+++ sql/plan/indexed_table_access.go
@@ -235,6 +235,8 @@ func (i *IndexedTableAccess) String() string {
 		}
 	}
 
+	children = append(children, fmt.Sprintf("reverse: %v", i.lookup.IsReverse))
+
 	pr.WriteChildren(children...)
 	return pr.String()
 }
