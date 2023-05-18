9435d4261b9226938bbe173117af473c8a778faa
JCOR11599 <JCOR11599@users.noreply.github.com>
2023-05-18 21:20:04 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/expression/function/spatial/st_area.go
+++ sql/expression/function/spatial/st_area.go
@@ -16,6 +16,7 @@ package spatial
 
 import (
 	"fmt"
+
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/expression"
 	"github.com/dolthub/go-mysql-server/sql/types"
