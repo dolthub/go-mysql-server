ebfe225f83176f37bfb18070770158484923c348
Nick Tobey <nick@dolthub.com>
2023-05-21 23:43:35 -0400

    Remove remaining use of RejectRowsWithNullCondition

--- sql/plan/join.go
+++ sql/plan/join.go
@@ -245,9 +245,6 @@ type JoinNode struct {
 	Op         JoinType
 	CommentStr string
 	ScopeLen   int
-	// If RejectRowsWithNullCondition is true, then the results will filter any row from the left child that
-	// was compared against a row on the right table and returned NULL. This only makes sense for outer joins.
-	RejectRowsWithNullCondition bool
 }
 
 var _ sql.Node = (*JoinNode)(nil)
