8a5b9ac2842fe673322f52213c0635f7d4734163
Nick Tobey <nick@dolthub.com>
2023-06-16 14:28:35 -0700

    Document new NetListener options.

--- server/net_listener_unix.go
+++ server/net_listener_unix.go
@@ -9,6 +9,10 @@ import (
 	"syscall"
 )
 
+// Very rarely in our CI, the server fails to bind to the port with the error: "port already in use."
+// This is odd because the server already confirms that the port is not in use before connecting.
+// Using the SO_REUSEADDR and SO_REUSEPORT options prevents this spurious failure.
+// This is safe to do because we have already checked that the
 func newNetListener(protocol, address string) (net.Listener, error) {
 	lc := net.ListenConfig{
 		Control: func(network, address string, c syscall.RawConn) error {
