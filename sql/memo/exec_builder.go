cc3b121849a67ab82a4a5a60b64c48fd6b9d14d7
JCOR11599 <JCOR11599@users.noreply.github.com>
2023-06-26 22:42:52 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/memo/exec_builder.go
+++ sql/memo/exec_builder.go
@@ -2,13 +2,12 @@ package memo
 
 import (
 	"fmt"
-	"github.com/dolthub/go-mysql-server/sql/types"
-
-	"github.com/dolthub/go-mysql-server/sql/fixidx"
 
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/expression"
+	"github.com/dolthub/go-mysql-server/sql/fixidx"
 	"github.com/dolthub/go-mysql-server/sql/plan"
+	"github.com/dolthub/go-mysql-server/sql/types"
 )
 
 type ExecBuilder struct{}
