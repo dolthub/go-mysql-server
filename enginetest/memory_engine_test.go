88eeda8853b9cc8b85775bd8af7c164690d95f4a
JCOR11599 <JCOR11599@users.noreply.github.com>
2023-07-13 20:22:47 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- enginetest/memory_engine_test.go
+++ enginetest/memory_engine_test.go
@@ -292,8 +292,7 @@ func TestSingleScript_Experimental(t *testing.T) {
 			Assertions: []queries.ScriptTestAssertion{
 				{
 					Query:    `select * from t right join lateral (select * from t1 where t.i != t1.j) as tt on t.i > tt.j`,
-					Expected: []sql.Row{
-					},
+					Expected: []sql.Row{},
 				},
 			},
 		},
