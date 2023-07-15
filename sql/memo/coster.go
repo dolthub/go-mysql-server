3a14a7c2c868696bd68d5104b815b89bcf432010
Nick Tobey <nick@dolthub.com>
2023-07-14 17:45:18 -0700

    Add coster for Sliding Range Join

--- sql/memo/coster.go
+++ sql/memo/coster.go
@@ -71,6 +71,8 @@ func (c *coster) costRel(ctx *sql.Context, n RelExpr, s sql.StatsReader) (float6
 		return c.costMergeJoin(ctx, n, s)
 	case *LookupJoin:
 		return c.costLookupJoin(ctx, n, s)
+	case *SlidingRangeJoin:
+		return c.costSlidingRangeJoin(ctx, n, s)
 	case *LateralCrossJoin:
 		return c.costLateralCrossJoin(ctx, n, s)
 	case *LateralInnerJoin:
@@ -187,6 +189,11 @@ func (c *coster) costLookupJoin(_ *sql.Context, n *LookupJoin, _ sql.StatsReader
 		return l*(cpuCostFactor+randIOCostFactor) - r*seqIOCostFactor, nil
 	}
 	return l*r*sel*(cpuCostFactor+randIOCostFactor) - r*seqIOCostFactor, nil
+}
+
+func (c *coster) costSlidingRangeJoin(_ *sql.Context, n *SlidingRangeJoin, _ sql.StatsReader) (float64, error) {
+	// For now always favor sliding range.
+	return 0, nil
 }
 
 func (c *coster) costLateralCrossJoin(ctx *sql.Context, n *LateralCrossJoin, _ sql.StatsReader) (float64, error) {
