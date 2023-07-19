67745f417538881b5504f6ee6dbc56f15a3ee343
Nick Tobey <nick@dolthub.com>
2023-07-19 15:03:28 -0700

    When there's no index for the sliding range, use nil instead.
    (Once this is complete, SlidingRange can choose whether to use an index or a sort.)

--- sql/analyzer/indexed_joins.go
+++ sql/analyzer/indexed_joins.go
@@ -699,8 +699,10 @@ func addSlidingRangeJoin(m *memo.Memo) error {
 					// iterjoin assumes that there's a filter condition.
 					// rel.Filter = rel.Filter[1:]
 					rel.SlidingRange = &memo.SlidingRange{
-						LeftIndex:  *lIdx,
-						RightIndex: *rIdx,
+						LeftIndex:  lIdx,
+						RightIndex: rIdx,
+						ValueExpr:  &f.Value.Scalar,
+						MinExpr:    &f.Min.Scalar,
 						ValueCol:   valueColRef,
 						MinColRef:  minColRef,
 						MaxColRef:  maxColRef,
@@ -710,6 +712,25 @@ func addSlidingRangeJoin(m *memo.Memo) error {
 					e.Group().Prepend(rel)
 				}
 			}
+			rel := &memo.SlidingRangeJoin{
+				JoinBase: join.Copy(),
+			}
+			// TODO: Remove the filter that was used to create the sliding range because it's no longer
+			// necessary to evaluate. However, removing this can cause issues if it's the only filter because
+			// iterjoin assumes that there's a filter condition.
+			// rel.Filter = rel.Filter[1:]
+			rel.SlidingRange = &memo.SlidingRange{
+				LeftIndex:  nil,
+				RightIndex: nil,
+				ValueExpr:  &f.Value.Scalar,
+				MinExpr:    &f.Min.Scalar,
+				ValueCol:   valueColRef,
+				MinColRef:  minColRef,
+				MaxColRef:  maxColRef,
+				Parent:     rel.JoinBase,
+			}
+			rel.Op = rel.Op.AsSlidingRange()
+			e.Group().Prepend(rel)
 
 			return nil
 		default:
