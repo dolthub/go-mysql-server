b53b3b26604fb304bdc9bbc34fb53b489271f14c
Zach Musgrave <zach@dolthub.com>
2023-08-16 12:23:57 -0700

    reskip test

--- enginetest/memory_engine_test.go
+++ enginetest/memory_engine_test.go
@@ -276,7 +276,7 @@ func TestSingleQueryPrepared(t *testing.T) {
 
 // Convenience test for debugging a single query. Unskip and set to the desired query.
 func TestSingleScript(t *testing.T) {
-	// t.Skip()
+	t.Skip()
 	var scripts = []queries.ScriptTest{
 		{
 			Name: "datetime precision",
