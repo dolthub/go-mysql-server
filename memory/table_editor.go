6c91e8513b429f287dd71a42abae970630d99d22
Zach Musgrave <zach@dolthub.com>
2023-08-31 17:27:58 -0700

    Cleanup

--- memory/table_editor.go
+++ memory/table_editor.go
@@ -284,8 +284,6 @@ func (t *tableEditor) Update(ctx *sql.Context, oldRow sql.Row, newRow sql.Row) e
 func (t *tableEditor) SetAutoIncrementValue(ctx *sql.Context, val uint64) error {
 	t.editedTable.autoIncVal = val
 	return nil
-	// TODO: this seems like a bug, we should be closing this in the engine
-	// return t.Close(ctx)
 }
 
 func (t *tableEditor) IndexedAccess(ctx *sql.Context, i sql.IndexLookup) (sql.IndexedTable, error) {
