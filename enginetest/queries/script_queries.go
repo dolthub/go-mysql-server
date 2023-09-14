36b874b004348e6aaae46a9e57916198ba8cc8a3
James Cor <james@dolthub.com>
2023-09-14 11:35:56 -0700

    type test 2

--- enginetest/queries/script_queries.go
+++ enginetest/queries/script_queries.go
@@ -199,6 +199,10 @@ var ScriptTests = []ScriptTest{
 			"insert into b values (1,2), (1,3), (3,4);",
 			"create table c (m int, n int);",
 			"insert into c values (1,3), (1,3), (3,4);",
+			"create table t1 (i int);",
+			"insert into t1 values (1), (2), (3);",
+			"create table t2 (i float);",
+			"insert into t2 values (1.0), (1.99), (3);",
 		},
 		Assertions: []ScriptTestAssertion{
 			{
@@ -231,6 +235,12 @@ var ScriptTests = []ScriptTest{
 				Expected: []sql.Row{
 					{1, 3},
 					{1, 3},
+				},
+			},
+			{
+				Query: "table t1 except table t2 order by i;",
+				Expected: []sql.Row {
+					{2},
 				},
 			},
 		},
