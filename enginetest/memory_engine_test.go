577b511eb0ca550fe5760957d36ba1a7f308e956
Zach Musgrave <zach@dolthub.com>
2023-10-17 16:35:35 -0700

    Revert engine test

--- enginetest/memory_engine_test.go
+++ enginetest/memory_engine_test.go
@@ -195,24 +195,100 @@ func newUpdateResult(matched, updated int) types.OkResult {
 
 // Convenience test for debugging a single query. Unskip and set to the desired query.
 func TestSingleScript(t *testing.T) {
-	// t.Skip()
+	t.Skip()
 	var scripts = []queries.ScriptTest{
 		{
-			Name: "virtual column json extract",
+			Name: "virtual column inserts, updates, deletes",
 			SetUpScript: []string{
-				"create table t1 (a int primary key, j json, b int generated always as (j->>'$.b') virtual)",
+				"create table t1 (a int primary key, b int generated always as (a + 1) virtual)",
 			},
 			Assertions: []queries.ScriptTestAssertion{
 				{
-					Query:    `insert into t1 (a, j) values (1, '{"a": 1, "b": 2}'), (2, '{"a": 1}'), (3, '{"b": "300"}')`,
+					Query:    "insert into t1 (a) values (1), (2), (3)",
 					Expected: []sql.Row{{types.NewOkResult(3)}},
 				},
 				{
 					Query:    "select * from t1 order by a",
+					Expected: []sql.Row{{1, 2}, {2, 3}, {3, 4}},
+				},
+				{
+					Query: "update t1 set a = 4 where a = 3",
+					Expected: []sql.Row{{types.OkResult{
+						RowsAffected: 1,
+						Info: plan.UpdateInfo{
+							Matched: 1,
+							Updated: 1,
+						}},
+					}},
+				},
+				{
+					Query:    "select * from t1 order by a",
+					Expected: []sql.Row{{1, 2}, {2, 3}, {4, 5}},
+				},
+				{
+					Query:    "delete from t1 where a = 2",
+					Expected: []sql.Row{{types.OkResult{RowsAffected: 1}}},
+				},
+				{
+					Query:    "select * from t1 order by a",
+					Expected: []sql.Row{{1, 2}, {4, 5}},
+				},
+			},
+		},
+		{
+			Name: "virtual column ordering",
+			SetUpScript: []string{
+				// virtual is the default for generated columns
+				"create table t1 (v1 int generated always as (2), a int, v2 int generated always as (a + v1), c int)",
+			},
+			Assertions: []queries.ScriptTestAssertion{
+				{
+					Query:    "insert into t1 (a, c) values (1,5), (3,7)",
+					Expected: []sql.Row{{types.NewOkResult(2)}},
+				},
+				{
+					Query:    "insert into t1 (c, a) values (5,6), (7,8)",
+					Expected: []sql.Row{{types.NewOkResult(2)}},
+				},
+				{
+					Query: "select * from t1 order by a",
 					Expected: []sql.Row{
-						{1, types.MustJSON(`{"a": 1, "b": 2}`), 2},
-						{2, types.MustJSON(`{"a": 1}`), nil},
-						{3, types.MustJSON(`{"b": "300"}`), 300}},
+						{2, 1, 3, 5},
+						{2, 3, 5, 7},
+						{2, 6, 8, 5},
+						{2, 8, 10, 7},
+					},
+				},
+				{
+					Query: "update t1 set a = 4 where a = 3",
+					Expected: []sql.Row{{types.OkResult{
+						RowsAffected: 1,
+						Info: plan.UpdateInfo{
+							Matched: 1,
+							Updated: 1,
+						}},
+					}},
+				},
+				{
+					Query: "select * from t1 order by a",
+					Expected: []sql.Row{
+						{2, 1, 3, 5},
+						{2, 4, 6, 7},
+						{2, 6, 8, 5},
+						{2, 8, 10, 7},
+					},
+				},
+				{
+					Query:    "delete from t1 where v2 = 6",
+					Expected: []sql.Row{{types.OkResult{RowsAffected: 1}}},
+				},
+				{
+					Query: "select * from t1 order by a",
+					Expected: []sql.Row{
+						{2, 1, 3, 5},
+						{2, 6, 8, 5},
+						{2, 8, 10, 7},
+					},
 				},
 			},
 		},
