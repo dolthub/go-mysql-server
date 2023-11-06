0acf418e34d38869002624bb8b9653ab00f9111e
Max Hoffman <max@dolthub.com>
2023-11-06 14:14:36 -0800

    fix build

--- enginetest/queries/queries.go
+++ enginetest/queries/queries.go
@@ -37,6 +37,27 @@ type QueryTest struct {
 	Bindings        map[string]*query.BindVariable
 	SkipPrepared    bool
 }
+type QueryPlanTest struct {
+	Query        string
+	ExpectedPlan string
+	Skip         bool
+}
+
+// QueryPlanTODOs are queries where the query planner produces a correct (results) but suboptimal plan.
+var QueryPlanTODOs = []QueryPlanTest{
+	{
+		// TODO: this should use an index. Extra join condition should get moved out of the join clause into a filter
+		Query: `SELECT pk,i,f FROM one_pk RIGHT JOIN niltable ON pk=i and pk > 0 ORDER BY 2,3`,
+		ExpectedPlan: "Sort(niltable.i ASC, niltable.f ASC)\n" +
+			" └─ Project(one_pk.pk, niltable.i, niltable.f)\n" +
+			"     └─ RightJoin((one_pk.pk = niltable.i) AND (one_pk.pk > 0))\n" +
+			"         ├─ Projected table access on [pk]\n" +
+			"         │   └─ Table(one_pk)\n" +
+			"         └─ Projected table access on [i f]\n" +
+			"             └─ Table(niltable)\n" +
+			"",
+	},
+}
 
 var SpatialQueryTests = []QueryTest{
 	{
