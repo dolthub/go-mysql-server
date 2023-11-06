c647eff0fe1e16b9c39bccdf92f83dc6eddb6ff3
Max Hoffman <max@dolthub.com>
2023-11-06 08:23:18 -0800

    Delete old indexing code

--- sql/analyzer/analyzer_test.go
+++ sql/analyzer/analyzer_test.go
@@ -32,7 +32,7 @@ func TestAddRule(t *testing.T) {
 
 	defRulesCount := countRules(NewDefault(nil).Batches)
 
-	a := NewBuilder(nil).AddPostAnalyzeRule(-1, generateIndexScans).Build()
+	a := NewBuilder(nil).AddPostAnalyzeRule(-1, pushFilters).Build()
 
 	require.Equal(countRules(a.Batches), defRulesCount+1)
 }
@@ -42,7 +42,7 @@ func TestAddPreValidationRule(t *testing.T) {
 
 	defRulesCount := countRules(NewDefault(nil).Batches)
 
-	a := NewBuilder(nil).AddPreValidationRule(-1, generateIndexScans).Build()
+	a := NewBuilder(nil).AddPreValidationRule(-1, pushFilters).Build()
 
 	require.Equal(countRules(a.Batches), defRulesCount+1)
 }
@@ -52,7 +52,7 @@ func TestAddPostValidationRule(t *testing.T) {
 
 	defRulesCount := countRules(NewDefault(nil).Batches)
 
-	a := NewBuilder(nil).AddPostValidationRule(-1, generateIndexScans).Build()
+	a := NewBuilder(nil).AddPostValidationRule(-1, pushFilters).Build()
 
 	require.Equal(countRules(a.Batches), defRulesCount+1)
 }
