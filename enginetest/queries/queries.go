0b0930082b768dc2f5e6dca847e1f4f3cb14fdb0
James Cor <james@dolthub.com>
2023-11-23 23:50:13 -0800

    dayname

--- enginetest/queries/queries.go
+++ enginetest/queries/queries.go
@@ -8469,6 +8469,58 @@ order by x, y;`,
 		},
 	},
 	{
+//		Query: `
+//select
+//    dayname(id),
+//    dayname(i8),
+//    dayname(i16),
+//    dayname(i32),
+//    dayname(i64),
+//    dayname(u8),
+//    dayname(u16),
+//    dayname(u32),
+//    dayname(u64),
+//    dayname(f32),
+//    dayname(f64),
+//    dayname(ti),
+//    dayname(da),
+//    dayname(te),
+//    dayname(bo),
+//    dayname(js),
+//    dayname(bl),
+//    dayname(e1),
+//    dayname(s1)
+//from typestable`,
+	Query: `
+select 
+	dayname(bo), 
+	dayname(s1) 
+from typestable`,
+		Expected: []sql.Row{
+			{
+				nil,
+				nil,
+				nil,
+				nil,
+				nil,
+				nil,
+				nil,
+				nil,
+				nil,
+				nil,
+				nil,
+				"Tuesday",
+				"Tuesday",
+				nil,
+				"Saturday",
+				nil,
+				nil,
+				nil,
+				"Saturday",
+			},
+		},
+	},
+	{
 		Query: "select * from mytable order by dayname(i)",
 		Expected: []sql.Row{
 			{1, "first row"},
