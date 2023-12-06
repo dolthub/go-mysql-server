895d7048faeed2c0593c9dedab718c74d822f462
JCOR11599 <JCOR11599@users.noreply.github.com>
2023-12-06 22:30:28 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- enginetest/enginetests.go
+++ enginetest/enginetests.go
@@ -4248,24 +4248,24 @@ func TestOnUpdateTimestamp(t *testing.T, harness Harness) {
 	defer e.Close()
 
 	time1 := time.Date(
-		2000, // year
-		1,    // month
-		1,    // day
-		1,    // hour
-		0,    // min
-		0,    // sec
-		0,    // nsec
+		2000,     // year
+		1,        // month
+		1,        // day
+		1,        // hour
+		0,        // min
+		0,        // sec
+		0,        // nsec
 		time.UTC, // location (UTC)
 	)
 
 	time2 := time.Date(
-		2000, // year
-		2,    // month
-		2,    // day
-		2,    // hour
-		0,    // min
-		0,    // sec
-		0,    // nsec
+		2000,     // year
+		2,        // month
+		2,        // day
+		2,        // hour
+		0,        // min
+		0,        // sec
+		0,        // nsec
 		time.UTC, // location (UTC)
 	)
 
@@ -4276,15 +4276,15 @@ func TestOnUpdateTimestamp(t *testing.T, harness Harness) {
 	RunQueryWithContext(t, e, harness, ctx, "INSERT INTO t1(id) VALUES (1), (2), (3)")
 
 	okRes := []sql.Row{{types.OkResult{RowsAffected: 3, Info: plan.UpdateInfo{Matched: 3, Updated: 3}}}}
-	TestQueryWithContext(t, ctx, e, harness, "UPDATE t1 SET id = 100",  okRes, nil, nil)
+	TestQueryWithContext(t, ctx, e, harness, "UPDATE t1 SET id = 100", okRes, nil, nil)
 
-	exp1 := []sql.Row{{100, time1},{100, time1},{100, time1}}
+	exp1 := []sql.Row{{100, time1}, {100, time1}, {100, time1}}
 	TestQueryWithContext(t, ctx, e, harness, "SELECT * FROM t1", exp1, nil, nil)
 
 	ctx.SetQueryTime(time2)
-	TestQueryWithContext(t, ctx, e, harness, "UPDATE t1 SET id = 200",  okRes, nil, nil)
+	TestQueryWithContext(t, ctx, e, harness, "UPDATE t1 SET id = 200", okRes, nil, nil)
 
-	exp2 := []sql.Row{{200, time2},{200, time2},{200, time2}}
+	exp2 := []sql.Row{{200, time2}, {200, time2}, {200, time2}}
 	TestQueryWithContext(t, ctx, e, harness, "SELECT * FROM t1", exp2, nil, nil)
 }
 
