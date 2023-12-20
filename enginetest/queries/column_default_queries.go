fb2d84a64f12f94674bb7ed18deeb716b64bb9e7
Max Hoffman <maximilian.wolfgang1@gmail.com>
2023-12-20 14:55:35 -0600

    generated test

--- enginetest/queries/column_default_queries.go
+++ enginetest/queries/column_default_queries.go
@@ -21,7 +21,7 @@ import (
 
 var ColumnDefaultTests = []ScriptTest{
 	{
-		Name: "update join ambiguous column",
+		Name: "update join ambiguous default",
 		SetUpScript: []string{
 			"CREATE TABLE t1(name varchar(10) primary key, cnt int, hash varchar(100) NOT NULL DEFAULT (concat('id00',md5(name))))",
 			"INSERT INTO t1 (name, cnt) VALUES ('one', 1), ('two', 2)",
@@ -35,6 +35,24 @@ var ColumnDefaultTests = []ScriptTest{
 			{
 				Query:    "select name, cnt from t1",
 				Expected: []sql.Row{{"one", 2}, {"two", 3}},
+			},
+		},
+	},
+	{
+		Name: "update join ambiguous generated column",
+		SetUpScript: []string{
+			"CREATE TABLE t1 (x int primary key, y int generated always as (x + 1) virtual)",
+			"INSERT INTO t1 (x) values (1), (2), (3)",
+			"create view t2 as SELECT x, y from t1",
+		},
+		Assertions: []ScriptTestAssertion{
+			{
+				Query:    "update t1 n inner join t2 m on n.y = m.y set n.x =n.y where n.x = 3;",
+				Expected: []sql.Row{{newUpdateResult(1, 1)}},
+			},
+			{
+				Query:    "select * from t1",
+				Expected: []sql.Row{{1, 2}, {2, 3}, {4, 5}},
 			},
 		},
 	},
