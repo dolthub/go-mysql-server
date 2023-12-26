db6d579b0d157a919e90b09c2f94f91541c5231e
jennifersp <44716627+jennifersp@users.noreply.github.com>
2023-12-26 11:57:03 -0800

    fix type evaluating for `mult` and `div` arithmetic operations (#2208)

--- enginetest/queries/queries.go
+++ enginetest/queries/queries.go
@@ -4612,6 +4612,14 @@ Select * from (
 		},
 	},
 	{
+		Query:    "select 0 in (1 / 1000), 0 in (1 / 1000, 0), 0.001 in (1 / 1000, 0), 0.0001 in (1 / 1000, 0);",
+		Expected: []sql.Row{{false, true, true, false}},
+	},
+	{
+		Query:    "select 0 in (0.01 * 0.30), 1 in (1.0 * 1)",
+		Expected: []sql.Row{{false, true}},
+	},
+	{
 		Query: "SELECT '3' > 2 FROM tabletest",
 		Expected: []sql.Row{
 			{true},
@@ -8728,6 +8736,13 @@ var KeylessQueries = []QueryTest{
 
 // BrokenQueries are queries that are known to be broken in the engine.
 var BrokenQueries = []QueryTest{
+	// https://github.com/dolthub/dolt/issues/7207
+	{
+		Query: "select 0 in (1/100000);",
+		Expected: []sql.Row{
+			{false},
+		},
+	},
 	// union and aggregation typing are tricky
 	{
 		Query: "with recursive t (n) as (select sum('1') from dual union all select (2.00) from dual) select sum(n) from t;",
