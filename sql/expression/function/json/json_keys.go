73532305016490af7213c3c9c21526695a0a4155
James Cor <james@dolthub.com>
2024-01-30 14:39:32 -0800

    implemented, need tests

---
+++ sql/expression/function/json/json_keys.go
@@ -0,0 +1,149 @@
+// Copyright 2024 Dolthub, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package json
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/dolthub/jsonpath"
+
+	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/expression"
+	"github.com/dolthub/go-mysql-server/sql/types"
+)
+
+// JSONKeys (json_doc[, path])
+//
+// JSONKeys Returns the keys from the top-level value of a JSON object as a JSON array, or, if a path argument is given,
+// the top-level keys from the selected path. Returns NULL if any argument is NULL, the json_doc argument is not an
+// object, or path, if given, does not locate an object. An error occurs if the json_doc argument is not a valid JSON
+// document or the path argument is not a valid path expression or contains a * or ** wildcard. The result array is
+// empty if the selected object is empty. If the top-level value has nested subobjects, the return value does not
+// include keys from those subobjects.
+//
+// https://dev.mysql.com/doc/refman/8.0/en/json-search-functions.html#function_json-keys
+type JSONKeys struct {
+	JSON sql.Expression
+	Path sql.Expression
+}
+
+var _ sql.FunctionExpression = &JSONKeys{}
+
+// NewJSONKeys creates a new JSONKeys function.
+func NewJSONKeys(args ...sql.Expression) (sql.Expression, error) {
+	if len(args) == 1 {
+		return &JSONKeys{args[0], expression.NewLiteral("$", types.Text)}, nil
+	}
+	if len(args) == 2 {
+		return &JSONKeys{args[0], args[2]}, nil
+	}
+	return nil, sql.ErrInvalidArgumentNumber.New("JSON_VALID", "1 or 2", len(args))
+}
+
+// FunctionName implements sql.FunctionExpression
+func (j *JSONKeys) FunctionName() string {
+	return "json_valid"
+}
+
+// Description implements sql.FunctionExpression
+func (j *JSONKeys) Description() string {
+	return "returns whether JSON value is valid."
+}
+
+// IsUnsupported implements sql.UnsupportedFunctionStub
+func (j *JSONKeys) IsUnsupported() bool {
+	return false
+}
+
+// Resolved implements the sql.Expression interface.
+func (j *JSONKeys) Resolved() bool {
+	return j.JSON.Resolved()
+}
+
+// String implements the sql.Expression interface.
+func (j *JSONKeys) String() string {
+	return fmt.Sprintf("%s(%s)", j.Description(), j.JSON.String())
+}
+
+// Type implements the sql.Expression interface.
+func (j *JSONKeys) Type() sql.Type {
+	return types.JSON
+}
+
+// IsNullable implements the sql.Expression interface.
+func (j *JSONKeys) IsNullable() bool {
+	return j.JSON.IsNullable()
+}
+
+// Eval implements the sql.Expression interface.
+func (j *JSONKeys) Eval(ctx *sql.Context, row sql.Row) (interface{}, error) {
+	doc, err := getJSONDocumentFromRow(ctx, row, j.JSON)
+	if err != nil {
+		return nil, err
+	}
+	if doc == nil {
+		return nil, nil
+	}
+
+	pathVal, err := j.Path.Eval(ctx, row)
+	if err != nil {
+		return false, nil
+	}
+	if pathVal == nil {
+		return nil, nil
+	}
+	var path string
+	if p, _, strErr := types.LongText.Convert(pathVal); strErr == nil {
+		path = p.(string)
+	} else {
+		return nil, strErr
+	}
+
+	js, err := jsonpath.JsonPathLookup(doc.Val, path)
+	if err != nil {
+		return nil, nil
+	}
+
+	switch v := js.(type) {
+	case map[string]any:
+		var res []string
+		for k := range v {
+			res = append(res, k)
+		}
+		sort.Slice(res, func(i, j int) bool {
+			if len(res[i]) != len(res[j]) {
+				return len(res[i]) < len(res[j])
+			}
+			return res[i] < res[j]
+		})
+		return res, nil
+	default:
+		return nil, nil
+	}
+}
+
+// Children implements the Expression interface.
+func (j *JSONKeys) Children() []sql.Expression {
+	return []sql.Expression{j.JSON, j.Path}
+}
+
+// WithChildren implements the Expression interface.
+func (j *JSONKeys) WithChildren(children ...sql.Expression) (sql.Expression, error) {
+	if len(j.Children()) != len(children) {
+		return nil, fmt.Errorf("json_valid did not receive the correct amount of args")
+	}
+	return NewJSONKeys(children...)
+}
--- sql/expression/function/json/json_unsupported.go
+++ sql/expression/function/json/json_unsupported.go
@@ -27,42 +27,6 @@ var ErrUnsupportedJSONFunction = errors.NewKind("unsupported JSON function: %s")
 // JSON search functions //
 ///////////////////////////
 
-// JSON_KEYS(json_doc[, path])
-//
-// JSONKeys Returns the keys from the top-level value of a JSON object as a JSON array, or, if a path argument is given,
-// the top-level keys from the selected path. Returns NULL if any argument is NULL, the json_doc argument is not an
-// object, or path, if given, does not locate an object. An error occurs if the json_doc argument is not a valid JSON
-// document or the path argument is not a valid path expression or contains a * or ** wildcard. The result array is
-// empty if the selected object is empty. If the top-level value has nested subobjects, the return value does not
-// include keys from those subobjects.
-//
-// https://dev.mysql.com/doc/refman/8.0/en/json-search-functions.html#function_json-keys
-type JSONKeys struct {
-	sql.Expression
-}
-
-var _ sql.FunctionExpression = JSONKeys{}
-
-// NewJSONKeys creates a new JSONKeys function.
-func NewJSONKeys(args ...sql.Expression) (sql.Expression, error) {
-	return nil, ErrUnsupportedJSONFunction.New(JSONKeys{}.FunctionName())
-}
-
-// FunctionName implements sql.FunctionExpression
-func (j JSONKeys) FunctionName() string {
-	return "json_keys"
-}
-
-// Description implements sql.FunctionExpression
-func (j JSONKeys) Description() string {
-	return "array of keys from JSON document."
-}
-
-// IsUnsupported implements sql.UnsupportedFunctionStub
-func (j JSONKeys) IsUnsupported() bool {
-	return true
-}
-
 // JSON_OVERLAPS(json_doc1, json_doc2)
 //
 // JSONOverlaps Compares two JSON documents. Returns true (1) if the two document have any key-value pairs or array
