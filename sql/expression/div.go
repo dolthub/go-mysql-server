af48541e5637a25719505864a7e86ee7f8c2d652
jycor <jycor@users.noreply.github.com>
2024-02-15 21:58:33 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/expression/div.go
+++ sql/expression/div.go
@@ -19,7 +19,7 @@ import (
 	"math"
 	"strconv"
 	"strings"
-		"time"
+	"time"
 
 	"github.com/dolthub/vitess/go/vt/sqlparser"
 	"github.com/shopspring/decimal"
@@ -52,7 +52,7 @@ type Div struct {
 
 // NewDiv creates a new Div / sql.Expression.
 func NewDiv(left, right sql.Expression) *Div {
-	d := &Div{BinaryExpressionStub:        BinaryExpressionStub{LeftChild: left, RightChild: right}}
+	d := &Div{BinaryExpressionStub: BinaryExpressionStub{LeftChild: left, RightChild: right}}
 	setDivOps(d, countDivOps(d))
 	setArithmeticOps(d, countArithmeticOps(d))
 	return d
