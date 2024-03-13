a832e8662590090d0778636b6b898bbf3df82fa7
James Cor <james@dolthub.com>
2024-03-13 10:44:25 -0700

    partial

--- sql/expression/function/json/json_unsupported.go
+++ sql/expression/function/json/json_unsupported.go
@@ -23,61 +23,6 @@ import (
 // ErrUnsupportedJSONFunction is returned when a unsupported JSON function is called.
 var ErrUnsupportedJSONFunction = errors.NewKind("unsupported JSON function: %s")
 
-///////////////////////////
-// JSON search functions //
-///////////////////////////
-
-// JSON_SEARCH(json_doc, one_or_all, search_str[, escape_char[, path] ...])
-//
-// JSONSearch Returns the path to the given string within a JSON document. Returns NULL if any of the json_doc,
-// search_str, or path arguments are NULL; no path exists within the document; or search_str is not found. An error
-// occurs if the json_doc argument is not a valid JSON document, any path argument is not a valid path expression,
-// one_or_all is not 'one' or 'all', or escape_char is not a constant expression.
-// The one_or_all argument affects the search as follows:
-//   - 'one': The search terminates after the first match and returns one path string. It is undefined which match is
-//     considered first.
-//   - 'all': The search returns all matching path strings such that no duplicate paths are included. If there are
-//     multiple strings, they are autowrapped as an array. The order of the array elements is undefined.
-//
-// Within the search_str search string argument, the % and _ characters work as for the LIKE operator: % matches any
-// number of characters (including zero characters), and _ matches exactly one character.
-//
-// To specify a literal % or _ character in the search string, precede it by the escape character. The default is \ if
-// the escape_char argument is missing or NULL. Otherwise, escape_char must be a constant that is empty or one character.
-// For more information about matching and escape character behavior, see the description of LIKE in Section 12.8.1,
-// “String Comparison Functions and Operators”: https://dev.mysql.com/doc/refman/8.0/en/string-comparison-functions.html
-// For escape character handling, a difference from the LIKE behavior is that the escape character for JSON_SEARCH()
-// must evaluate to a constant at compile time, not just at execution time. For example, if JSON_SEARCH() is used in a
-// prepared statement and the escape_char argument is supplied using a ? parameter, the parameter value might be
-// constant at execution time, but is not at compile time.
-//
-// https://dev.mysql.com/doc/refman/8.0/en/json-search-functions.html#function_json-search
-type JSONSearch struct {
-	sql.Expression
-}
-
-var _ sql.FunctionExpression = JSONSearch{}
-
-// NewJSONSearch creates a new NewJSONSearch function.
-func NewJSONSearch(args ...sql.Expression) (sql.Expression, error) {
-	return nil, ErrUnsupportedJSONFunction.New(JSONSearch{}.FunctionName())
-}
-
-// FunctionName implements sql.FunctionExpression
-func (j JSONSearch) FunctionName() string {
-	return "json_search"
-}
-
-// Description implements sql.FunctionExpression
-func (j JSONSearch) Description() string {
-	return "path to value within JSON document."
-}
-
-// IsUnsupported implements sql.UnsupportedFunctionStub
-func (j JSONSearch) IsUnsupported() bool {
-	return true
-}
-
 // value MEMBER OF(json_array)
 //
 // Returns true (1) if value is an element of json_array, otherwise returns false (0). value must be a scalar or a JSON
