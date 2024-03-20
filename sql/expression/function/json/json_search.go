d6df0ce2c55a578bdd1c2fc09822f4689b1ded87
James Cor <james@dolthub.com>
2024-03-20 14:36:06 -0700

    implement and some tests

--- sql/expression/function/json/json_search.go
+++ sql/expression/function/json/json_search.go
@@ -15,8 +15,14 @@
 package json
 
 import (
-					"github.com/dolthub/go-mysql-server/sql"
-		)
+	"fmt"
+	"gopkg.in/src-d/go-errors.v1"
+	"strings"
+
+	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/types"
+	"github.com/dolthub/jsonpath"
+)
 
 // JSONSearch (json_doc, one_or_all, search_str[, escape_char[, path] ...])
 //
@@ -46,16 +52,42 @@ import (
 type JSONSearch struct {
 	JSON      sql.Expression
 	OneOrAll  sql.Expression
-	SearchStr sql.Expression
+	Search    sql.Expression
 	Escape    sql.Expression
 	Path      sql.Expression
 }
+
+var errOneOrAll = fmt.Errorf("the oneOrAll argument to json_search may take these values: 'one' or 'all'")
 
 var _ sql.FunctionExpression = &JSONSearch{}
 
 // NewJSONSearch creates a new NewJSONSearch function.
 func NewJSONSearch(args ...sql.Expression) (sql.Expression, error) {
-	return nil, ErrUnsupportedJSONFunction.New(JSONSearch{}.FunctionName())
+	switch len(args) {
+	case 3:
+		return &JSONSearch{
+			JSON:     args[0],
+			OneOrAll: args[1],
+			Search:   args[2],
+		}, nil
+	case 4:
+		return &JSONSearch{
+			JSON:     args[0],
+			OneOrAll: args[1],
+			Search:   args[2],
+			Escape:   args[3],
+		}, nil
+	case 5:
+		return &JSONSearch{
+			JSON:     args[0],
+			OneOrAll: args[1],
+			Search:   args[2],
+			Escape:   args[3],
+			Path:     args[4],
+		}, nil
+	default:
+		return nil, sql.ErrInvalidArgumentNumber.New("JSON_SEARCH", "3 to 5", len(args))
+	}
 }
 
 // FunctionName implements sql.FunctionExpression
@@ -68,37 +100,181 @@ func (j *JSONSearch) Description() string {
 	return "path to value within JSON document."
 }
 
-func (j *JSONSearch)Resolved() bool {
-	//TODO implement me
-panic("implement me")
+// Resolved implements sql.Expression
+func (j *JSONSearch) Resolved() bool {
+	return j.JSON.Resolved() &&
+		j.OneOrAll.Resolved() &&
+		j.Search.Resolved() &&
+		(j.Escape == nil || j.Escape.Resolved()) &&
+		(j.Path == nil || j.Path.Resolved())
 }
 
-func (j *JSONSearch)String() string {
-	//TODO implement me
-panic("implement me")
+// String implements sql.Expression
+func (j *JSONSearch) String() string {
+	// TODO: maybe just don't print if escape/path are nil?
+	var escapeStr, pathStr string
+	if j.Escape == nil {
+		escapeStr = "NULL"
+	} else {
+		escapeStr = j.Escape.String()
+	}
+	if j.Path == nil {
+		pathStr = "NULL"
+	} else {
+		pathStr = j.Path.String()
+	}
+	return fmt.Sprintf("%s(%s, %s, %s, %s, %s)",
+		j.FunctionName(),
+		j.JSON.String(),
+		j.OneOrAll.String(),
+		j.Search.String(),
+		escapeStr,
+		pathStr,
+	)
 }
 
-func (j *JSONSearch)Type() sql.Type {
-	//TODO implement me
-panic("implement me")
+// Type implements sql.Expression
+func (j *JSONSearch) Type() sql.Type {
+	return types.JSON
 }
 
-func (j *JSONSearch)IsNullable() bool {
-	//TODO implement me
-panic("implement me")
+// IsNullable implements sql.Expression
+func (j *JSONSearch) IsNullable() bool {
+	return j.JSON.IsNullable() ||
+		j.OneOrAll.IsNullable() ||
+		j.Search.IsNullable() ||
+		(j.Escape != nil && j.Escape.IsNullable()) ||
+		(j.Path != nil && j.Path.IsNullable())
 }
 
-func (j *JSONSearch)Eval(ctx *sql.Context, row sql.Row) ( interface{},  error) {
-	//TODO implement me
-panic("implement me")
+func jsonSearch(json interface{}, searchVal string, currPath string, once bool) ([]string, bool) {
+	switch j := json.(type) {
+	case string:
+		if j == searchVal {
+			// Need to format the path as a JSON string
+			return []string{fmt.Sprintf(`"%s"`, currPath)}, once
+		}
+		return nil, false
+	case []interface{}:
+		var results []string
+		for i, v := range j {
+			path := fmt.Sprintf("%s[%d]", currPath, i)
+			res, stop := jsonSearch(v, searchVal, path, once)
+			if stop {
+				return res, true
+			}
+			results = append(results, res...)
+		}
+		return results, false
+	case map[string]interface{}:
+		var results []string
+		for k, v := range j {
+			path := fmt.Sprintf("%s.%s", currPath, k)
+			res, stop := jsonSearch(v, searchVal, path, once)
+			if stop {
+				return res, true
+			}
+			results = append(results, res...)
+		}
+		return results, false
+	default:
+		return nil, false
+	}
 }
 
-func (j *JSONSearch)Children() []sql.Expression {
-	//TODO implement me
-panic("implement me")
+// Eval implements sql.Expression
+func (j *JSONSearch) Eval(ctx *sql.Context, row sql.Row) (interface{}, error) {
+	span, ctx := ctx.Span(fmt.Sprintf("function.%s", j.FunctionName()))
+	defer span.End()
+
+	doc, err := getJSONDocumentFromRow(ctx, row, j.JSON)
+	if err != nil {
+		return nil, err
+	}
+	if doc == nil {
+		return nil, nil
+	}
+
+	oneOrAll, err := j.OneOrAll.Eval(ctx, row)
+	if err != nil {
+		return nil, err
+	}
+	if oneOrAll == nil {
+		return nil, nil
+	}
+	oneOrAll, _, err = types.Text.Convert(oneOrAll)
+	if err != nil {
+		return nil, err
+	}
+	oneOrAllStr := oneOrAll.(string)
+	var isOne bool
+	switch strings.ToLower(oneOrAllStr) {
+	case "one":
+		isOne = true
+	case "all":
+		isOne = false
+	default:
+		return nil, errOneOrAll
+	}
+
+	search, err := j.Search.Eval(ctx, row)
+	if err != nil {
+		return nil, err
+	}
+	if search == nil {
+		return nil, nil
+	}
+	search, _, err = types.Text.Convert(search)
+	if err != nil {
+		return nil, err
+	}
+	searchStr := search.(string)
+
+	if j.Escape != nil {
+		// TODO: implement escape character handling
+	}
+
+	path := "$"
+	if j.Path != nil {
+		newPath, err := buildPath(ctx, j.Path, row)
+		if err != nil {
+			return nil, err
+		}
+		if newPath == nil {
+			return nil, nil
+		}
+		path = newPath.(string)
+	}
+	js, err := jsonpath.JsonPathLookup(doc.Val, path)
+	if err != nil {
+		if errors.Is(err, jsonpath.ErrKeyError) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	res, _ := jsonSearch(js, searchStr, path, isOne)
+	if len(res) > 0 && isOne {
+		results, _, err := types.JSON.Convert(res[0])
+		if err != nil {
+			return nil, err
+		}
+		return results, nil
+	}
+
+	results, _, err := types.JSON.Convert(res)
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
 }
 
-func (j *JSONSearch)WithChildren(children ...sql.Expression) ( sql.Expression,  error) {
-	//TODO implement me
-panic("implement me")
+// Children implements sql.Expression
+func (j *JSONSearch) Children() []sql.Expression {
+	return []sql.Expression{j.JSON, j.OneOrAll, j.Search, j.Escape, j.Path}
+}
+
+// WithChildren implements sql.Expression
+func (j *JSONSearch) WithChildren(children ...sql.Expression) (sql.Expression, error) {
+	return NewJSONSearch(children...)
 }
---
+++ sql/expression/function/json/json_search_test.go
@@ -0,0 +1,118 @@
+// Copyright 2024 Dolthub, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package json
+
+import (
+	"fmt"
+"strings"
+"testing"
+
+	"github.com/stretchr/testify/require"
+	"gopkg.in/src-d/go-errors.v1"
+
+	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/expression"
+	"github.com/dolthub/go-mysql-server/sql/types"
+)
+
+func TestJSONSearch(t *testing.T) {
+	_, err := NewJSONSearch()
+	require.True(t, errors.Is(err, sql.ErrInvalidArgumentNumber))
+
+	_, err = NewJSONSearch(
+		expression.NewGetField(0, types.LongText, "arg1", false),
+	)
+	require.True(t, errors.Is(err, sql.ErrInvalidArgumentNumber))
+
+	_, err = NewJSONSearch(
+		expression.NewGetField(0, types.LongText, "arg1", false),
+		expression.NewGetField(1, types.LongText, "arg2", false),
+	)
+	require.True(t, errors.Is(err, sql.ErrInvalidArgumentNumber))
+
+	_, err = NewJSONSearch(
+		expression.NewGetField(0, types.LongText, "arg1", false),
+		expression.NewGetField(1, types.LongText, "arg2", false),
+		expression.NewGetField(2, types.LongText, "arg3", false),
+		expression.NewGetField(3, types.LongText, "arg4", false),
+		expression.NewGetField(4, types.LongText, "arg5", false),
+		expression.NewGetField(5, types.LongText, "arg6", false),
+	)
+	require.True(t, errors.Is(err, sql.ErrInvalidArgumentNumber))
+
+	f3 := buildGetFieldExpressions(t, NewJSONSearch, 3)
+	//f4 := buildGetFieldExpressions(t, NewJSONSearch, 4)
+	//f5 := buildGetFieldExpressions(t, NewJSONSearch, 5)
+
+	json := `["abc", [{"k": "10"}, "def"], {"x":"abc"}, {"y":"bcd"}]`
+
+	testCases := []struct {
+		f   sql.Expression
+		row sql.Row
+		exp interface{}
+		err bool
+	}{
+		{
+			f:   f3,
+			row: sql.Row{"", "one", "abc"},
+			err: true,
+		},
+		{
+			f:   f3,
+			row: sql.Row{json, "NotOneOrAll", "abc"},
+			err: true,
+		},
+
+		{
+			f:   f3,
+			row: sql.Row{nil, "one", "abc"},
+			exp: nil,
+		},
+		{
+			f:   f3,
+			row: sql.Row{json, nil, "abc"},
+			exp: nil,
+		},
+		{
+			f:   f3,
+			row: sql.Row{json, "one", nil},
+			exp: nil,
+		},
+
+		{
+			f:   f3,
+			row: sql.Row{json, "one", "abc"},
+			exp: types.MustJSON(`"$[0]"`),
+		},
+
+	}
+
+	for _, tt := range testCases {
+		var args []string
+		for _, a := range tt.row {
+			args = append(args, fmt.Sprintf("%v", a))
+		}
+		t.Run(strings.Join(args, ", "), func(t *testing.T) {
+			require := require.New(t)
+			res, err := tt.f.Eval(sql.NewEmptyContext(), tt.row)
+			if tt.err {
+				require.Error(err)
+				return
+			}
+			require.NoError(err)
+			require.Equal(tt.exp, res)
+		})
+	}
+}
