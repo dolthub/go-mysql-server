028f2ab19df27c9297bff68defed1d9a4b6b9fbf
James Cor <james@dolthub.com>
2024-03-29 13:57:02 -0700

    skip result check

--- enginetest/queries/script_queries.go
+++ enginetest/queries/script_queries.go
@@ -7010,12 +7010,14 @@ var PreparedScriptTests = []ScriptTest{
 				},
 			},
 			{
+				SkipResultCheckOnServerEngine: true,
 				Query: "execute sd using @d;",
 				Expected: []sql.Row{
 					{types.NewOkResult(1)},
 				},
 			},
 			{
+				SkipResultCheckOnServerEngine: true,
 				Query: "execute sdt using @dt;",
 				Expected: []sql.Row{
 					{types.NewOkResult(1)},
@@ -7024,12 +7026,14 @@ var PreparedScriptTests = []ScriptTest{
 			{
 				// types.Timespan not supported as bindvar
 				Skip: true,
+				SkipResultCheckOnServerEngine: true,
 				Query: "execute st using @t;",
 				Expected: []sql.Row{
 					{types.NewOkResult(1)},
 				},
 			},
 			{
+				SkipResultCheckOnServerEngine: true,
 				Query: "execute sts using @ts;",
 				Expected: []sql.Row{
 					{types.NewOkResult(1)},
