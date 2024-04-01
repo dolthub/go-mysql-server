d999ef6fea619b09de665aecc1f95a5e752f91a9
James Cor <james@dolthub.com>
2024-04-01 13:10:13 -0700

    Formatting

--- enginetest/queries/variable_queries.go
+++ enginetest/queries/variable_queries.go
@@ -15,10 +15,10 @@
 package queries
 
 import (
-	"github.com/dolthub/go-mysql-server/sql/plan"
-"math"
+	"math"
 
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/plan"
 	"github.com/dolthub/go-mysql-server/sql/types"
 )
 
