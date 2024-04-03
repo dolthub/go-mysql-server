80d2ced2de6315facbdbffd3ecc7ca29eaa695cc
James Cor <james@dolthub.com>
2024-04-02 17:00:25 -0700

    no timeout

--- server/handler_test.go
+++ server/handler_test.go
@@ -1315,7 +1315,6 @@ func TestStatusVariableQuestions(t *testing.T) {
 }
 
 const waitTimeout = 250 * time.Millisecond
-const pauseDuration = 10 * time.Millisecond
 
 func checkGlobalStatVar(t *testing.T, name string, expected uint64) {
 	start := time.Now()
@@ -1327,7 +1326,6 @@ func checkGlobalStatVar(t *testing.T, name string, expected uint64) {
 		if globalVal == expected {
 			return
 		}
-		time.Sleep(pauseDuration)
 	}
 	require.Fail(t, "expected global status variable %s to be %d, got %d", name, expected, globalVal)
 }
@@ -1342,7 +1340,6 @@ func checkSessionStatVar(t *testing.T, sess sql.Session, name string, expected u
 		if sessVal == expected {
 			return
 		}
-		time.Sleep(pauseDuration)
 	}
 	require.Fail(t, "expected session status variable %s to be %d, got %d", name, expected, sessVal)
 }
