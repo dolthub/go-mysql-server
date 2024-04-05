e996a44fb131ddc2d36334e730c4820e56a88076
James Cor <james@dolthub.com>
2024-04-05 13:31:30 -0700

    skip

--- enginetest/memory_engine_test.go
+++ enginetest/memory_engine_test.go
@@ -206,6 +206,7 @@ func newUpdateResult(matched, updated int) types.OkResult {
 
 // Convenience test for debugging a single query. Unskip and set to the desired query.
 func TestSingleScript(t *testing.T) {
+	t.Skip()
 	var scripts = []queries.ScriptTest{
 		{
 			Name: "test index naming",
