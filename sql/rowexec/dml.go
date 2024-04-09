6b7f01b5447cd38378e605b940afe0cfcab266ba
Nick Tobey <nick@dolthub.com>
2024-04-09 13:43:37 -0700

    Respond to PR feedback.

--- sql/rowexec/dml.go
+++ sql/rowexec/dml.go
@@ -15,6 +15,7 @@
 package rowexec
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 
@@ -58,16 +59,16 @@ func (b *BaseBuilder) buildInsertInto(ctx *sql.Context, ii *plan.InsertInto, row
 	insertExpressions := getInsertExpressions(ii.Source)
 	if ii.HasUnspecifiedAutoInc {
 		_, i, _ := sql.SystemVariables.GetGlobal("innodb_autoinc_lock_mode")
-		lockMode := i.(int64)
-		if err != nil {
-			return nil, err
+		lockMode, ok := i.(int64)
+		if !ok {
+			return nil, errors.New(fmt.Sprintf("unexpected type for innodb_autoinc_lock_mode, expected int64, got %T", i))
 		}
 		// Lock modes "traditional" (0) and "consecutive" (1) require that a single lock is held for the entire iteration.
 		// Lock mode "interleaved" (2) will acquire the lock only when inserting into the table.
 		if lockMode != 2 {
 			autoIncrementable, ok := sql.GetUnderlyingTable(insertable).(sql.AutoIncrementTable)
 			if !ok {
-				panic("Auto increment expression on non-AutoIncrement table. This should not be possible.")
+				return nil, errors.New("auto increment expression on non-AutoIncrement table. This should not be possible")
 			}
 
 			unlocker, err = autoIncrementable.AutoIncrementSetter(ctx).AcquireAutoIncrementLock(ctx)
--- sql/variables/system_variables.go
+++ sql/variables/system_variables.go
@@ -1029,9 +1029,8 @@ var systemVars = map[string]sql.SystemVariable{
 		Scope:             sql.GetMysqlScope(sql.SystemVariableScope_Global),
 		Dynamic:           false,
 		SetVarHintApplies: false,
-		// TODO: lower bound should be 0: https://github.com/dolthub/dolt/issues/7634
-		Type:    types.NewSystemIntType("innodb_autoinc_lock_mode", 0, 2, false),
-		Default: int64(2),
+		Type:              types.NewSystemIntType("innodb_autoinc_lock_mode", 0, 2, false),
+		Default:           int64(2),
 	},
 	// Row locking is currently not supported. This variable is provided for 3p tools, and we always return the
 	// Lowest value allowed by MySQL, which is 1. If you attempt to set this value to anything other than 1, errors ensue.
