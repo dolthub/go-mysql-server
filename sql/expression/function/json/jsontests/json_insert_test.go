8fa6a46c782edc6577dabbbfa40ba5e0ad853632
Nick Tobey <nick@dolthub.com>
2024-06-04 16:24:10 -0700

    Move `json_function_tests.go` and json tests that depend on it to their own package.
    This ensures that non-test code in `sql/expression/function/json` doesn't depend on `testify`, which is a library that we only want to depend on for tests.

--- sql/expression/function/json/jsontests/json_insert_test.go
+++ sql/expression/function/json/jsontests/json_insert_test.go
@@ -12,19 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package json
+package jsontests
 
 import (
+	"github.com/dolthub/go-mysql-server/sql/expression/function/json"
+	"gopkg.in/src-d/go-errors.v1"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-	"gopkg.in/src-d/go-errors.v1"
-
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/stretchr/testify/require"
 )
 
 func TestInsert(t *testing.T) {
-	_, err := NewJSONInsert()
+	_, err := json.NewJSONInsert()
 	require.True(t, errors.Is(err, sql.ErrInvalidArgumentNumber))
 
 	for _, format := range jsonFormatTests {
