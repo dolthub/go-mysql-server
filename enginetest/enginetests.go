8baa9e4c474218188642d10514d56f1bea4fd5dd
Maximilian Hoffman <max@dolthub.com>
2024-06-17 15:50:33 -0700

    [no-release-notes] add tests for index lookup precision loss+overflow (#2554)
    * [no-release-notes] add tests for index lookup precision loss+overflow
    
    * simplify

--- enginetest/enginetests.go
+++ enginetest/enginetests.go
@@ -1633,6 +1633,21 @@ func TestScripts(t *testing.T, harness Harness) {
 	}
 }
 
+func TestNumericErrorScripts(t *testing.T, harness Harness) {
+	harness.Setup(setup.MydbData)
+	for _, script := range queries.NumericErrorQueries {
+		if sh, ok := harness.(SkippingHarness); ok {
+			if sh.SkipQueryTest(script.Name) {
+				t.Run(script.Name, func(t *testing.T) {
+					t.Skip(script.Name)
+				})
+				continue
+			}
+		}
+		TestScript(t, harness, script)
+	}
+}
+
 func TestSpatialScripts(t *testing.T, harness Harness) {
 	harness.Setup(setup.MydbData)
 	for _, script := range queries.SpatialScriptTests {
