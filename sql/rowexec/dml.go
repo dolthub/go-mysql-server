1eb87e4e13dd9d3767f8abab00dc8b24295edab5
James Cor <james@dolthub.com>
2024-06-20 16:15:41 -0700

    feedback

--- sql/rowexec/dml.go
+++ sql/rowexec/dml.go
@@ -384,10 +384,6 @@ func (b *BaseBuilder) buildRowUpdateAccumulator(ctx *sql.Context, n *plan.RowUpd
 				iter = i.InnerIter()
 			case *triggerIter:
 				iter = i.child
-				if uji, ok := i.child.(*updateJoinIter); ok {
-					uji.accumulator = rowHandler.(*updateJoinRowHandler)
-					done = true
-				}
 			default:
 				return nil, fmt.Errorf("failed to apply rowHandler to updateJoin, unknown type: %T", iter)
 			}
