3851e977fb9f45b6dbdf65bf429005e8d484baf7
jennifersp <jennifer@dolthub.com>
2024-06-24 16:22:29 -0700

    Revert "fix"
    This reverts commit 6d33a93071a13aa6e7ff54d6ef438d542c94e7ac.

--- sql/performance_schema/performance_schema.go
+++ sql/performance_schema/performance_schema.go
@@ -57,7 +57,3 @@ func (db performanceSchemaDatabase) GetTableNames(ctx *sql.Context) ([]string, e
 
 	return tblNames, nil
 }
-
-func (db performanceSchemaDatabase) SchemaName() string {
-	return db.name
-}
