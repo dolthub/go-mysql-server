dacd26c608d40afd3e6ea311293f0aaeb83f040e
jycor <jycor@users.noreply.github.com>
2024-07-31 21:56:50 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/rowexec/dml.go
+++ sql/rowexec/dml.go
@@ -79,19 +79,19 @@ func (b *BaseBuilder) buildInsertInto(ctx *sql.Context, ii *plan.InsertInto, row
 		}
 	}
 	insertIter := &insertIter{
-		schema:              dstSchema,
-		tableNode:           ii.Destination,
-		inserter:            inserter,
-		replacer:            replacer,
-		updater:             updater,
-		rowSource:           rowIter,
-		autoAutoIncIdx:      ii.UnspecifiedAutoIncIdx,
-		unlocker:            unlocker,
-		updateExprs:         ii.OnDupExprs,
-		insertExprs:         insertExpressions,
-		checks:              ii.Checks(),
-		ctx:                 ctx,
-		ignore:              ii.Ignore,
+		schema:         dstSchema,
+		tableNode:      ii.Destination,
+		inserter:       inserter,
+		replacer:       replacer,
+		updater:        updater,
+		rowSource:      rowIter,
+		autoAutoIncIdx: ii.UnspecifiedAutoIncIdx,
+		unlocker:       unlocker,
+		updateExprs:    ii.OnDupExprs,
+		insertExprs:    insertExpressions,
+		checks:         ii.Checks(),
+		ctx:            ctx,
+		ignore:         ii.Ignore,
 	}
 
 	var ed sql.EditOpenerCloser
