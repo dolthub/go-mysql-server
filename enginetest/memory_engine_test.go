7102cf374093f6fda00a65d2ed1171a245169e7c
jennifersp <jennifer@dolthub.com>
2024-09-16 15:53:41 -0700

    skip test

--- enginetest/memory_engine_test.go
+++ enginetest/memory_engine_test.go
@@ -145,7 +145,7 @@ func TestBrokenJSONTableScripts(t *testing.T) {
 
 // Convenience test for debugging a single query. Unskip and set to the desired query.
 func TestSingleQuery(t *testing.T) {
-	//t.Skip()
+	t.Skip()
 	var test queries.QueryTest
 	test = queries.QueryTest{
 		Query:    `select now() = sysdate(), sleep(0.1), now(6) < sysdate(6);`,
@@ -175,7 +175,6 @@ func TestSingleQueryPrepared(t *testing.T) {
 			{
 				// Original Issue: https://github.com/dolthub/dolt/issues/5714
 				Query: `select 1.0/0.0 from dual`,
-
 				Expected: []sql.Row{
 					{4},
 				},
