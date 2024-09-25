60e73355dc7c7c1b8c84445d6eb3b0257e894d23
Nick Tobey <nick@dolthub.com>
2024-09-25 14:30:41 -0700

    Respond to PR feedback.

--- sql/core.go
+++ sql/core.go
@@ -877,3 +877,24 @@ func IncrementStatusVariable(ctx *Context, name string, val int) {
 	StatusVariables.IncrementGlobal(name, val)
 	ctx.Session.IncrementStatusVariable(ctx, name, val)
 }
+
+// OrderAndLimit stores the context of an ORDER BY ... LIMIT statement, and is used by index lookups and iterators.
+type OrderAndLimit struct {
+	OrderBy       Expression
+	Limit         Expression
+	CalcFoundRows bool
+}
+
+func (v OrderAndLimit) DebugString() string {
+	if v.Limit != nil {
+		return fmt.Sprintf("%v LIMIT %v", DebugString(v.OrderBy), DebugString(v.Limit))
+	}
+	return DebugString(v.OrderBy)
+}
+
+func (v OrderAndLimit) String() string {
+	if v.Limit != nil {
+		return fmt.Sprintf("%v LIMIT %v", v.OrderBy, v.Limit)
+	}
+	return v.OrderBy.String()
+}
