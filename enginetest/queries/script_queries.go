9ee86c83dd069497b9712fe0a33d216063a87e19
James Cor <jcor@ucsd.edu>
2024-10-11 14:58:35 -0700

    clear warnings better and separate warning count from actual warnings (#2696)

--- enginetest/queries/script_queries.go
+++ enginetest/queries/script_queries.go
@@ -8809,20 +8809,16 @@ var CreateDatabaseScripts = []ScriptTest{
 				Expected: []sql.Row{{types.NewOkResult(1)}},
 			},
 			{
-				SkipResultCheckOnServerEngine: true, // tracking issue here, https://github.com/dolthub/dolt/issues/6921. Also for when run with prepares, the warning is added twice
-				Query:                         "SHOW WARNINGS /* 1 */",
-				Expected:                      []sql.Row{{"Warning", 1235, "Setting CHARACTER SET, COLLATION and ENCRYPTION are not supported yet"}},
+				Query:    "SHOW WARNINGS /* 1 */",
+				Expected: []sql.Row{{"Warning", 1235, "Setting CHARACTER SET, COLLATION and ENCRYPTION are not supported yet"}},
 			},
 			{
 				Query:    "CREATE DATABASE newtest1db DEFAULT COLLATE binary ENCRYPTION='Y'",
 				Expected: []sql.Row{{types.NewOkResult(1)}},
 			},
 			{
-				SkipResultCheckOnServerEngine: true, // tracking issue here, https://github.com/dolthub/dolt/issues/6921.
-				// TODO: There should only be one warning (the warnings are not clearing for create database query) AND 'PREPARE' statements should not create warning from its query
 				Query: "SHOW WARNINGS /* 2 */",
 				Expected: []sql.Row{
-					{"Warning", 1235, "Setting CHARACTER SET, COLLATION and ENCRYPTION are not supported yet"},
 					{"Warning", 1235, "Setting CHARACTER SET, COLLATION and ENCRYPTION are not supported yet"},
 				},
 			},
@@ -8939,10 +8935,8 @@ var DropDatabaseScripts = []ScriptTest{
 				Expected: []sql.Row{{types.OkResult{RowsAffected: 1}}},
 			},
 			{
-				// TODO: there should not be warning
-				//  https://github.com/dolthub/dolt/issues/6921
 				Query:    "SHOW WARNINGS",
-				Expected: []sql.Row{{"Note", 1008, "Can't drop database mydb; database doesn't exist "}},
+				Expected: []sql.Row{},
 			},
 			{
 				Query:    "SELECT DATABASE()",
