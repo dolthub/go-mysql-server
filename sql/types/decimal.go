ed790cbfc5a7f9d2ed782f775b55a9a09e1e7ab0
max-hoffman <max-hoffman@users.noreply.github.com>
2024-12-10 04:50:22 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/types/decimal.go
+++ sql/types/decimal.go
@@ -16,7 +16,6 @@ package types
 
 import (
 	"fmt"
-	"github.com/dolthub/go-mysql-server/sql/encodings"
 	"math/big"
 	"reflect"
 	"strings"
@@ -27,6 +26,7 @@ import (
 	"gopkg.in/src-d/go-errors.v1"
 
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/encodings"
 )
 
 const (
