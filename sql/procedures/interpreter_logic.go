15d34c0d3fc001e9f9c8602bcc69c128f85650bb
James Cor <james@dolthub.com>
2025-04-23 23:27:19 -0700

    progress

--- sql/procedures/interpreter_logic.go
+++ sql/procedures/interpreter_logic.go
@@ -147,6 +147,9 @@ func replaceVariablesInExpr(ctx *sql.Context, stack *InterpreterStack, expr ast.
 		if e.AsOf == nil && asOf != nil {
 			e.AsOf = asOf.Time
 		}
+		if len(e.ProcName.Qualifier.String()) == 0 {
+			e.ProcName.Qualifier = ast.NewTableIdent(stack.GetDatabase())
+		}
 	case *ast.Limit:
 		newOffset, err := replaceVariablesInExpr(ctx, stack, e.Offset, asOf)
 		if err != nil {
@@ -251,12 +254,18 @@ func replaceVariablesInExpr(ctx *sql.Context, stack *InterpreterStack, expr ast.
 			e.Values[i] = newExpr.(ast.ValTuple)
 		}
 	case *ast.Insert:
+		if asOf != nil {
+			return nil, sql.ErrProcedureCallAsOfReadOnly.New()
+		}
 		newExpr, err := replaceVariablesInExpr(ctx, stack, e.Rows, asOf)
 		if err != nil {
 			return nil, err
 		}
 		e.Rows = newExpr.(ast.InsertRows)
 	case *ast.Delete:
+		if asOf != nil {
+			return nil, sql.ErrProcedureCallAsOfReadOnly.New()
+		}
 		if e.Where != nil {
 			newExpr, err := replaceVariablesInExpr(ctx, stack, e.Where.Expr, asOf)
 			if err != nil {
@@ -265,6 +274,9 @@ func replaceVariablesInExpr(ctx *sql.Context, stack *InterpreterStack, expr ast.
 			e.Where.Expr = newExpr.(ast.Expr)
 		}
 	case *ast.Update:
+		if asOf != nil {
+			return nil, sql.ErrProcedureCallAsOfReadOnly.New()
+		}
 		if e.Where != nil {
 			newExpr, err := replaceVariablesInExpr(ctx, stack, e.Where.Expr, asOf)
 			if err != nil {
@@ -828,6 +840,9 @@ func Call(ctx *sql.Context, iNode InterpreterNode) (sql.RowIter, *InterpreterSta
 	var retSch sql.Schema
 	runner := iNode.GetRunner()
 	statements := iNode.GetStatements()
+	if dbNode, isDbNode := iNode.(sql.Databaser); isDbNode {
+		stack.SetDatabase(dbNode.Database().Name())
+	}
 	for {
 		counter++
 		if counter < 0 {
