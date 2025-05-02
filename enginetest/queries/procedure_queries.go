524c86f1f96e5fc39f045ab889565660a402043e
James Cor <jcor@ucsd.edu>
2025-05-02 10:38:17 -0700

    fix infinite recursion with recursive stored procedures (#2962)

--- enginetest/queries/procedure_queries.go
+++ enginetest/queries/procedure_queries.go
@@ -2192,6 +2192,70 @@ END;`,
 			},
 		},
 	},
+	{
+		Name: "recursive procedure",
+		SetUpScript: []string{
+			`
+create procedure recursive_proc(in counter int)
+begin
+  set counter := counter + 1;
+  if counter > 3 then
+    select concat('ended with value: ', counter) as result;
+  else
+    call recursive_proc(counter);
+  end if;
+end;`,
+		},
+		Assertions: []ScriptTestAssertion{
+			{
+				Query: "call recursive_proc(1);",
+				Expected: []sql.Row{
+					{"ended with value: 4"},
+				},
+			},
+		},
+	},
+	{
+		Name: "multi recursive procedures",
+		SetUpScript: []string{
+			`
+create procedure procA(in counter int)
+begin
+  set counter := counter + 1;
+  if counter > 3 then
+    select concat('ended in procA with value: ', counter) as result;
+  else
+    call procB(counter);
+  end if;
+end;
+`,
+			`
+create procedure procB(in counter int)
+begin
+  set counter := counter + 1;
+  if counter > 3 then
+    select concat('ended in procB with value: ', counter) as result;
+  else
+    call procA(counter);
+  end if;
+end;
+`,
+		},
+		Assertions: []ScriptTestAssertion{
+			{
+				Query: "call procA(1);",
+				Expected: []sql.Row{
+					{"ended in procA with value: 4"},
+				},
+			},
+			{
+				Query: "call procB(1);",
+				Expected: []sql.Row{
+					{"ended in procB with value: 4"},
+				},
+			},
+		},
+	},
 }
 
 var ProcedureCallTests = []ScriptTest{
