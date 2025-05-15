d500d0c1893e70149b704ad0e127125bf241be66
Nick Tobey <nick@dolthub.com>
2025-05-15 14:28:17 -0700

    Unwrap inputs to JSON_OBJECT.

--- enginetest/enginetests.go
+++ enginetest/enginetests.go
@@ -1907,7 +1907,7 @@ func TestComplexIndexQueriesPrepared(t *testing.T, harness Harness) {
 }
 
 func TestJsonScriptsPrepared(t *testing.T, harness Harness, skippedTests []string) {
-	harness.Setup(setup.MydbData)
+	harness.Setup(setup.MydbData, setup.BlobData)
 	for _, script := range queries.JsonScripts {
 		for _, skippedTest := range skippedTests {
 			if strings.Contains(script.Name, skippedTest) {
@@ -5154,6 +5154,7 @@ func TestNullRanges(t *testing.T, harness Harness) {
 }
 
 func TestJsonScripts(t *testing.T, harness Harness, skippedTests []string) {
+	harness.Setup(setup.MydbData, setup.BlobData)
 	for _, script := range queries.JsonScripts {
 		for _, skippedTest := range skippedTests {
 			if strings.Contains(script.Name, skippedTest) {
