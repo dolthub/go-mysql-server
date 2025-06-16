871149976240c4b84e66cc851346d0e6ac355de4
James Cor <james@dolthub.com>
2025-06-16 13:21:12 -0700

    test

--- enginetest/memory_engine_test.go
+++ enginetest/memory_engine_test.go
@@ -202,20 +202,20 @@ func TestSingleQueryPrepared(t *testing.T) {
 
 // Convenience test for debugging a single query. Unskip and set to the desired query.
 func TestSingleScript(t *testing.T) {
-	t.Skip()
+	//t.Skip()
 	var scripts = []queries.ScriptTest{
 		{
-			Name:        "AS OF propagates to nested CALLs",
+			Name:        "asdf",
 			SetUpScript: []string{},
 			Assertions: []queries.ScriptTestAssertion{
 				{
-					Query: "create procedure create_proc() create table t (i int primary key, j int);",
+					Query: "create table t (`order` int, check (`order` > 0))",
 					Expected: []sql.Row{
 						{types.NewOkResult(0)},
 					},
 				},
 				{
-					Query: "call create_proc()",
+					Query: "insert into t values (1)",
 					Expected: []sql.Row{
 						{types.NewOkResult(0)},
 					},
