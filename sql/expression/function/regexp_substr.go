856c0709c9cf5ddf5a491c782ee2cad007a8802f
James Cor <james@dolthub.com>
2025-06-17 15:16:48 -0700

    fix and benchmark regexp substr

--- sql/expression/function/regexp_substr.go
+++ sql/expression/function/regexp_substr.go
@@ -154,8 +154,8 @@ func (r *RegexpSubstr) String() string {
 // compile handles compilation of the regex.
 func (r *RegexpSubstr) compile(ctx *sql.Context, row sql.Row) {
 	r.compileOnce.Do(func() {
-		r.cacheRegex = canBeCached(r.Text, r.Pattern, r.Flags)
-		r.cacheVal = canBeCached(r.Text, r.Pattern, r.Position, r.Occurrence, r.Flags)
+		r.cacheRegex = canBeCached(r.Pattern, r.Flags)
+		r.cacheVal = r.cacheRegex && canBeCached(r.Text, r.Position, r.Occurrence)
 		if r.cacheRegex {
 			r.re, r.compileErr = compileRegex(ctx, r.Pattern, r.Text, r.Flags, r.FunctionName(), row)
 		}
