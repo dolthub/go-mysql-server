ee61b872b04016ca55cac2b952dda1dfe4045c2b
jycor <jycor@users.noreply.github.com>
2025-06-18 21:00:35 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/plan/hash_lookup.go
+++ sql/plan/hash_lookup.go
@@ -16,8 +16,9 @@ package plan
 
 import (
 	"fmt"
-	"github.com/dolthub/go-mysql-server/sql/expression"
 	"sync"
+
+	"github.com/dolthub/go-mysql-server/sql/expression"
 
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/hash"
