90ba90c8db5a76b260baffe86c7bc6307b108f96
Elian <elian.ddb@outlook.com>
2025-07-01 09:57:19 -0700

    add implicit default null

--- sql/planbuilder/ddl.go
+++ sql/planbuilder/ddl.go
@@ -1421,11 +1421,11 @@ func (b *Builder) tableSpecToSchema(inScope, outScope *scope, db sql.Database, t
 		schema[i].Default = b.convertDefaultExpression(outScope, def, schema[i].Type, schema[i].Nullable)
 		if schema[i].Default == nil && schema[i].Nullable {
 			schema[i].Default = &sql.ColumnDefaultValue{
-				Expr:          expression.NewLiteral(nil, schema[i].Type),
-				OutType:       nil,
+				Expr:          b.buildScalar(inScope, &ast.NullVal{}),
+				OutType:       types.Null,
 				Literal:       true,
-				Parenthesized: false,
 				ReturnNil:     true,
+				Parenthesized: false,
 			}
 		}
 		err := validateDefaultExprs(schema[i])
