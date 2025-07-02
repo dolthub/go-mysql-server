936afd9feafc22d24b2efba5cc9008270b80bd2e
Angela Xie <angela@dolthub.com>
2025-07-02 14:58:52 -0700

    add null case

--- enginetest/queries/script_queries.go
+++ enginetest/queries/script_queries.go
@@ -8733,19 +8733,19 @@ where
 		Name:    "subqueries should coerce union types",
 		Dialect: "mysql",
 		SetUpScript: []string{
-			"create table enum_table (i int primary key, e enum('a','b'))",
+			"create table enum_table (i int primary key, e enum('a','b') not null)",
 			"insert into enum_table values (1,'a'),(2,'b')",
 			"create table uv (u int primary key, v varchar(10))",
-			"insert into uv values (0, 'bug'),(1,'ant')",
+			"insert into uv values (0, 'bug'),(1,'ant'),(3, null)",
 		},
 		Assertions: []ScriptTestAssertion{
 			{
 				Query:    "select * from (select e from enum_table union select v from uv) sq",
-				Expected: []sql.Row{{"a"}, {"b"}, {"bug"}, {"ant"}},
+				Expected: []sql.Row{{"a"}, {"b"}, {"bug"}, {"ant"}, {nil}},
 			},
 			{
 				Query:    "with a as (select e from enum_table union select v from uv) select * from a",
-				Expected: []sql.Row{{"a"}, {"b"}, {"bug"}, {"ant"}},
+				Expected: []sql.Row{{"a"}, {"b"}, {"bug"}, {"ant"}, {nil}},
 			},
 		},
 	},
