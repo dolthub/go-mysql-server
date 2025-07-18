cc338d8e9b47b48ba8d6727f853c22ba89459a15
elianddb <elianddb@users.noreply.github.com>
2025-07-18 23:07:47 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/plan/alter_foreign_key.go
+++ sql/plan/alter_foreign_key.go
@@ -655,7 +655,7 @@ func foreignKeyComparableTypes(ctx *sql.Context, type1 sql.Type, type2 sql.Type)
 	if (types.IsTime(type1) || types.IsTimespan(type1)) && (types.IsTime(type2) || types.IsTimespan(type2)) {
 		// MySQL allows time-related types to reference each other in foreign keys:
 		// - DATETIME can reference DATETIME with different precision
-		// - TIMESTAMP can reference TIMESTAMP with different precision  
+		// - TIMESTAMP can reference TIMESTAMP with different precision
 		// - DATETIME can reference TIMESTAMP and vice versa
 		// - TIME can reference TIME with different precision
 		return true
