8e9d379755d73a9297c2c166fcf9d5283af08bf2
jycor <jycor@users.noreply.github.com>
2025-07-25 00:49:28 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- enginetest/queries/script_queries.go
+++ enginetest/queries/script_queries.go
@@ -8507,7 +8507,7 @@ where
 	},
 	{
 		// https://github.com/dolthub/dolt/issues/8728
-		Name:    "test merge join optimization (removing sort node over indexed tables) does not break ordering",
+		Name: "test merge join optimization (removing sort node over indexed tables) does not break ordering",
 		SetUpScript: []string{
 			"create table t1 (i int primary key);",
 			"create table t2 (j int primary key);",
