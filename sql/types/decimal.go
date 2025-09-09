759a42c7efbc0a6fd91647142ef0d62d521adfaf
James Cor <james@dolthub.com>
2025-09-09 10:14:58 -0700

    attempt at consolidating logic

--- sql/types/decimal.go
+++ sql/types/decimal.go
@@ -20,12 +20,12 @@ import (
 	"math/big"
 	"reflect"
 
-	"github.com/dolthub/vitess/go/sqltypes"
-	"github.com/dolthub/vitess/go/vt/proto/query"
 	"github.com/shopspring/decimal"
 	"gopkg.in/src-d/go-errors.v1"
 
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/vitess/go/sqltypes"
+	"github.com/dolthub/vitess/go/vt/proto/query"
 )
 
 const (
