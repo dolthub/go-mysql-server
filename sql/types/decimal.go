8888b237c542424bef3ce4baa8d56c0b2a1026b2
jycor <jycor@users.noreply.github.com>
2025-09-11 23:56:26 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- sql/types/decimal.go
+++ sql/types/decimal.go
@@ -17,10 +17,14 @@ package types
 import (
 	"context"
 	"fmt"
-	"github.com/shopspring/decimal"
-	"gopkg.in/src-d/go-errors.v1"
 	"math/big"
 	"reflect"
+
+	"github.com/dolthub/vitess/go/sqltypes"
+	"github.com/dolthub/vitess/go/vt/proto/query"
+	"github.com/shopspring/decimal"
+	"github.com/shopspring/decimal"
+	"gopkg.in/src-d/go-errors.v1"
 
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/vitess/go/sqltypes"
