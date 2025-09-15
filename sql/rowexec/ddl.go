52225fbf59399f978b9f3f5a6fe585283fef290e
elianddb <elian@dolthub.com>
2025-09-15 11:03:48 -0700

    add fallback for create schema

--- sql/rowexec/ddl.go
+++ sql/rowexec/ddl.go
@@ -405,8 +405,17 @@ func (b *BaseBuilder) buildCreateDB(ctx *sql.Context, n *plan.CreateDB, row sql.
 
 func (b *BaseBuilder) buildCreateSchema(ctx *sql.Context, n *plan.CreateSchema, row sql.Row) (sql.RowIter, error) {
 	database := ctx.GetCurrentDatabase()
+	
+	// If no database is selected, first try to fall back to CREATE DATABASE behavior
+	// since CREATE SCHEMA is a synonym for CREATE DATABASE in MySQL
+	// https://dev.mysql.com/doc/refman/8.4/en/create-database.html
 	if database == "" {
-		return nil, sql.ErrNoDatabaseSelected.New()
+		return b.buildCreateDB(ctx, &plan.CreateDB{
+			Catalog:     n.Catalog,
+			DbName:      n.DbName,
+			IfNotExists: n.IfNotExists,
+			Collation:   n.Collation,
+		}, row)
 	}
 
 	db, err := n.Catalog.Database(ctx, database)
