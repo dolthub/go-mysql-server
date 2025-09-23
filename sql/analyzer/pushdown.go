79e68b7c2df27382a4ca62cc8cd6f529bb1b6679
Angela Xie <angela@dolthub.com>
2025-09-23 14:10:10 -0700

    rename func to updateFilterNode

--- sql/analyzer/pushdown.go
+++ sql/analyzer/pushdown.go
@@ -96,7 +96,7 @@ func pushFilters(ctx *sql.Context, a *Analyzer, n sql.Node, scope *plan.Scope, s
 				return n, transform.SameTree, fmt.Errorf("pushdown mistakenly converted filter to non-filter: %T", ret)
 			}
 			// remove handled
-			newF := removePushedDownPredicates(ctx, a, retF, filters)
+			newF := updateFilterNode(ctx, a, retF, filters)
 			if newF != nil {
 				same = transform.NewTree
 				ret = newF
@@ -202,7 +202,7 @@ func transformPushdownSubqueryAliasFilters(ctx *sql.Context, a *Analyzer, n sql.
 		return transform.NodeWithCtx(n, filterPushdownChildSelector, func(c transform.Context) (sql.Node, transform.TreeIdentity, error) {
 			switch node := c.Node.(type) {
 			case *plan.Filter:
-				newF := removePushedDownPredicates(ctx, a, node, filters)
+				newF := updateFilterNode(ctx, a, node, filters)
 				if newF == nil {
 					return node, transform.SameTree, nil
 				}
@@ -311,9 +311,12 @@ func pushdownFiltersUnderSubqueryAlias(ctx *sql.Context, a *Analyzer, sa *plan.S
 	return n, transform.NewTree, nil
 }
 
-// removePushedDownPredicates removes all handled filter predicates from the filter given and returns. If all
-// predicates have been handled, it replaces the filter with its child.
-func removePushedDownPredicates(ctx *sql.Context, a *Analyzer, node *plan.Filter, filters *filterSet) sql.Node {
+// updateFilterNode updates the filter node based on the filter predicates handled. Any handled filter predicates are
+// removed from the filter node. If all filter predicates have been handled and there are no unhandled predicates, the
+// filter node is removed. If there are remaining filter predicates and the immediate child of the filter is a non-outer
+// join, the remaining unhandled filters are pushed into the join node and added to the join filters, and the filter
+// node is removed.
+func updateFilterNode(ctx *sql.Context, a *Analyzer, node *plan.Filter, filters *filterSet) sql.Node {
 	filterExpressions := expression.SplitConjunction(node.Expression)
 	unhandled := subtractExprSet(filterExpressions, filters.handledFilters)
 
