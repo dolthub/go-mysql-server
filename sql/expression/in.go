eabb468046391d42b759a8a8eac5b1d28cc9fd8a
James Cor <james@dolthub.com>
2025-09-26 01:55:57 -0700

    refactor in tuple logic

--- sql/expression/in.go
+++ sql/expression/in.go
@@ -16,6 +16,7 @@ package expression
 
 import (
 	"fmt"
+	"github.com/dolthub/vitess/go/mysql"
 
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/hash"
@@ -59,65 +60,120 @@ func NewInTuple(left sql.Expression, right sql.Expression) *InTuple {
 	return &InTuple{BinaryExpressionStub{left, right}}
 }
 
+// validateAndEvalRightTuple will evaluate the right tuple, check if leftType and the right Tuple are comparable,
+// determine what type to use to compare the two sides, and indicate if right Tuple contains any NULL elements.
+// The NULL handling for IN expressions is tricky. According to
+// https://dev.mysql.com/doc/refman/8.0/en/comparison-operators.html#operator_in:
+// To comply with the SQL standard, IN() returns NULL not only if the expression on the left hand side is NULL, but
+// also if no match is found in the list and one of the expressions in the list is NULL.
+func validateAndEvalRightTuple(ctx *sql.Context, lType sql.Type, right Tuple, row sql.Row) ([]any, sql.Type, bool, error) {
+	// If left is StringType and ANY of the right is NumberType, then we should use Double Type for comparison
+	// If left is NumberType and ANT of the left is StringType, then we should use Double Type for comparison
+	lColCount := types.NumColumns(lType)
+	lIsNumType := types.IsNumber(lType)
+	lIsStrType := types.IsText(lType)
+	var rHasNumType, rHasStrType, rHasNull bool
+	rVals := make([]any, len(right))
+	for i, el := range right {
+		rType := el.Type()
+
+		// Nested tuples must have the same number of columns
+		rColCount := types.NumColumns(rType)
+		if rColCount != lColCount {
+			return nil, nil, false, sql.ErrInvalidOperandColumns.New(lColCount, types.NumColumns(el.Type()))
+		}
+
+		if types.IsNumber(rType) {
+			rHasNumType = true
+		} else if types.IsText(rType) {
+			rHasStrType = true
+		}
+
+		// Null elements are not hashed into the Tuple Map
+		if types.IsNullType(rType) {
+			rHasNull = true
+			continue
+		}
+		v, err := el.Eval(ctx, row)
+		if err != nil {
+			return nil, nil, false, err
+		}
+		if v == nil {
+			rHasNull = true
+			continue
+		}
+
+		rVals[i] = v
+	}
+
+	var cmpType sql.Type
+	if (lIsStrType && rHasNumType) || (lIsNumType && rHasStrType) {
+		cmpType = types.Float64
+	} else if types.IsEnum(lType) || types.IsSet(lType) || types.IsText(lType) {
+		cmpType = lType
+	} else {
+		cmpType = types.GetCompareType(lType, right[0].Type())
+	}
+
+	return rVals, cmpType, rHasNull, nil
+}
+
 // Eval implements the Expression interface.
 func (in *InTuple) Eval(ctx *sql.Context, row sql.Row) (interface{}, error) {
-	leftElems := types.NumColumns(in.Left().Type())
-	originalLeft, err := in.Left().Eval(ctx, row)
+	leftVal, err := in.Left().Eval(ctx, row)
 	if err != nil {
 		return nil, err
 	}
-	if originalLeft == nil {
+	if leftVal == nil {
 		return nil, nil
 	}
 
-	// The NULL handling for IN expressions is tricky. According to
-	// https://dev.mysql.com/doc/refman/8.0/en/comparison-operators.html#operator_in:
-	// To comply with the SQL standard, IN() returns NULL not only if the expression on the left hand side is NULL, but
-	// also if no match is found in the list and one of the expressions in the list is NULL.
-	rightNull := false
-
-	switch right := in.Right().(type) {
-	case Tuple:
-		for _, el := range right {
-			if types.NumColumns(el.Type()) != leftElems {
-				return nil, sql.ErrInvalidOperandColumns.New(leftElems, types.NumColumns(el.Type()))
-			}
-		}
-
-		leftLit := NewLiteral(originalLeft, in.Left().Type())
-		for _, el := range right {
-			originalRight, err := el.Eval(ctx, row)
-			if err != nil {
-				return nil, err
-			}
-
-			if !rightNull && originalRight == nil {
-				rightNull = true
-				continue
-			}
-
-			comp := newComparison(leftLit, NewLiteral(originalRight, el.Type()))
-			l, r, compareType, err := comp.castLeftAndRight(ctx, originalLeft, originalRight)
-			if err != nil {
-				return nil, err
-			}
-			cmp, err := compareType.Compare(ctx, l, r)
-			if err != nil {
-				return nil, err
-			}
-			if cmp == 0 {
-				return true, nil
-			}
-		}
-
-		if rightNull {
-			return nil, nil
-		}
-
-		return false, nil
-	default:
+	right, isTuple := in.Right().(Tuple)
+	if !isTuple {
 		return nil, ErrUnsupportedInOperand.New(right)
 	}
+
+	lType := in.Left().Type()
+	rVals, cmpType, rHasNull, err := validateAndEvalRightTuple(ctx, lType, right, row)
+	if err != nil {
+		return nil, err
+	}
+
+	lv, _, lErr := cmpType.Convert(ctx, leftVal)
+	if lErr != nil {
+		if !sql.ErrTruncatedIncorrect.Is(lErr) {
+			return nil, lErr
+		}
+		ctx.Warn(mysql.ERTruncatedWrongValue, "%s", lErr.Error())
+	}
+
+	for _, rVal := range rVals {
+		if rVal == nil {
+			continue
+		}
+		rv, _, rErr := cmpType.Convert(ctx, rVal)
+		if rErr != nil {
+			if !sql.ErrTruncatedIncorrect.Is(rErr) {
+				return nil, rErr
+			}
+			ctx.Warn(mysql.ERTruncatedWrongValue, "%s", rErr.Error())
+		}
+		cmp, cErr := cmpType.Compare(ctx, lv, rv)
+		if cErr != nil {
+			if !sql.ErrTruncatedIncorrect.Is(cErr) {
+				return nil, cErr
+			}
+			ctx.Warn(mysql.ERTruncatedWrongValue, "%s", cErr.Error())
+		}
+		if cmp == 0 {
+			return true, nil
+		}
+	}
+	if rHasNull {
+		return nil, nil
+	}
+
+	return false, nil
 }
 
 // WithChildren implements the Expression interface.
@@ -191,60 +247,15 @@ func newInMap(ctx *sql.Context, lType sql.Type, right Tuple) (map[uint64]struct{
 	if len(right) == 0 {
 		return nil, nil, false, nil
 	}
-
-	// If left is StringType and ANY of the right is NumberType, then we should use Double Type for comparison
-	// If left is NumberType and ANT of the left is StringType, then we should use Double Type for comparison
-	lColumnCount := types.NumColumns(lType)
-	lIsNumType := types.IsNumber(lType)
-	lIsStrType := types.IsText(lType)
-	var rHasNumType, rHasStrType, rHasNull bool
-	rVals := make([]any, len(right))
-	for i, el := range right {
-		rType := el.Type()
-
-		// Nested tuples must have the same number of columns
-		rColumnCount := types.NumColumns(rType)
-		if rColumnCount != lColumnCount {
-			return nil, nil, false, sql.ErrInvalidOperandColumns.New(lColumnCount, rColumnCount)
-		}
-
-		if types.IsNumber(rType) {
-			rHasNumType = true
-		} else if types.IsText(rType) {
-			rHasStrType = true
-		}
-
-		// Null elements are not hashed into the Tuple Map
-		if types.IsNullType(rType) {
-			rHasNull = true
-			continue
-		}
-		v, err := el.Eval(ctx, sql.Row{})
-		if err != nil {
-			return nil, nil, false, err
-		}
-		if v == nil {
-			rHasNull = true
-			continue
-		}
-
-		rVals[i] = v
+	rVals, cmpType, rHasNull, err := validateAndEvalRightTuple(ctx, lType, right, nil)
+	if err != nil {
+		return nil, nil, false, err
 	}
-
-	var cmpType sql.Type
-	if (lIsStrType && rHasNumType) || (lIsNumType && rHasStrType) {
-		cmpType = types.Float64
-	} else if types.IsEnum(lType) || types.IsSet(lType) || types.IsText(lType) {
-		cmpType = lType
-	} else {
-		cmpType = types.GetCompareType(lType, right[0].Type())
-	}
-
 	elements := make(map[uint64]struct{})
 	for _, v := range rVals {
-		key, err := hash.HashOfSimple(ctx, v, cmpType)
-		if err != nil {
-			return nil, nil, false, err
+		key, hErr := hash.HashOfSimple(ctx, v, cmpType)
+		if hErr != nil {
+			return nil, nil, false, hErr
 		}
 		elements[key] = struct{}{}
 	}
