da6f48f199a9278626a36cc30a3f288650b9ef60
James Cor <james@dolthub.com>
2025-09-29 16:01:42 -0700

    fix

--- sql/types/number.go
+++ sql/types/number.go
@@ -23,6 +23,7 @@ import (
 	"reflect"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 	"unicode"
 
@@ -454,8 +455,6 @@ func (t NumberTypeImpl_) ConvertRound(ctx context.Context, v interface{}) (any, 
 		return convertToInt64(t, v, ShouldRound)
 	case sqltypes.Uint64:
 		return convertToUint64(t, v, ShouldRound)
-	case sqltypes.Uint64:
-		return convertToUint64(t, v, true)
 	default:
 		return t.Convert(ctx, v)
 	}
