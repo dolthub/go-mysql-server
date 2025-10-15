f9fa8405bf428ed20345c28fe2ce2329a8fd3aa4
Angela Xie <angela@dolthub.com>
2025-10-15 14:40:55 -0700

    add aggregate dependency column back to analyzeHaving, skip having child projection, add select aliases to select dependencies so they can be resolved by order by exprs

--- sql/analyzer/validation_rules.go
+++ sql/analyzer/validation_rules.go
@@ -251,9 +251,13 @@ func validateGroupBy(ctx *sql.Context, a *Analyzer, n sql.Node, scope *plan.Scop
 	}
 
 	var err error
+	var parent sql.Node
 	var project *plan.Project
 	var orderBy *plan.Sort
 	transform.Inspect(n, func(n sql.Node) bool {
+		defer func() {
+			parent = n
+		}()
 		switch n := n.(type) {
 		case *plan.GroupBy:
 			var noGroupBy bool
@@ -334,8 +338,10 @@ func validateGroupBy(ctx *sql.Context, a *Analyzer, n sql.Node, scope *plan.Scop
 				}
 			}
 		case *plan.Project:
-			project = n
-			orderBy = nil
+			if _, isHaving := parent.(*plan.Having); !isHaving {
+				project = n
+				orderBy = nil
+			}
 		case *plan.Sort:
 			orderBy = n
 		}
@@ -406,6 +412,7 @@ func resolveExpr(expr sql.Expression, selectDeps map[string]sql.Expression, grou
 		switch expr := expr.(type) {
 		case *expression.Alias:
 			if dep, ok := selectDeps[strings.ToLower(expr.Child.String())]; ok {
+				selectDeps[strings.ToLower(expr.Name())] = dep
 				return dep, transform.NewTree, nil
 			}
 		case *expression.GetField:
