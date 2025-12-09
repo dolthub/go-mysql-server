770a84ccec2e37f998f6ee4c090d13f51f20bd22
Zach Musgrave <zach@dolthub.com>
2025-12-08 17:41:06 -0800

    Retain existing bounds checking behavior for foreign key lookups across types

--- sql/plan/foreign_key_editor.go
+++ sql/plan/foreign_key_editor.go
@@ -741,10 +741,10 @@ type ForeignKeyTypeConversionFn func(ctx *sql.Context, val any) (sql.Type, any, 
 // type in the corresponding table. Specify the schema of both child and parent tables, as well as whether the
 // transformation is from child to parent or vice versa.
 func GetForeignKeyTypeConversions(
-	parentSch sql.Schema,
-	childSch sql.Schema,
-	fkDef sql.ForeignKeyConstraint,
-	direction ForeignKeyTypeConversionDirection,
+		parentSch sql.Schema,
+		childSch sql.Schema,
+		fkDef sql.ForeignKeyConstraint,
+		direction ForeignKeyTypeConversionDirection,
 ) ([]ForeignKeyTypeConversionFn, error) {
 	var convFns []ForeignKeyTypeConversionFn
 
@@ -787,7 +787,10 @@ func GetForeignKeyTypeConversions(
 				convFns = make([]ForeignKeyTypeConversionFn, len(childSch))
 			}
 			convFns[childIndex] = func(ctx *sql.Context, val any) (sql.Type, any, error) {
-				convertedVal, _, err := toType.ConvertToType(ctx, fromType, val)
+				convertedVal, inRange, err := toType.ConvertToType(ctx, fromType, val)
+				if !inRange {
+					return toType, nil, sql.ErrValueOutOfRange.New(val, toType)
+				}
 				return toType, convertedVal, err
 			}
 		}
