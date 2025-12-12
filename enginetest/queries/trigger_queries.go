97609b31e83f3c859ce72e356e0549b26fc5d3cc
angelamayxie <angela@dolthub.com>
2025-12-11 16:36:14 -0800

    add test james's test case

--- enginetest/queries/trigger_queries.go
+++ enginetest/queries/trigger_queries.go
@@ -3881,11 +3881,20 @@ end;
 			"INSERT INTO t_1 (id, other, status) VALUES ROW(0x3E32, 0xC336, 0), ROW(0xEDC1, 0xC336, 0), ROW(0x9B15, 0xC336, 0);",
 			"CREATE TRIGGER t_change_status before UPDATE ON t_1 FOR EACH ROW INSERT INTO oldstatus (id, status) SELECT id, status FROM t_1 WHERE id = NEW.id;",
 			"UPDATE t_1 SET status = 1 WHERE id = 0x3E32;",
+			"create table src(a int, b int, c int)",
+			"create table dest(i int, j int)",
+			"create trigger trig before update on src for each row insert into dest(j, i) select a, c from src where b = new.b",
+			"insert into src values (1, 2, 3)",
+			"update src set a = 4 where b = 2",
 		},
 		Assertions: []ScriptTestAssertion{
 			{
 				Query:    `SELECT * FROM oldstatus`,
 				Expected: []sql.Row{{[]uint8{0x3e, 0x32}, uint8(0)}},
+			},
+			{
+				Query:    "select * from dest",
+				Expected: []sql.Row{{3, 1}},
 			},
 		},
 	},
