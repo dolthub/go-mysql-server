fca35423034dbbac4181a862ffecf645486d4529
James Cor <james@ucsd.edu>
2025-12-30 15:25:04 -0800

    concurrent row iters

--- sql/table_iter.go
+++ sql/table_iter.go
@@ -21,8 +21,12 @@ import (
 // TableRowIter is an iterator over the partitions in a table.
 type TableRowIter struct {
 	rowIters      []RowIter
+	rowChans      []chan Row
 	valueRowIters []ValueRowIter
+	valueRowChans []chan ValueRow
+	errChan       chan error
 	idx           int
+	hasInit       bool
 }
 
 var _ RowIter = (*TableRowIter)(nil)
@@ -52,61 +56,122 @@ func NewTableRowIter(ctx *Context, table Table, partitions PartitionIter) *Table
 			valueRowIters = append(valueRowIters, valRowIter)
 		}
 	}
+
+	var rowChans = make([]chan Row, len(rowIters))
+	for i := 0; i < len(rowIters); i++ {
+		rowChans[i] = make(chan Row, 512)
+	}
+
+	var valueRowChans []chan ValueRow
+	if len(valueRowIters) > 0 {
+		valueRowChans = make([]chan ValueRow, len(valueRowIters))
+		for i := 0; i < len(valueRowIters); i++ {
+			valueRowChans[i] = make(chan ValueRow, 512)
+		}
+	}
+
 	return &TableRowIter{
 		rowIters:      rowIters,
+		rowChans:      rowChans,
 		valueRowIters: valueRowIters,
+		valueRowChans: valueRowChans,
+		errChan:       make(chan error),
+	}
+}
+
+func (i *TableRowIter) exhaustIter(ctx *Context, rowIter RowIter, rowChan chan Row) {
+	defer close(rowChan)
+	for {
+		row, err := rowIter.Next(ctx)
+		if err != nil {
+			if err != io.EOF {
+				i.errChan <- err
+			}
+			err = rowIter.Close(ctx)
+			if err != nil {
+				i.errChan <- err
+			}
+			break
+		}
+		rowChan <- row
+	}
+}
+
+func (i *TableRowIter) initRowQueue(ctx *Context) {
+	for idx := 0; idx < len(i.rowIters); idx++ {
+		go i.exhaustIter(ctx, i.rowIters[idx], i.rowChans[idx])
 	}
 }
 
 func (i *TableRowIter) Next(ctx *Context) (Row, error) {
-	select {
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	default:
-	}
 	if i.idx == len(i.rowIters) {
 		return nil, io.EOF
 	}
-	rowIter := i.rowIters[i.idx]
-	row, err := rowIter.Next(ctx)
-	if err != nil {
-		if err != io.EOF {
-			return nil, err
-		}
-		err = rowIter.Close(ctx)
-		if err != nil {
-			return nil, err
-		}
-		i.idx++
-		return i.Next(ctx)
+	if !i.hasInit {
+		i.initRowQueue(ctx)
+		i.hasInit = true
 	}
-	return row, nil
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case err := <-i.errChan:
+		return nil, err
+	case row, ok := <-i.rowChans[i.idx]:
+		if !ok {
+			i.idx++
+			return i.Next(ctx)
+		}
+		return row, nil
+	}
+}
+
+func (i *TableRowIter) initValueRowQueue(ctx *Context) {
+	for idx := 0; idx < len(i.valueRowIters); idx++ {
+		go i.exhaustValueIter(ctx, i.valueRowIters[idx], i.valueRowChans[idx])
+	}
+}
+
+func (i *TableRowIter) exhaustValueIter(ctx *Context, rowIter ValueRowIter, rowChan chan ValueRow) {
+	defer close(rowChan)
+	for {
+		row, err := rowIter.NextValueRow(ctx)
+		if err != nil {
+			if err != io.EOF {
+				i.errChan <- err
+			}
+			err = rowIter.Close(ctx)
+			if err != nil {
+				i.errChan <- err
+			}
+			break
+		}
+		rowChan <- row
+	}
 }
 
 // NextValueRow implements the sql.ValueRowIter interface
 func (i *TableRowIter) NextValueRow(ctx *Context) (ValueRow, error) {
+	if i.idx == len(i.rowIters) {
+		return nil, io.EOF
+	}
+	if !i.hasInit {
+		i.initRowQueue(ctx)
+		i.hasInit = true
+	}
+
 	select {
 	case <-ctx.Done():
 		return nil, ctx.Err()
-	default:
-	}
-	if i.idx == len(i.valueRowIters) {
-		return nil, io.EOF
-	}
-	rowIter := i.valueRowIters[i.idx]
-	row, err := rowIter.NextValueRow(ctx)
-	if err != nil {
-		if err != io.EOF {
-			return nil, err
+	case err := <-i.errChan:
+		return nil, err
+	case row, ok := <-i.valueRowChans[i.idx]:
+		if !ok {
+			i.idx++
+			return i.NextValueRow(ctx)
 		}
-		err = rowIter.Close(ctx)
-		if err != nil {
-			return nil, err
-		}
-		i.idx++
-		return i.NextValueRow(ctx)
+		return row, nil
 	}
-	return row, nil
 }
 
 // IsValueRowIter implements the sql.ValueRowIter interface.
