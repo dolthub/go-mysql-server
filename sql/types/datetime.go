56694724404220510a8acb2d8d103bc5915e7677
Nick Tobey <nick@dolthub.com>
2026-03-19 15:59:04 -0700

    Use proper precision in DateTime::SQLValue

--- sql/types/datetime.go
+++ sql/types/datetime.go
@@ -499,6 +499,11 @@ func (t datetimeType) SQLValue(ctx *sql.Context, v sql.Value, dest []byte) (sqlt
 		vt := time.UnixMicro(x).UTC()
 		if vt.Equal(ZeroTime) {
 			dest = append(dest, ZeroTimestampDatetimeStr...)
+			if t.precision > 0 {
+				dest = append(dest, []byte(".000000")...)
+				digitsToTrim := 6 - t.precision
+				dest = dest[:len(dest)-digitsToTrim]
+			}
 		} else {
 			dest = appendDatetimeFormat(dest, vt, t.precision)
 		}
