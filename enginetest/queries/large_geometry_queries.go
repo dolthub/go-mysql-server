0fb741bf7f9910f04aa9119e01c0fd6a8765f074
Zach Musgrave <zach@dolthub.com>
2026-04-02 16:37:32 -0700

    PR feedback

--- enginetest/queries/large_geometry_queries.go
+++ enginetest/queries/large_geometry_queries.go
@@ -143,11 +143,11 @@ var LargeGeometryScriptTests = []ScriptTest{
 		},
 		Assertions: []ScriptTestAssertion{
 			{
-				Query:    "SELECT ST_SRID(ST_SRID(l, 3857)) FROM large_line_srid WHERE i = 1",
-				Expected: []sql.Row{{uint32(3857)}},
+				Query:    "SELECT ST_SRID(ST_SRID(l, 0)) FROM large_line_srid WHERE i = 1",
+				Expected: []sql.Row{{uint32(0)}},
 			},
 			{
-				Query:    "SELECT ST_ASWKT(ST_STARTPOINT(ST_SRID(l, 3857))) FROM large_line_srid WHERE i = 1",
+				Query:    "SELECT ST_ASWKT(ST_STARTPOINT(ST_SRID(l, 0))) FROM large_line_srid WHERE i = 1",
 				Expected: []sql.Row{{"POINT(0 0)"}},
 			},
 		},
