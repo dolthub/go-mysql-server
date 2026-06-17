030899472daf158dc59134b57e008a70b188f66c
Aaron Son <aaron@dolthub.com>
2026-06-16 18:20:31 -0700

    Revert "Merge pull request #3593 from dolthub/aaron/vitess-async-read-eof"
    This reverts commit ef65f605ab10112ae223acc711fe1a43622eb0b1, reversing
    changes made to 6466ba36ebf5012bf1ff9ecee79bb1a753c74c54.

---
+++ internal/sockstate/netstat_windows.go
@@ -0,0 +1,36 @@
+// Copyright 2020-2021 Dolthub, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build windows
+// +build windows
+
+package sockstate
+
+import (
+	"net"
+
+	"github.com/sirupsen/logrus"
+)
+
+// tcpSocks returns a slice of active TCP sockets containing only those
+// elements that satisfy the accept function
+func tcpSocks(accept AcceptFn) ([]sockTabEntry, error) {
+	// (juanjux) TODO: not implemented
+	logrus.Warn("Connection checking not implemented for Windows")
+	return nil, ErrSocketCheckNotImplemented.New()
+}
+
+func GetConnInode(c *net.TCPConn) (n uint64, err error) {
+	return 0, ErrSocketCheckNotImplemented.New()
+}
