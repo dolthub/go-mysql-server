3f43a35f944339e6e325cd319d7029883c8491ea
James Cor <james@ucsd.edu>
2026-06-22 14:06:28 -0700

    avoid SPrintf for int types

--- sql/hash/hash.go
+++ sql/hash/hash.go
@@ -189,6 +189,26 @@ func HashOfSimple(ctx *sql.Context, i any, t sql.Type) (uint64, sql.ConvertInRan
 
 		// Remove trailing 0s from floats
 		switch v := val.(type) {
+		case int:
+			str = strconv.FormatInt(int64(v), 10)
+		case int8:
+			str = strconv.FormatInt(int64(v), 10)
+		case int16:
+			str = strconv.FormatInt(int64(v), 10)
+		case int32:
+			str = strconv.FormatInt(int64(v), 10)
+		case int64:
+			str = strconv.FormatInt(v, 10)
+		case uint:
+			str = strconv.FormatUint(uint64(v), 10)
+		case uint8:
+			str = strconv.FormatUint(uint64(v), 10)
+		case uint16:
+			str = strconv.FormatUint(uint64(v), 10)
+		case uint32:
+			str = strconv.FormatUint(uint64(v), 10)
+		case uint64:
+			str = strconv.FormatUint(v, 10)
 		case float32:
 			str = strconv.FormatFloat(float64(v), 'f', -1, 32)
 			if str == "-0" {
