6c4649bade76e3841bc6c7ed4ef0719be3439990
James Cor <james@ucsd.edu>
2026-07-23 16:35:05 -0700

    fix test

--- enginetest/join_stats_tests.go
+++ enginetest/join_stats_tests.go
@@ -78,8 +78,8 @@ var JoinStatTests = []struct {
 			},
 			{
 				// b with filter is smaller
-				q:     "select /*+ LEFT_DEEP */ count(*) from `u-15` a join `u+15` b on a.b = b.b where b.b < 14",
-				order: [][]string{{"b", "aa"}},
+				q:     "select /*+ LEFT_DEEP */ count(*) from `u-15` a join `u+15` b on a.b = b.b where b.b < 4",
+				order: [][]string{{"b", "a"}},
 			},
 		},
 	},
