fab90a8719e7baa09e742b700730468a05cc6ce4
James Cor <james@ucsd.edu>
2026-07-23 02:03:51 -0700

    oops

--- sql/analyzer/costed_index_scan.go
+++ sql/analyzer/costed_index_scan.go
@@ -617,7 +617,7 @@ func (c *indexCoster) updateBest(s sql.Statistic, hist []sql.HistogramBucket, fd
 	if rowCnt < c.bestCnt {
 		// TODO: secondary indexes incur an additional index lookup cost, so they need to reduce rowCount by a substantial amount
 		if s.Qualifier().Index() != "PRIMARY" && s.Qualifier().Index() != "" {
-			if rowCnt/4 < c.bestCnt {
+			if rowCnt < c.bestCnt/4 {
 				update = true
 			}
 			return
