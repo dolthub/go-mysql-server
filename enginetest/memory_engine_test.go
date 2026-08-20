135470e95b7e1c4dec2929802953b82a39252f7c
James Cor <james@ucsd.edu>
2026-08-20 16:44:33 -0700

    revert

--- enginetest/memory_engine_test.go
+++ enginetest/memory_engine_test.go
@@ -193,18 +193,30 @@ func TestSingleQueryPrepared(t *testing.T) {
 
 // Convenience test for debugging a single query. Unskip and set to the desired query.
 func TestSingleScript(t *testing.T) {
-	//t.Skip()
+	t.Skip()
 	var scripts = []queries.ScriptTest{
 		{
 			Name: "Parse table name as column",
 			SetUpScript: []string{
-				"CREATE TABLE t(id INT PRIMARY KEY, d DATE);",
-				"INSERT INTO t VALUES (1, '2024-01-01'), (2, '2024-01-02');",
+				`CREATE TABLE test (pk INT PRIMARY KEY, v1 VARCHAR(255));`,
+				`INSERT INTO test VALUES (1, 'a'), (2, 'b');`,
 			},
 			Assertions: []queries.ScriptTestAssertion{
 				{
-					Query:    "SELECT round(date('2001-02-03')) > 0;",
-					Expected: []sql.Row{true},
+					Query:    "SELECT temporarytesting(t) FROM test AS t;",
+					Expected: []sql.Row{},
+				},
+				{
+					Query:    "SELECT temporarytesting(test) FROM test;",
+					Expected: []sql.Row{},
+				},
+				{
+					Query:    "SELECT temporarytesting(pk, test) FROM test;",
+					Expected: []sql.Row{},
+				},
+				{
+					Query:    "SELECT temporarytesting(v1, test, pk) FROM test;",
+					Expected: []sql.Row{},
 				},
 			},
 		},
