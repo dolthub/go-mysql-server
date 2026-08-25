20a07a9627d7937894bb0baabbf800c7446dd35d
jycor <jycor@users.noreply.github.com>
2026-08-25 02:08:32 +0000

    [ga-format-pr] Run ./format_repo.sh to fix formatting

--- enginetest/memory_engine_test.go
+++ enginetest/memory_engine_test.go
@@ -16,6 +16,14 @@ package enginetest_test
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/dolthub/sqllogictest/go/logictest"
+	"github.com/stretchr/testify/require"
+
 	"github.com/dolthub/go-mysql-server/enginetest"
 	"github.com/dolthub/go-mysql-server/enginetest/queries"
 	"github.com/dolthub/go-mysql-server/enginetest/scriptgen/setup"
@@ -25,12 +33,6 @@ import (
 	"github.com/dolthub/go-mysql-server/sql/expression"
 	"github.com/dolthub/go-mysql-server/sql/types"
 	_ "github.com/dolthub/go-mysql-server/sql/variables"
-	"github.com/dolthub/sqllogictest/go/logictest"
-	"github.com/stretchr/testify/require"
-	"log"
-	"os"
-	"testing"
-	"time"
 )
 
 // This file is for validating both the engine itself and the in-memory database implementation in the memory package.
