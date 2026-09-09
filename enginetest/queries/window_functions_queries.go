6839124afdd6070760430a5dd3b45a087ef0914f
zachmu <zach@dolthub.com>
2026-09-09 00:03:55 +0000

    Test String() for NTile

--- enginetest/queries/window_functions_queries.go
+++ enginetest/queries/window_functions_queries.go
@@ -1139,6 +1139,14 @@ ORDER BY id;`,
 		},
 		Assertions: []ScriptTestAssertion{
 			{
+				Query: "select i, ntile(2) over(order by i), ntile(3) over(order by i) from t where i <= 3;",
+				Expected: []sql.Row{
+					{1, uint64(1), uint64(1)},
+					{2, uint64(1), uint64(2)},
+					{3, uint64(2), uint64(3)},
+				},
+			},
+			{
 				Query:       "select i, ntile(0) over() from t;",
 				ExpectedErr: sql.ErrInvalidArgument,
 			},
