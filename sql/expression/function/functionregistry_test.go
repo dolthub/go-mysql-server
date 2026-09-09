cdca18a2dfd55c3924f936dd5ca4b109d79bdf56
zachmu <zach@dolthub.com>
2026-09-09 00:04:09 +0000

    Fix String() for STEquals

--- sql/expression/function/functionregistry_test.go
+++ sql/expression/function/functionregistry_test.go
@@ -78,3 +78,17 @@ func TestMultiLineStringFromTextRegistryString(t *testing.T) {
 	require.Equal(t, "st_mlinefromtext('MULTILINESTRING((1 2, 3 4))')", expr.String())
 	exprtest.AssertFunctionRoundTrip(t, expr.(sql.FunctionExpression))
 }
+
+func TestSTEqualsRegistryString(t *testing.T) {
+	ctx := sql.NewEmptyContext()
+	reg := function.NewRegistry()
+	reg.Register(function.BuiltIns...)
+	fn, ok := reg.Function(ctx, "", "st_equals")
+	require.True(t, ok)
+	point := expression.NewLiteral(types.Point{X: 1, Y: 2}, types.PointType{})
+	expr, err := fn.NewInstance(ctx, []sql.Expression{point, point})
+	require.NoError(t, err)
+	require.IsType(t, &spatial.STEquals{}, expr)
+	require.Equal(t, "ST_EQUALS(ST_GeomFromWKB(0x0101000000000000000000F03F0000000000000040, 0), ST_GeomFromWKB(0x0101000000000000000000F03F0000000000000040, 0))", expr.String())
+	exprtest.AssertFunctionRoundTrip(t, expr.(sql.FunctionExpression))
+}
