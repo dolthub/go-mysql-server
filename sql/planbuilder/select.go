65f068fd89534f13fb39771f66310e3ef2e04307
Jason Fulghum <jason@dolthub.com>
2026-09-09 16:47:19 -0700

    Refactor correlated aggregate resolution

--- sql/planbuilder/select.go
+++ sql/planbuilder/select.go
@@ -66,7 +66,11 @@ func (b *Builder) buildSelect(inScope *scope, s *ast.Select) (outScope *scope) {
 	// 5) Build top-level scopes, replacing aggregation and aliases with
 	//    projections from (4).
 	// 6) Finish with final target projections.
+	outerQuery := inScope.querySource
 	fromScope := b.buildFrom(inScope, s.From)
+	fromScope.querySource = fromScope
+	fromScope.outerQuery = outerQuery
+	fromScope.querySubquery = inScope.nearestSubquery()
 	if cn, ok := fromScope.node.(sql.CommentedNode); ok && len(s.Comments) > 0 {
 		fromScope.node = cn.WithComment(string(s.Comments[0]))
 	}
@@ -87,6 +91,11 @@ func (b *Builder) buildSelect(inScope *scope, s *ast.Select) (outScope *scope) {
 	// Find aggregations in order by
 	orderByScope := b.analyzeOrderBy(fromScope, projScope, s.OrderBy)
 
+	var groupingCols []sql.Expression
+	if len(s.GroupBy) > 0 {
+		groupingCols = b.buildGroupingCols(fromScope, projScope, s.GroupBy, s.SelectExprs)
+	}
+
 	// Find aggregations in having
 	b.analyzeHaving(fromScope, projScope, s.Having)
 
@@ -94,7 +103,6 @@ func (b *Builder) buildSelect(inScope *scope, s *ast.Select) (outScope *scope) {
 	// so we can build the FROM clause
 	needsAggregation := b.needsAggregation(fromScope, s)
 	if needsAggregation {
-		groupingCols := b.buildGroupingCols(fromScope, projScope, s.GroupBy, s.SelectExprs)
 		outScope = b.buildAggregation(fromScope, projScope, groupingCols, s.Having)
 	} else if fromScope.windowFuncs != nil {
 		outScope = b.buildWindow(fromScope, projScope)
