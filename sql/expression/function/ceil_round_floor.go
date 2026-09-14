c621fe024cb6775986dc13e8f2e4f2fc931ad90b
James Cor <james@ucsd.edu>
2026-09-14 11:32:04 -0700

    fix

--- sql/expression/function/ceil_round_floor.go
+++ sql/expression/function/ceil_round_floor.go
@@ -275,7 +275,11 @@ func (r *Round) Eval(ctx *sql.Context, row sql.Row) (any, error) {
 	if err != nil {
 		return nil, err
 	}
-	val, _, err = types.TypeAwareConversion(ctx, val, r.Num.Type(ctx), types.InternalDecimalType)
+	if typ := r.Num.Type(ctx); types.IsTime(typ) {
+		val, _, err = types.TypeAwareConversion(ctx, val, typ, types.InternalDecimalType)
+	} else {
+		val, _, err = types.InternalDecimalType.Convert(ctx, val)
+	}
 	if err != nil && sql.ErrTruncatedIncorrect.Is(err) {
 		ctx.Warn(mysql.ERTruncatedWrongValue, "%s", err.Error())
 	}
