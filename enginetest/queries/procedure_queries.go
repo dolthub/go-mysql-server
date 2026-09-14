f8a3b0cbbb6e086796f55e3006341e3059a824d9
Zach Musgrave <zach@dolthub.com>
2026-09-14 21:18:31 +0000

    Place regression coverage in the shared engine suite

--- enginetest/queries/procedure_queries.go
+++ enginetest/queries/procedure_queries.go
@@ -1355,6 +1355,19 @@ END`,
 			},
 		},
 	},
+	// https://github.com/dolthub/dolt/issues/6742
+	{
+		Name: "Test cursor continue-handler checksum loops",
+		SetUpScript: []string{
+			"CREATE TABLE checksums(id INT AUTO_INCREMENT PRIMARY KEY,checksum VARCHAR(40))",
+			"INSERT INTO checksums VALUES(1,SHA('macneale'))",
+			"CREATE PROCEDURE calculate_checksum()\nBEGIN\n DECLARE done INT DEFAULT 0;\n DECLARE current_checksum VARCHAR(40);\n DECLARE concat_string VARCHAR(10000) DEFAULT '';\n DECLARE cur CURSOR FOR SELECT checksum FROM checksums ORDER BY id;\n DECLARE CONTINUE HANDLER FOR NOT FOUND SET done=1;\n OPEN cur;\n read_loop: LOOP\n  FETCH cur INTO current_checksum;\n  IF done THEN LEAVE read_loop; END IF;\n  SET concat_string=CONCAT(concat_string,current_checksum);\n END LOOP;\n CLOSE cur;\n INSERT INTO checksums(checksum) VALUES(SHA1(concat_string));\nEND",
+			"CALL calculate_checksum()",
+		},
+		Assertions: []ScriptTestAssertion{
+			{Query: "SELECT * FROM checksums ORDER BY id", Expected: []sql.Row{{int32(1), "ca530ba53d2e3b54206e62c7ab257657b7367cc7"}, {int32(2), "89fa71febbc9effd2fa58c7441ad2ed899fcdcf1"}}},
+		},
+	},
 	{
 		Name: "DECLARE HANDLERs exit according to the block they were declared in",
 		SetUpScript: []string{
