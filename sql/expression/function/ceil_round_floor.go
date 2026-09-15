255f3e657a53b1e2a0edf582d2b79dac98a65acd
James Cor <james@ucsd.edu>
2026-09-15 13:32:23 -0700

    special case

--- sql/expression/function/ceil_round_floor.go
+++ sql/expression/function/ceil_round_floor.go
@@ -275,7 +275,11 @@ func (r *Round) Eval(ctx *sql.Context, row sql.Row) (any, error) {
 	if err != nil {
 		return nil, err
 	}
-	val, _, err = types.TypeAwareConversion(ctx, val, r.Num.Type(ctx), types.InternalDecimalType)
+	if typ := r.Num.Type(ctx); types.IsBlobType(typ) {
+		val, _, err = types.InternalDecimalType.Convert(ctx, val)
+	} else {
+		val, _, err = types.TypeAwareConversion(ctx, val, typ, types.InternalDecimalType)
+	}
 	if err != nil && sql.ErrTruncatedIncorrect.Is(err) {
 		ctx.Warn(mysql.ERTruncatedWrongValue, "%s", err.Error())
 	}
